b72d4f12669950d7adf5b3d1244d6850d72eb937
Shawn Ledbetter <sledbetter@google.com>
2011-09-09 10:30:10 -0700

    use repo url for marvell
    Change-Id: Iecb02afcd440dadbfb5b801b3bcbead85842e1e1

--- package/mv_cust/mv_cust.mk
+++ package/mv_cust/mv_cust.mk
@@ -1,14 +1,13 @@
 MV_CUST_VERSION=master
-MV_CUST_SITE=git://localhost/vendor-marvell-mv_cust.git
-# MV_CUST_DEPENDENCIES=linux
+MV_CUST_SITE=repo://vendor/marvell/mv_cust
+MV_CUST_DEPENDENCIES=linux
 
 define MV_CUST_BUILD_CMDS
 	mkdir -p $(@D)/lib
-	$(MAKE) -C $(@D) CROSS_COMPILE=$(TARGET_CROSS) KDIR=$(LINUX_DIR) 
+	$(MAKE) -C $(@D) CROSS_COMPILE=$(TARGET_CROSS) KDIR=$(LINUX_DIR)
 endef
 
 define MV_CUST_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/lib/modules/mv_cust
 	$(INSTALL) -D -m 0555 $(@D)/lib/mvcust.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extras/mvcust.ko
 endef
 
