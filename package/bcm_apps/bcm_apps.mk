57c2c05216b0f2e014f55aa16d4f3ad1cadb807d
Shawn Ledbetter <sledbetter@google.com>
2011-09-13 21:26:52 -0700

    Add Broadcom AppLibs target
    enable the apps target in the bruno config
    currently disable the RAP_DDP option (missing DDP targets)
    using proxy mode for Nexus (uses bcm-nexus built kernel mode)
    remove the DEBUG flag (use B_REFSW_DEBUG flag only)
    
    Change-Id: I5a08aa416bd70a9b91bb93a961c1426eefdddd06

---
+++ package/bcm_apps/bcm_apps.mk
@@ -0,0 +1,52 @@
+BCM_APPS_SITE=repo://vendor/broadcom/AppLibs
+BCM_APPS_DEPENDENCIES=linux bcm_bseav
+BCM_APPS_CONFIGURE_CMDS=ln -sf $(@D) $(BUILD_DIR)/AppLibs
+BCM_APPS_POST_EXTRACT_HOOKS=BCM_REMOVE_PATCH_REJECTS
+
+define BCM_REMOVE_PATCH_REJECTS
+	find $(@D) -name '*.rej' -exec rm \{\} \;
+endef
+
+BCM_APPS_APPLIB_TARGETS=$(BCM_OBJS-y)
+
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_REFSW)       += refsw
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_AVAHI)       += avahi
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_OSAPI)       += osapi
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_NETAPP)      += netapp
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_DLNA)        += dlna
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_DVB)         += dvb
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_DIRECTFB)    += directfb
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_STAGECRAFT)  += stagecraft
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_ARGO)        += argo
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_COVERFLOW3D) += coverflow3d
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_BMC)         += bmc
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_BROWSER)     += browser
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_GSTREAMER)   += gstreamer
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_ALSA)        += alsa
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_PLAYBACK_IP) += playback_ip
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_DTCP_IP)     += dtcp_ip
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_QT)          += qt
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_LIBCURL)     += libcurl
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_SQLITE)      += sqlite
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_LIBXML2)     += libxml2
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_PIXMAN)      += pixman
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_LIBXSLT)     += libxslt
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_FONTCONFIG)  += fontconfig
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_CAIRO)       += cairo
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_ICU)         += icu
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_ZLIB)        += zlib
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_LIBPNG)      += libpng
+BCM_OBJS-$(BR2_PACKAGE_BCM_APP_JPEG)        += jpeg
+
+
+include package/bcm_common/bcm_common.mk
+
+define BCM_APPS_BUILD_CMDS
+	$(MAKE) $(BCM_MAKEFLAGS)  -C $(@D)/common $(BCM_APPS_APPLIB_TARGETS)
+endef
+
+define BCM_APPS_INSTALL_TARGET_CMDS
+	$(TAR) -xf $(@D)/97425*.mipsel-linux-uclibc.release.*tgz -C $(TARGET_DIR)
+endef
+
+$(eval $(call GENTARGETS,package,bcm_apps))
