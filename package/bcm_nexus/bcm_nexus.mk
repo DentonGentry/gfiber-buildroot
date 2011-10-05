8be9ec235de1e7e2ea67f3d3937eac08ceb8501f
Shawn Ledbetter <sledbetter@google.com>
2011-10-05 09:33:13 -0700

    Add dependencies to host-pkg-config, use sed/install for build.
    Change-Id: I93d473589e73c2e2bd27f94d2861935b6b41c0e8

--- package/bcm_nexus/bcm_nexus.mk
+++ package/bcm_nexus/bcm_nexus.mk
@@ -1,5 +1,5 @@
 BCM_NEXUS_SITE=repo://vendor/broadcom/nexus
-BCM_NEXUS_DEPENDENCIES=linux bcm_magnum bcm_bseav
+BCM_NEXUS_DEPENDENCIES=linux bcm_magnum bcm_bseav host-pkg-config
 BCM_NEXUS_CONFIGURE_CMDS=ln -sf $(@D) $(BUILD_DIR)/nexus
 BCM_NEXUS_INSTALL_STAGING=YES
 BCM_NEXUS_INSTALL_TARGET=YES
@@ -17,9 +17,8 @@ endef
 
 define BCM_NEXUS_INSTALL_STAGING_CMDS
 	ln -sf $(@D) $(STAGING_DIR)/$(BCM_NEXUS_STAGING_PATH)
-	cat $(@D)/bin/nexus.pc | \
-	sed "s@$(@D)@/$(BCM_NEXUS_STAGING_PATH)@g" | \
-	sed "s@std=c89@std=c99@g" > $(STAGING_DIR)/usr/lib/pkgconfig/nexus.pc
+	sed -i"" -e "s@$(@D)@/$(BCM_NEXUS_STAGING_PATH)@g" -e "s@std=c89@std=c99@g" $(@D)/bin/nexus.pc
+	$(INSTALL) -D $(@D)/bin/nexus.pc $(STAGING_DIR)/usr/lib/pkgconfig/nexus.pc
 endef
 
 define BCM_NEXUS_INSTALL_TARGET_CMDS
