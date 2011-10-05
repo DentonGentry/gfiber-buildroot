8f44ff1b097ff8f3ae4ddc94ffadf495a8cb5eb8
Shawn Ledbetter <sledbetter@google.com>
2011-10-05 09:33:13 -0700

    Setup bcm_nexus as a pkg-config installable
    Rewrite the directory in nexus.pc to use the staging area.  Use c99
    when compiling - this allows unadulterated alsa packages to be used.
    
    Change-Id: I5c188273e2778c06ab7a107e05647b843ca228cf

--- package/bcm_nexus/bcm_nexus.mk
+++ package/bcm_nexus/bcm_nexus.mk
@@ -1,11 +1,31 @@
 BCM_NEXUS_SITE=repo://vendor/broadcom/nexus
 BCM_NEXUS_DEPENDENCIES=linux bcm_magnum bcm_bseav
 BCM_NEXUS_CONFIGURE_CMDS=ln -sf $(@D) $(BUILD_DIR)/nexus
+BCM_NEXUS_INSTALL_STAGING=YES
+BCM_NEXUS_INSTALL_TARGET=YES
+
+BCM_NEXUS_STAGING_PATH=usr/lib/nexus
+BCM_NEXUS_TARGET_PATH=usr/lib/nexus
 
 include package/bcm_common/bcm_common.mk
 
 define BCM_NEXUS_BUILD_CMDS
-        $(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D)/build
+	$(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D)/build all
+	$(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D)/examples pkg-config
+endef
+
+
+define BCM_NEXUS_INSTALL_STAGING_CMDS
+	ln -sf $(@D) $(STAGING_DIR)/$(BCM_NEXUS_STAGING_PATH)
+	cat $(@D)/bin/nexus.pc | \
+	sed "s@$(@D)@/$(BCM_NEXUS_STAGING_PATH)@g" | \
+	sed "s@std=c89@std=c99@g" > $(STAGING_DIR)/usr/lib/pkgconfig/nexus.pc
+endef
+
+define BCM_NEXUS_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/$(BCM_NEXUS_TARGET_PATH)/bin
+	cp $(@D)/bin/* $(TARGET_DIR)/$(BCM_NEXUS_TARGET_PATH)/bin
+	rm $(TARGET_DIR)/$(BCM_NEXUS_TARGET_PATH)/bin/nexus.pc
 endef
 
 $(eval $(call GENTARGETS,package,bcm_nexus))
