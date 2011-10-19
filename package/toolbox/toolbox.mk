e2eddae2ffbd691ec26bdcfc456e66333697d158
Shawn Ledbetter <sledbetter@google.com>
2011-10-19 16:50:14 -0700

    Add toolbox-for-linux package to buildroot.
    Toolbox-for-linux is a BSD-licensed replacement for busybox.  This
    commit adds the target to buildroot and disables busybox if toolbox is
    enabled; currently it is NOT enabled.  Replacing busybox with toolbox
    requires the coordination of other packages (dhcp, coreutils, ntp,
    bridge to start with) and is not happening in this commit.
    
    Change-Id: Ife0aaaa4af2c1b8c06280c554d1418c93b2c91ca

---
+++ package/toolbox/toolbox.mk
@@ -0,0 +1,15 @@
+TOOLBOX_SITE=repo://toolbox-for-linux
+
+# TODO
+# replacements needed for:
+# getty, udhcpd, ntpd, mount (w/ volume support), fdisk
+
+define TOOLBOX_BUILD_CMDS
+	$(MAKE) -C $(@D) CC="$(TARGET_CC)" all
+endef
+
+define TOOLBOX_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D) CC="$(TARGET_CC)" TARGET_OUT="$(TARGET_DIR)/bin" install
+endef
+
+$(eval $(call GENTARGETS,package,toolbox))
