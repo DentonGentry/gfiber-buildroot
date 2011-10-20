f2e508e03acdf7f44f97b8f01c8e1b66940494ed
Ke Dong <kedong@google.com>
2011-10-20 13:43:30 -0700

    Fix system init problem.
    Fix libnexus.so target directory.
    Move bridge utility from Busybox to buildroot.
    Remove ifupdown from Busybox.
    
    Change-Id: Ib4ab1dfbe13c9b6cd1513b533587243f8f6fce1b

--- package/bcm_rockford/bcm_rockford.mk
+++ package/bcm_rockford/bcm_rockford.mk
@@ -5,13 +5,13 @@ BCM_ROCKFORD_CONFIGURE_CMDS=ln -sf $(@D) $(BUILD_DIR)/rockford
 include package/bcm_common/bcm_common.mk
 
 define BCM_ROCKFORD_BUILD_CMDS
-        $(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D)/middleware/v3d -f V3DDriver.mk
-        $(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D)/middleware/platform/nexus -f platform_nexus.mk
+	$(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D)/middleware/v3d -f V3DDriver.mk
+	$(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D)/middleware/platform/nexus -f platform_nexus.mk
 endef
 
 define BCM_ROCKFORD_INSTALL_TARGET_CMDS
-  $(INSTALL) $(BCM_NEXUS_DIR)/bin/libv3ddriver.so $(TARGET_DIR)/usr/lib/modules/libv3ddriver.so
-  $(INSTALL) $(BCM_NEXUS_DIR)/bin/libnxpl.so $(TARGET_DIR)/usr/lib/modules/libnxpl.so
+	$(INSTALL) -D $(BCM_NEXUS_DIR)/bin/libv3ddriver.so $(TARGET_DIR)/usr/lib/libv3ddriver.so
+	$(INSTALL) -D $(BCM_NEXUS_DIR)/bin/libnxpl.so $(TARGET_DIR)/usr/lib/libnxpl.so
 endef
 
 $(eval $(call GENTARGETS,package,bcm_rockford))
