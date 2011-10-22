99d13ae866c7f69d256f3ead2bc5656e9b7a19ab
Shawn Ledbetter <sledbetter@google.com>
2011-10-22 10:01:24 -0700

    Add Netflix library to buildroot.
    We add Netflix using the bcm_app package, since we're still relying on
    Broadcom's build system to generate an image.  We need to pass
    different flags to Netflix configure script and not override LD (they
    use g++ & ld directly via the CROSS_COMPILE variable to do the linking
    from what I could see.)
    
    We also the bcm_apps to the staging directory, as Netflix links
    against the DirectFB built in bcm_apps.
    
    Change-Id: Ia886983b0231b817055b19a2497c9b87e8ea1f2d

--- package/bcm_apps/bcm_apps.mk
+++ package/bcm_apps/bcm_apps.mk
@@ -2,6 +2,8 @@ BCM_APPS_SITE=repo://vendor/broadcom/AppLibs
 BCM_APPS_DEPENDENCIES=linux bcm_bseav bcm_nexus
 BCM_APPS_CONFIGURE_CMDS=ln -sf $(@D) $(BUILD_DIR)/AppLibs
 BCM_APPS_POST_EXTRACT_HOOKS=BCM_REMOVE_PATCH_REJECTS
+BCM_APPS_INSTALL_STAGING=YES
+BCM_APPS_INSTALL_TARGET=YES
 
 define BCM_REMOVE_PATCH_REJECTS
 	find $(@D) -name '*.rej' -exec rm \{\} \;
@@ -43,16 +45,28 @@ BCM_OBJS-$(BR2_PACKAGE_BCM_APP_ZLIB)        += zlib
 BCM_OBJS-$(BR2_PACKAGE_BCM_APP_LIBPNG)      += libpng
 BCM_OBJS-$(BR2_PACKAGE_BCM_APP_JPEG)        += jpeg
 
-
 include package/bcm_common/bcm_common.mk
 
+ifeq (y,$(BR2_PACKAGE_BCM_APP_NETFLIX))
+BCM_APPS_APPLIB_TARGETS += directfb
+BCM_APPS_DEPENDENCIES += openssl expat curl
+endif
+
 define BCM_APPS_BUILD_CMDS
-	$(BCM_MAKE_ENV) $(MAKE1) $(BCM_MAKEFLAGS) -C $(@D)/common $(BCM_APPS_APPLIB_TARGETS) bundle
+	$(BCM_MAKE_ENV) $(MAKE1) $(BCM_MAKEFLAGS) -C $(@D)/common $(BCM_APPS_APPLIB_TARGETS)
+	[ "y" == "$(BR2_PACKAGE_BCM_APP_NETFLIX)" ] && \
+	$(BCM_MAKE_ENV) NEXUS=$(BCM_NEXUS_DIR) $(MAKE1) $(NETFLIX_MAKEFLAGS) -C $(@D)/thirdparty/netflix/3.x all
 endef
 
 
 define BCM_APPS_INSTALL_TARGET_CMDS
+	$(BCM_MAKE_ENV) $(MAKE1) $(BCM_MAKEFLAGS) -C $(@D)/common bundle
 	$(TAR) -xf $(@D)/target/97425*.mipsel-linux*release.*tgz -C $(TARGET_DIR)
+endef
+
+define BCM_APPS_INSTALL_STAGING_CMDS
+	$(BCM_MAKE_ENV) $(MAKE1) $(BCM_MAKEFLAGS) -C $(@D)/common bundle
+	$(TAR) -xf $(@D)/target/97425*.mipsel-linux*release.*tgz -C $(STAGING_DIR)
 endef
 
 $(eval $(call GENTARGETS,package,bcm_apps))
