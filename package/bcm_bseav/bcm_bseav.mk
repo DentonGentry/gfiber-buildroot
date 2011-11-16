fb4f9f8fdb46a995b8a77ef62551a4ea2eef7a33
Shawn Ledbetter <sledbetter@google.com>
2011-11-15 17:49:17 -0800

    Reenable SSL using system SSL for Nexus.
    We also add explicit openssl dependency to BSEAV.
    
    Change-Id: I93191f430d4480f4f13b85ff7db945d907dbc2a6

--- package/bcm_bseav/bcm_bseav.mk
+++ package/bcm_bseav/bcm_bseav.mk
@@ -1,5 +1,5 @@
 BCM_BSEAV_SITE=repo://vendor/broadcom/BSEAV
-BCM_BSEAV_DEPENDENCIES=linux
+BCM_BSEAV_DEPENDENCIES=linux openssl
 BCM_BSEAV_CONFIGURE_CMDS=ln -sf $(@D) $(BUILD_DIR)/BSEAV
 
 include package/bcm_common/bcm_common.mk
