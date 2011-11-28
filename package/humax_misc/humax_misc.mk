7dd5aec08e57384fc05a43f7d28483030ab30520
Denton Gentry <dgentry@google.com>
2011-11-28 12:22:11 -0800

    Add libupgrade build to humax_misc.
    Change-Id: Ida96739f0e978d642c201b7c88a3b522c2906c75

--- package/humax_misc/humax_misc.mk
+++ package/humax_misc/humax_misc.mk
@@ -4,9 +4,16 @@ HUMAX_MISC_INSTALL_TARGET=YES
 
 HUMAX_MISC_STAGING_PATH=usr/lib/humax
 
+define HUMAX_MISC_BUILD_CMDS
+	TARGET=$(TARGET_CROSS) $(MAKE) -C $(@D)/libupgrade
+endef
+
 define HUMAX_MISC_INSTALL_STAGING_CMDS
 	mkdir -p $(STAGING_DIR)/$(HUMAX_MISC_STAGING_PATH) && \
 	$(INSTALL) -m 0755 $(@D)/makehdf $(STAGING_DIR)/$(HUMAX_MISC_STAGING_PATH)
+	$(INSTALL) -m 0755 $(@D)/libupgrade/libhmxupgrade.a $(STAGING_DIR)/usr/lib/libhmxupgrade.a
+	$(INSTALL) -m 0644 $(@D)/libupgrade/hmx_upgrade_flash.h $(STAGING_DIR)/usr/include/humax
+	$(INSTALL) -m 0644 $(@D)/libupgrade/hmx_upgrade_nvram.h $(STAGING_DIR)/usr/include/humax
 endef
 
 $(eval $(call GENTARGETS,package,humax_misc))
