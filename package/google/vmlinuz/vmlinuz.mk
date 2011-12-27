00cb1e0da26cbe7a197e89fd609a53677182d9de
Ke Dong <kedong@google.com>
2011-12-27 10:37:47 -0800

    Add initrd support in buildroot.
    Create python script to manage two stage build for initramfs.
    The one stage build will still be supported in the new build script.
    
    Change-Id: I65f591fce479e76810a69d250671e1961787267a

--- package/google/vmlinuz/vmlinuz.mk
+++ package/google/vmlinuz/vmlinuz.mk
@@ -5,8 +5,8 @@
 #############################################################
 GOOGLE_VMLINUZ_SITE=repo://vendor/google/platform
 GOOGLE_VMLINUZ_DEPENDENCIES=linux host-mtd
-GOOGLE_VMLINUZ_INSTALL_STAGING=NO
-GOOGLE_VMLINUZ_INSTALL_TARGET=YES
+GOOGLE_VMLINUZ_INSTALL_STAGING=YES
+GOOGLE_VMLINUZ_INSTALL_TARGET=NO
 
 UBI_UBINIZE_OPTS := -m $(BR2_TARGET_ROOTFS_UBIFS_MINIOSIZE)
 UBI_UBINIZE_OPTS += -p $(BR2_TARGET_ROOTFS_UBI_PEBSIZE)
@@ -14,12 +14,9 @@ ifneq ($(BR2_TARGET_ROOTFS_UBI_SUBSIZE),0)
  UBI_UBINIZE_OPTS += -s $(BR2_TARGET_ROOTFS_UBI_SUBSIZE)
 endif
 
-define GOOGLE_VMLINUZ_INSTALL_TARGET_CMDS
-	cp package/google/vmlinuz/kernel_ubinize.cfg ${BINARIES_DIR}/kernel_ubinize.cfg
-	(cd ${BINARIES_DIR} ; \
-	 gzip -9 < vmlinux > vmlinuz ; \
-	 $(HOST_DIR)/usr/sbin/ubinize -o vmlinuz.ubi $(UBI_UBINIZE_OPTS) kernel_ubinize.cfg)
-	rm ${BINARIES_DIR}/kernel_ubinize.cfg
+define GOOGLE_VMLINUZ_INSTALL_STAGING_CMDS
+	cp package/google/vmlinuz/kernel_ubinize.cfg $(STAGING_DIR)/etc/kernel_ubinize.cfg
+	echo $(UBI_UBINIZE_OPTS) > $(STAGING_DIR)/etc/kernel_ubi_opts
 endef
 
 $(eval $(call GENTARGETS,package/google,google_vmlinuz))
