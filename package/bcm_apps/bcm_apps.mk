545ddd7d7378ff8aae650b2a6b33ce4c3b9d35eb
Jean-Francois Thibert <jfthibert@google.com>
2012-01-04 15:36:44 -0500

    Remove unneeded packages from AppLibs
    Change-Id: I20e8716fd0354464d65cb3c8569c3caa5306749c

--- package/bcm_apps/bcm_apps.mk
+++ package/bcm_apps/bcm_apps.mk
@@ -12,11 +12,7 @@ endef
 BCM_APPS_APPLIB_TARGETS=$(BCM_OBJS-y)
 
 BCM_OBJS-$(BR2_PACKAGE_BCM_APP_REFSW)       += refsw
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_AVAHI)       += avahi
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_OSAPI)       += osapi
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_NETAPP)      += netapp
 BCM_OBJS-$(BR2_PACKAGE_BCM_APP_DLNA)        += dlna
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_DVB)         += dvb
 BCM_OBJS-$(BR2_PACKAGE_BCM_APP_DIRECTFB)    += directfb
 
 ifeq ($(BR2_PACKAGE_BCM_APP_DIRECTFB),y)
@@ -27,32 +23,12 @@ ifeq ($(BR2_PACKAGE_DIRECTFB),y)
 BCM_APPS_DEPENDENCIES += bcm_rockford libpng jpeg zlib freetype
 endif
 
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_STAGECRAFT)  += stagecraft
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_ARGO)        += argo
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_COVERFLOW3D) += coverflow3d
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_BMC)         += bmc
-
 ifeq ($(BR2_PACKAGE_BCM_APP_BROWSER),y)
 BCM_APPS_DEPENDENCIES += openssl expat libcurl libxml2 libxslt fontconfig sqlite pixman cairo
 BCM_OBJS-$(BR2_PACKAGE_BCM_APP_BROWSER)     += browser
 endif
 
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_GSTREAMER)   += gstreamer
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_ALSA)        += alsa
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_PLAYBACK_IP) += playback_ip
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_DTCP_IP)     += dtcp_ip
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_QT)          += qt
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_LIBCURL)     += libcurl
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_SQLITE)      += sqlite
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_LIBXML2)     += libxml2
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_PIXMAN)      += pixman
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_LIBXSLT)     += libxslt
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_FONTCONFIG)  += fontconfig
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_CAIRO)       += cairo
 BCM_OBJS-$(BR2_PACKAGE_BCM_APP_ICU)         += icu
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_ZLIB)        += zlib
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_LIBPNG)      += libpng
-BCM_OBJS-$(BR2_PACKAGE_BCM_APP_JPEG)        += jpeg
 
 include package/bcm_common/bcm_common.mk
 
