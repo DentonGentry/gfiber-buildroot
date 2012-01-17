a7e5a0f5228fbb8cd227ac118f7369603c78407a
Alice Wang <alicejwang@google.com>
2012-01-17 14:14:37 -0800

    Add fio debug command.
    Change-Id: I92322cd82783497af972898f6b5799efa0d7d255

---
+++ package/fio/fio.mk
@@ -0,0 +1,20 @@
+#############################################################
+#
+# fio library
+#
+#############################################################
+
+FIO_VERSION = fio-2.0.1
+FIO_SITE = git://git.kernel.dk/fio.git
+
+FIO_DEPENDENCIES = linux libaio
+
+define FIO_BUILD_CMDS
+  $(MAKE) CC="$(TARGET_CC)" -C $(@D) DESTDIR="$(TARGET_DIR)" all
+endef
+
+define FIO_INSTALL_TARGET_CMDS
+  $(MAKE) CC="$(TARGET_CC)" -C $(@D) DESTDIR="$(TARGET_DIR)" install
+endef
+
+$(eval $(call GENTARGETS,package,fio))
