fed9a6d5c3d99de6c231574666e837000f481857
Shawn Ledbetter <sledbetter@google.com>
2012-02-10 10:22:11 -0800

    Include sageserver, start from r/w partition if exists in test builds.
    Change-Id: Ic2ef7b2f36d35df43856acd00e3d8d926a65f92f

--- package/google/sageserver/google_sageserver.mk
+++ package/google/sageserver/google_sageserver.mk
@@ -1,18 +1,19 @@
-GOOGLE_SAGESERVER_VERSION = 20111108
+GOOGLE_SAGESERVER_VERSION = 20120207
 GOOGLE_SAGESERVER_SOURCE = sagetv_server-$(GOOGLE_SAGESERVER_VERSION).tar.gz
 GOOGLE_SAGESERVER_SITE = http://www.google.com
 GOOGLE_SAGESERVER_DEPENDENCIES =
 
-define GOOGLE_SAGESERVER_BUILD_CMDS
-        echo Using pre-built SageTV server package
-endef
-
+ifeq ($(BR2_PACKAGE_BRUNO_TEST),y)
 define GOOGLE_SAGESERVER_INSTALL_TARGET_CMDS
-        cp -drpf $(@D)/* $(TARGET_DIR)/app/sage/
-        @if [ ! -f $(TARGET_DIR)/etc/init.d/S95sageserver ]; then \
-          $(INSTALL) -m 0755 -D package/google/sageserver/S95sageserver $(TARGET_DIR)/etc/init.d/S95sageserver; \
-        fi
+	cp -drpf $(@D)/* $(TARGET_DIR)/app/sage/
+	$(INSTALL) -m 0755 -D package/google/sageserver/S95sageserver-test $(TARGET_DIR)/etc/init.d/S95sageserver
 endef
+else
+define GOOGLE_SAGESERVER_INSTALL_TARGET_CMDS
+	cp -drpf $(@D)/* $(TARGET_DIR)/app/sage/
+	$(INSTALL) -m 0755 -D package/google/sageserver/S95sageserver $(TARGET_DIR)/etc/init.d/S95sageserver
+endef
+endif
 
 # To prevent trying to download from buildroot.net
 ifeq ($(BR2_PACKAGE_GOOGLE_SAGESERVER),y)
