5335844d9f030ab1e1518c8d5ca1272eabc97449
Shawn Ledbetter <sledbetter@google.com>
2012-02-28 17:30:16 -0800

    Add iputils to BRUNO_TEST image.
    ping6 doesn't work yet -- our uclibc version is missing dn_comp.
    
    Change-Id: I1de4a98d0c09359931630bcefaae1a948ed3d968

---
+++ package/iputils/iputils.mk
@@ -0,0 +1,34 @@
+#############################################################
+#
+# iputils library
+#
+#############################################################
+
+IPUTILS_VERSION = s20101006
+IPUTILS_SOURCE = iputils-$(IPUTILS_VERSION).tar.bz2
+IPUTILS_SITE = http://www.skbuff.net/iputils
+
+IPUTILS_DEPENDENCIES = linux
+
+# ping6 needs patch for dn_comp
+IPUTILS_CMDS = ping tracepath tracepath6 traceroute6
+
+#IPUTILS_CMDS += arping
+#IPUTILS_DEPENDENCIES += libsysfs
+
+#IPUTILS_CMDS += ping6
+#IPUTILS_BUILD_FLAGS = VPATH="$(STAGING_DIR)/usr/lib"
+
+define IPUTILS_BUILD_CMDS
+	$(MAKE) $(IPUTILS_BUILD_FLAGS) CC="$(TARGET_CC)" -C $(@D) $(IPUTILS_CMDS)
+
+endef
+
+define IPUTILS_INSTALL_TARGET_CMDS
+	for cmd in $(IPUTILS_CMDS); do \
+		$(INSTALL) -m 0755 -D $(@D)/$$cmd $(TARGET_DIR)/usr/bin/; \
+	done
+endef
+
+
+$(eval $(call GENTARGETS,package,iputils))
