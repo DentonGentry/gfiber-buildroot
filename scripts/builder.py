43218510fa7461c9b56592142a7a10047054132b
Avery Pennarun <apenwarr@gmail.com>
2012-03-19 21:32:52 -0400

    builder.py: split init and app builds into two dirs.
    The previous version of the code just built everything in $OUTDIR.  First it
    built the kernel+initramfs, then it did a make clean (!), then it built the
    app squashfs using a separate configuration.  This meant that trying to
    rebuild the original kernel+initramfs would be virtually impossible without
    skipping builder.py and doing it all yourself.
    
    This splits the two builds into $OUTDIR/init and $OUTDIR/app, respectively.
    $OUTDIR/app/images/ contains what used to be $OUTDIR/images/.
    
    As part of this, I was able to simplify the builder.py code quite a bit
    without losing any functionality.
    
    Note: This change will make "official" builds (ie. builds from scratch) take
    somewhat longer, but should make day-to-day development faster and less
    upredictable, which seems like the right tradeoff.
    
    Change-Id: I566eb6052df5948f06edd1f07a761baad7729461

--- scripts/builder.py
+++ scripts/builder.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python
 # Copyright 2011 Google Inc. All Rights Reserved.
 
-"""Script for building Bruno images."""
+"""Script for building Bruno images.
 
+NOTE NOTE NOTE NOTE NOTE!
+
+Please consider carefully whether you really want to add stuff to this script.
+Almost everything you might want in this script should be added as a
+buildroot package instead.  Otherwise people who want to do a quick rebuild
+of a particular package using 'make' won't be able to do so.
+
+This script already does too much stuff.  Most of its existing features
+should be migrated out into packages eventually.  You almost certainly don't
+want to add more.
+
+Think of the goal for this script as running ./configure in an autoconf
+program.  Everybody needs to do it to set their options, but not every
+time you run an incremental build.
+"""
+
+import errno
+import glob
 import gzip
 import os
-import shutil
+import re
 import subprocess
 import sys
 import tarfile
 import time
 import options
-import re
 
 __author__ = 'kedong@google.com (Ke Dong)'
 
@@ -23,12 +40,13 @@ p,product-family=  Product family (eg. bruno, bcm7425) [bruno]
 m,model=           Model name [gfhd100]
 c,chip-revision=   Chip revision [b0]
 v,verbose          Increase verbosity
-n,no-initramfs     Build kernel without initramfs
-d,debug,debug-image  Build debug image with extra debugging features
 b,bundle-only      Only bundle the image
-f,fresh            Build a fresh image (ie. build all from scratch)
-x,platform-only    Build only base platform (no webkit, netflix, etc.)
+f,fresh,force      Force rebuild (once=remove stamps, twice=make clean)
+x,platform-only    Build less stuff into the app (no webkit, netflix, etc.)
+no-init            Don't build the kernel+initramfs
+no-app             Don't build the app squashfs
 t,test,test-packages Add test packages
+d,debug,debug-image  Build debug image with extra debugging features
 """
 
 
@@ -36,6 +54,11 @@ RED = '\033[1;31m'
 GREEN = '\033[1;32m'
 YELLOW = '\033[1;33m'
 OFF = '\033[0m'
+
+
+# Possible values for init_or_app
+INIT = 'init'  # the kernel + initramfs
+APP = 'app'    # the application-layer squashfs
 
 
 def _Log(color, fmt, args):
@@ -57,14 +80,56 @@ def Error(fmt, *args):
   _Log(RED, fmt, args)
 
 
+class SubprocError(Exception):
+  pass
+
+
+def PopenAndRead(args, **_kwargs):
+  kwargs = dict(stdout=subprocess.PIPE)
+  kwargs.update(_kwargs)
+  p = subprocess.Popen(args, **kwargs)
+  data = p.stdout.read()
+  retval = p.wait()
+  if retval:
+    raise SubprocError('%r returned exit code %d' % (args, retval))
+  return data.strip()
+
+
 class BuildError(Exception):
   pass
+
+
+def Makedirs(dirname):
+  try:
+    os.makedirs(dirname)
+  except OSError, e:
+    if e.errno == errno.EEXIST:
+      pass
+    else:
+      raise
+
+
+def Unlink(filename):
+  try:
+    os.unlink(filename)
+  except OSError, e:
+    if e.errno == errno.ENOENT:
+      pass
+    else:
+      raise
+
+
+def UnlinkGlob(g):
+  for filename in glob.glob(g):
+    Unlink(filename)
 
 
 class BuildRootBuilder(object):
   """Builder class to wrap up buildroot."""
 
-  def __init__(self, opt):
+  def __init__(self, base_dir, opt):
+    self.top_dir = os.path.abspath('.')
+    self.base_dir = base_dir
     self.opt = opt
 
   def Build(self):
@@ -72,18 +137,18 @@ class BuildRootBuilder(object):
     starttime = time.time()
     try:
       self.PrintOptions()
-      if self.opt.fresh:
-        self.CleanBuildOutputDir()
+      Makedirs(self._Path(INIT, 'images'))
+      Makedirs(self._Path(APP, 'images'))
       if not self.opt.bundle_only:
-        self.BuildInitramfs()
-        self.BuildRootfs()
+        if self.opt.init:
+          self.BuildInitFs()
+        if self.opt.app:
+          self.BuildAppFs()
       self.BundleImage()
-    except BuildError as e:
-      #TODO(apenwarr): should return nonzero exit code back to shell
-      Error(e)
-    endtime = time.time()
-    elapsed = endtime - starttime
-    Info('Time executed: %d:%02d', elapsed / 60, elapsed % 60)
+    finally:
+      endtime = time.time()
+      elapsed = endtime - starttime
+      Info('Time executed: %d:%02d', elapsed / 60, elapsed % 60)
 
   def _LogStart(self, info):
     Info('##### Start %s', info)
@@ -97,205 +162,203 @@ class BuildRootBuilder(object):
     print 'CHIP REVISION  :', self.opt.chip_revision
     print 'DEBUG          :', self.opt.debug
     print 'TEST_IMAGE     :', self.opt.test_packages
-    print 'INITRAMFS      :', self.opt.initramfs
     print 'MODEL          :', self.opt.model
     print 'PRODUCT FAMILY :', self.opt.product_family
     print 'VERBOSE        :', self.opt.verbose
     print 'FRESH          :', self.opt.fresh
-    print 'BUILDROOT PATH :', self.opt.top_dir
-    print 'BUILD PATH     :', self.opt.base_dir
+    print 'BUILDROOT PATH :', self.top_dir
+    print 'BUILD PATH     :', self.base_dir
     print '=========================================================='
 
-  def PopenDir(self, cwd, args, **kwargs):
+  def PopenAt(self, cwd, args, **kwargs):
     """Execute Popen in arbitrary dir."""
     p = subprocess.Popen(args, cwd=cwd, **kwargs)
-    if p.wait():
-      raise BuildError('Failed to execute %r' % args)
+    retval = p.wait()
+    if retval:
+      raise BuildError('%r returned exit code %r' % (args, retval))
 
-  def Make(self, make_options):
+  def _Path(self, init_or_app, *paths):
+    return os.path.abspath(os.path.join(self.base_dir, init_or_app, *paths))
+
+  def Make(self, init_or_app, targets):
     """Execute make for buildroot.
 
     Args:
-      make_options: list of strings to pass to make
+      init_or_app: either INIT or APP
+      targets: which targets to ask make to build ([] means default)
     """
-    cmd = ['make', 'O=%s' % self.opt.base_dir] + make_options
-    self.PopenDir(self.opt.top_dir, cmd)
+    #TODO(apenwarr): put BHCP_VER elsewhere so 'make' finds it, even if
+    #  further builds are not done with this script.
+    cmd = ['make', 'O=%s' % self._Path(init_or_app),
+           'BHCP_VER=%s' % self.opt.chip_revision.upper()] + targets
+    if self.opt.verbose:
+      cmd += ['V=1']
+    self.PopenAt(self.top_dir, cmd)
 
-  def CleanBuildOutputDir(self):
-    Info('Cleaning %r...', self.opt.base_dir)
-    self.Make(['clean'])
-    if os.path.exists(os.path.join(self.opt.base_dir, '.localconfig')):
-      os.unlink(os.path.join(self.opt.base_dir, '.localconfig'))
+  def CleanOutputDir(self, init_or_app):
+    d = self._Path(init_or_app)
+    Info('Cleaning %r...', d)
+    self.Make(init_or_app, ['clean'])
 
-  def BuildConfig(self, config_file):
-    """Generate a config file for the current set of options."""
-    if self.opt.debug:
-      localcfg = file(os.path.join(self.opt.base_dir, '.localconfig'), 'a')
-      # TODO(kedong): when the loader can handle more than 40M kernel,
-      # we will add the strip_none back.
-      # localcfg.write('BR2_STRIP_none=y\n')
-      localcfg.write('BR2_PACKAGE_BRUNO_DEBUG=y\n')
-      localcfg.close()
-    Info('Final config file: %r...', config_file)
-    self.Make([config_file + '_rebuild'])
-    # Grab all the sources we need for this config
-    self.Make(['source'])
+  def BuildConfig(self, init_or_app, filename, **extra):
+    """Generate a config file for the given set of options."""
+    opts = dict(BR2_PACKAGE_BRUNO_DEBUG=self.opt.debug)
+    # TODO(kedong): when the loader can handle more than 40M kernel,
+    #   we will add the strip_none back.
+    # opts['BR2_STRIP_none'] = 1
+    opts.update(extra)
 
-  def ForceCleanPackages(self):
-    self._LogStart('Force cleaning packages')
-    packages = ['bruno', 'util-linux']
-    if os.path.exists(os.path.join(self.opt.top_dir, '.forceclean')):
-      packages.extend(file(os.path.join(self.opt.top_dir, '.forceclean'))
-                      .readlines())
-    packages = [p.strip() for p in packages]
-    Warn('Cleaning packages: %s', ' '.join(packages))
-    self.Make(['%s-dirclean' % p for p in packages])
-    self._LogDone('Completed cleaning packages')
+    # We append to the file because the user might have added (unrelated)
+    # custom entries, but later lines override earlier ones, so it's
+    # safe to just re-append forever.
+    localcfg = open(self._Path(init_or_app, '.localconfig'), 'a')
+    for key, value in opts.iteritems():
+      localcfg.write('%s=%s\n' % (key, value and 'y' or 'n'))
+    localcfg.close()
 
-  def BuildInitramfs(self):
-    """Build the initramfs (that gets linked into the kernel)."""
-    if not self.opt.initramfs:
-      return
-    self._LogStart('Building Initramfs')
+    # Actually generate the config file
+    Info('Config file: %r', filename)
+    fullpath = os.path.join(self.top_dir, 'configs', filename)
+    dotconfig = self._Path(init_or_app, '.config')
+    self.Make(init_or_app, [filename + '_rebuild'])
+
+    # Grab all the sources before starting, so we fail faster
+    self.Make(init_or_app, ['source'])
+
+  def RemoveStamps(self, init_or_app):
+    Info('Cleaning up install stamps...')
+    Unlink(self._Path(init_or_app, 'build/.root'))
+    UnlinkGlob(self._Path(init_or_app, 'build/*/.stamp*installed'))
+    UnlinkGlob(self._Path(init_or_app, 'stamps/*installed'))
+
+  def BuildInitFs(self):
+    """Build the kernel + initramfs."""
+    if self.opt.fresh >= 2:
+      self.CleanOutputDir(INIT)
+    self._LogStart('Building kernel + initramfs')
     config_file = ('%s_initramfs_%s%s_defconfig'
                    % (self.opt.product_family,
                       self.opt.model,
                       self.opt.chip_revision))
-    Info('Use config file %r for initramfs.', config_file)
-    self.BuildConfig(config_file)
-    # clean up target
-    build_dir = os.path.join(self.opt.base_dir, 'build')
-    target_dir = os.path.join(self.opt.base_dir, 'target')
-    stamp_dir = os.path.join(self.opt.base_dir, 'stamps')
-    self.ForceCleanPackages()
-    if os.path.exists(target_dir):
-      Info('Removing target directory...')
-      shutil.rmtree(target_dir)
-    if os.path.exists(build_dir):
-      Info('Cleaning up install stamps...')
-      rootfile = os.path.join(build_dir, '.root')
-      if os.path.exists(rootfile):
-        os.remove(rootfile)
-      cmd = 'find ' + build_dir + ' -name .stamp*installed -exec rm {} \;'
-      self.PopenDir(self.opt.top_dir, cmd, shell=True)
-      if os.path.exists(stamp_dir):
-        cmd = 'find ' + stamp_dir + ' -name *installed -exec rm {} \;'
-        self.PopenDir(self.opt.top_dir, cmd, shell=True)
-    args = []
-    args.append('BCHP_VER='+self.opt.chip_revision.upper())
-    if self.opt.verbose:
-      args.append('V=1')
-    self.Make(args)
-    vmlinux = os.path.join(self.opt.base_dir, 'images/vmlinux')
-    # save vmlinux to vmlinux-initramfs to prevent overwrite
-    shutil.copyfile(vmlinux, vmlinux + '-initramfs')
-    self._LogDone('Building Initramfs')
+    Info('kernel + initramfs: config file is %r', config_file)
+    self.BuildConfig(INIT, config_file)
+    if self.opt.fresh >= 1:
+      self.RemoveStamps(INIT)
+    self.Make(INIT, [])
+    self._LogDone('Building kernel + initramfs')
 
-  def BuildRootfs(self):
-    """Build the root filesystem (ie. the squashfs, not the initramfs)."""
-    self._LogStart('Building Rootfs')
+  def BuildAppFs(self):
+    """Build the squashfs (runs after the initramfs and contains the app)."""
+    if self.opt.fresh >= 2:
+      self.CleanOutputDir(APP)
+    self._LogStart('Building app squashfs')
     config_file = ('%s_%s%s_defconfig'
                    % (self.opt.product_family,
                       self.opt.model,
                       self.opt.chip_revision))
-    # Add packages if not platform build:
-    Info('Use config file %r for rootfs.', config_file)
-    localcfg = file(os.path.join(self.opt.base_dir, '.localconfig'), 'a')
-    if self.opt.platform_only:
-      Info('Building platform-only image.')
-      localcfg.write('BR2_PACKAGE_BRUNO_APPS=n\n')
-    else:
-      Info('Building full image.')
-      localcfg.write('BR2_PACKAGE_BRUNO_APPS=y\n')
-    if self.opt.test_packages:
-      Info('Adding test packages')
-      localcfg.write('BR2_PACKAGE_BRUNO_TEST=y\n')
-    localcfg.close()
-    self.BuildConfig(config_file)
-    args = []
-    args.append('BCHP_VER='+self.opt.chip_revision.upper())
-    if self.opt.verbose:
-      args.append('V=1')
-    self.ForceCleanPackages()
-    self.Make(args)
-    self._LogDone('Building Rootfs')
+    Info('app squashfs: config file is %r', config_file)
+    self.BuildConfig(APP, config_file,
+                     BR2_PACKAGE_BRUNO_APPS=not self.opt.platform_only,
+                     BR2_PACKAGE_BRUNO_TEST=self.opt.test_packages)
+    if self.opt.fresh >= 1:
+      self.RemoveStamps(APP)
+    self.Make(APP, [])
+    self._LogDone('Building app squashfs')
 
+  #TODO(apenwarr): postprocessing like this belongs in a buildroot package.
+  #  Generating vmlinuz, tarballs, etc are just more build rules like any
+  #  other. That way you could cd $OUTDIR and just run 'make' and the right
+  #  thing will happen, as long as you've run this script once for setup.
   def BundleImage(self):
     """Create UBI images and installer tarball."""
     self._LogStart('Bundling Image')
-    if self.opt.product_family == 'bruno':
-      binaries_dir = os.path.abspath(self.opt.base_dir + '/images')
-      staging_dir = os.path.abspath(self.opt.base_dir + '/staging')
-      host_dir = os.path.abspath(self.opt.base_dir + '/host')
-      opt_file = open(staging_dir + '/etc/kernel_ubi_opts')
-      ubi_ubinize_opts = ' '.join(opt_file.read().strip().split())
-      opt_file.close()
-      # gzip vmlinux
+
+    # gzip vmlinux
+    if self.opt.init:
       Info('Creating vmlinuz...')
-      vmlinux_path = binaries_dir + '/vmlinux'
-      if self.opt.initramfs:
-        vmlinux_path += '-initramfs'
-      if not os.path.exists(vmlinux_path):
-        raise BuildError('Cannot find %r' % vmlinux_path)
-      vmlinux = open(vmlinux_path, 'rb')
-      vmlinuz = gzip.open(binaries_dir + '/vmlinuz', 'wb', 9)
-      vmlinuz.write(vmlinux.read())
+      vmlinux = open(self._Path(INIT, 'images/vmlinux'), 'rb').read()
+      vmlinuz = gzip.open(self._Path(INIT, 'images/vmlinuz'), 'wb', 9)
+      vmlinuz.write(vmlinux)
       vmlinuz.close()
-      vmlinux.close()
-      # ubinize vmlinuz
+
+    # ubinize vmlinuz
+    if self.opt.init:
       Info('Creating ubi image for vmlinuz...')
-      cmd = ('%s/usr/sbin/ubinize -o %s/vmlinuz.ubi %s '
-             '%s/etc/kernel_ubinize.cfg'
-             % (host_dir, binaries_dir, ubi_ubinize_opts, staging_dir))
-      self.PopenDir(binaries_dir, cmd, shell=True)
-      # bundle image in tar format
-      Info('Creating final tgz image...')
-      tar = tarfile.open(binaries_dir + '/bruno_ginstall_image.tgz', 'w:gz')
-      tar.add(binaries_dir + '/vmlinuz', 'vmlinuz')
-      tar.add(binaries_dir + '/rootfs.squashfs_ubi', 'rootfs.squashfs_ubi')
-      tar.add(binaries_dir + '/version', 'version')
+      ubinize_opts = (open(self._Path(INIT, 'staging/etc/kernel_ubi_opts'))
+                      .read().strip().split())
+      cmd = ([self._Path(INIT, 'host/usr/sbin/ubinize'),
+              '-o', self._Path(INIT, 'images/vmlinuz.ubi')]
+             + ubinize_opts +
+             [self._Path(INIT, 'staging/etc/kernel_ubinize.cfg')])
+      self.PopenAt(self._Path(INIT, 'images'), cmd)
+
+    # For user friendliness, show the kernel from the initfs in app/images.
+    if self.opt.app and self.opt.init:
+      Unlink(self._Path(APP, 'images/vmlinuz'))
+      # I might prefer a symlink here, but then 'tar' would do the wrong thing
+      os.link(self._Path(INIT, 'images/vmlinuz'),
+              self._Path(APP, 'images/vmlinuz'))
+
+    # bundle complete package in tar format
+    if self.opt.app:
+      tarname = self._Path(APP, 'images/bruno_ginstall_image.tgz')
+      Info('Creating %r', tarname)
+      tar = tarfile.open(tarname, 'w:gz')
+      tar.add(self._Path(APP, 'images/version'), 'version')
+      tar.add(self._Path(APP, 'images/vmlinuz'), 'vmlinuz')
+      tar.add(self._Path(APP, 'images/rootfs.squashfs_ubi'),
+              'rootfs.squashfs_ubi')
       tar.close()
+
     self._LogDone('Bundling Image')
 
 
+def LoasTimeLeft():
+  try:
+    loasstat = PopenAndRead(['prodcertstatus', '--check_loas'])
+  except SubprocError:
+    pass
+  else:
+    g = re.match(r'LOAS cert expires in (\d+):(\d+)', loasstat)
+    if g:
+      return int(g.group(1))
+  # otherwise returns None: no valid certificate
+
+
+def CheckLoasCertificate():
+  loas_time_left = LoasTimeLeft()
+  if not loas_time_left:
+    Error('Your LOAS certificate has expired.\n'
+          'Please use prodaccess to renew your LOAS certificate.\n')
+    sys.exit(1)
+  elif loas_time_left < 2:
+    Error('Your LOAS certificate is only good for less than 2 hours.\n'
+          'Please use prodaccess to renew your LOAS certificate.\n')
+    sys.exit(1)
+
+
 def main():
+  os.chdir(os.path.dirname(sys.argv[0]) + '/..')
   o = options.Options(optspec)
   (opt, _, extra) = o.parse(sys.argv[1:])
   if extra:
     if len(extra) > 1:
-      o.fatal('at most one output-directory expected')
-    opt.base_dir = os.path.abspath(extra[0])
+      o.fatal('at most one output directory expected')
+    base_dir = os.path.abspath(extra[0])
   else:
     if opt.debug:
       debug_path = 'debug'
     else:
       debug_path = 'release'
-    gitpwd = subprocess.Popen('git rev-parse --abbrev-ref HEAD'.split(),
-                              stdout=subprocess.PIPE).communicate()[0].strip()
-    opt.base_dir = os.path.abspath(os.path.join('..', 'builds',
-                                                gitpwd, debug_path))
-    Warn('Using default build directory of buildroot branch: %s',
-         opt.base_dir)
-  if not os.path.exists(opt.base_dir):
-    Warn('Build directory %r does not exist; creating...', opt.base_dir)
-    os.makedirs(opt.base_dir)
+    branch = PopenAndRead(['git', 'rev-parse', '--abbrev-ref', 'HEAD'])
+    base_dir = os.path.abspath(os.path.join('../builds', branch, debug_path))
+    Warn('Default output dir: %s', base_dir)
   if not opt.platform_only:
-    loasstat = subprocess.Popen(
-        'prodcertstatus --check_loas'.split(),
-        stdout=subprocess.PIPE).communicate()[0].strip()
-    left = re.match(r'(LOAS cert expires in (\d+):(\d+))', loasstat)
-    if left and left.group(2):
-      if int(left.group(2)) < 2:
-          print 'Your LOAS certificate is only good for less than 2 hours.'
-          print 'Please use prodaccess to renew your LOAS cert.'
-          sys.exit(-1)
-    else:
-      print 'Your LOAS certificate has expired.'
-      print 'Please use prodaccess to renew your LOAS cert.'
-      sys.exit(-1)
-
-  opt.top_dir = os.path.abspath(os.path.dirname(sys.argv[0]) + '/..')
-  builder = BuildRootBuilder(opt)
+    #TODO(apenwarr): put this in the buildroot packages as a "pre-depends"
+    #  or something (so it runs early, but only when needed, and doesn't
+    #  depend on this script).
+    CheckLoasCertificate()
+  builder = BuildRootBuilder(base_dir, opt)
   builder.Build()
 
 
