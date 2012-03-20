5130224ec4d8741fa9ef1e293ced08d7165d661f
Shawn Ledbetter <sledbetter@google.com>
2012-03-19 18:28:55 -0700

    Add zsh package
    Change-Id: I687b665fddffe35abac8c5c49381260d03576899

---
+++ package/zsh/zsh.mk
@@ -0,0 +1,9 @@
+#
+# ZSH
+#
+
+ZSH_VERSION=4.3.17
+ZSH_SOURCE=zsh-$(ZSH_VERSION).tar.bz2
+ZSH_SITE=http://sourceforge.net/projects/zsh/files/zsh-dev/$(ZSH_VERSION)/$(ZSH_SOURCE)/download
+
+$(eval $(call AUTOTARGETS, package, zsh))
