9ca2bb0e3f0fce1742e41295223f75203b376186
Shawn Ledbetter <sledbetter@google.com>
2012-03-21 11:02:52 -0700

    Rearrange bruno package requirements
    Change-Id: Id1f3d7559db33afcc3e16c8bbb57887d9def35f1

--- package/cairo/cairo.mk
+++ package/cairo/cairo.mk
@@ -10,11 +10,25 @@ CAIRO_AUTORECONF = NO
 CAIRO_INSTALL_STAGING = YES
 CAIRO_INSTALL_TARGET = YES
 
+ifeq ($(BR2_PACKAGE_BRUNO),y)
+
+ifeq ($(BR2_PACKAGE_DIRECTFB),y)
+CAIRO_CONF_ENV += directfb_CFLAGS="-I$(BCM_APPS_DIR)/opensource/directfb/bin/DirectFB-1.4.15_$(BCM_APPS_BUILD_TYPE)_build.97425$(BCHP_VER)/usr/local/include/directfb"
+CAIRO_CONF_ENV += directfb_LIBS="-L$(STAGING_DIR)/staging/usr/local/lib -ldirect -ldirectfb -lfusion"
+
 define CAIRO_DIRECTFB_COPY_FILES
 	cp $(CAIRO_DIR_PREFIX)/$(RAWNAME)/cairo-directfb/* $(@D)/src
 endef
 
 CAIRO_POST_PATCH_HOOKS += CAIRO_DIRECTFB_COPY_FILES
+endif
+
+define CAIRO_REMOVE_TRACE
+	rm -f $(TARGET_DIR)/usr/bin/cairo-trace
+endef
+
+CAIRO_POST_INSTALL_TARGET_HOOKS += CAIRO_REMOVE_TRACE
+endif
 
 CAIRO_CONF_ENV = ac_cv_func_posix_getpwuid_r=yes glib_cv_stack_grows=no \
 		glib_cv_uscore=no ac_cv_func_strtod=yes \
@@ -44,19 +58,10 @@ CAIRO_CONF_ENV = ac_cv_func_posix_getpwuid_r=yes glib_cv_stack_grows=no \
 
 CAIRO_DEPENDENCIES = host-pkg-config fontconfig pixman
 
-ifeq ($(BR2_PACKAGE_BRUNO),y)
-	CAIRO_POST_INSTALL_TARGET_HOOKS += CAIRO_REMOVE_TRACE
-endif
-
-define CAIRO_REMOVE_TRACE
-	rm -f $(TARGET_DIR)/usr/bin/cairo-trace
-endef
 
 ifeq ($(BR2_PACKAGE_DIRECTFB),y)
 	CAIRO_CONF_OPT += --enable-directfb
 	CAIRO_DEPENDENCIES += directfb
-	CAIRO_CONF_ENV += directfb_CFLAGS="-I$(BCM_APPS_DIR)/opensource/directfb/bin/DirectFB-1.4.15_$(BCM_APPS_BUILD_TYPE)_build.97425$(BCHP_VER)/usr/local/include/directfb"
-	CAIRO_CONF_ENV += directfb_LIBS="-L$(STAGING_DIR)/staging/usr/local/lib -ldirect -ldirectfb -lfusion"
 else
 	CAIRO_CONF_OPT += --disable-directfb
 endif
