457ed70da283cfdc20717ff7eee87c615022d654
Avery Pennarun <apenwarr@gmail.com>
2012-04-02 03:32:14 -0400

    sysklogd: install in /sbin to override busybox's syslogd.
    Otherwise we have /sbin/syslogd and /usr/sbin/syslogd and /sbin (busybox)
    wins.
    
    Change-Id: I95e493ded5384c747177be8f7374bf67668f1267

--- package/sysklogd/sysklogd.mk
+++ package/sysklogd/sysklogd.mk
@@ -26,8 +26,8 @@ define SYSKLOGD_BUILD_CMDS
 endef
 
 define SYSKLOGD_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0500 $(@D)/syslogd $(TARGET_DIR)/usr/sbin/syslogd
-	$(INSTALL) -D -m 0500 $(@D)/klogd $(TARGET_DIR)/usr/sbin/klogd
+	$(INSTALL) -D -m 0500 $(@D)/syslogd $(TARGET_DIR)/sbin/syslogd
+	$(INSTALL) -D -m 0500 $(@D)/klogd $(TARGET_DIR)/sbin/klogd
 	$(INSTALL) -D -m 0644 $(@D)/sysklogd.8 $(TARGET_DIR)/usr/share/man/man8/sysklogd.8
 	$(INSTALL) -D -m 0644 $(@D)/syslogd.8 $(TARGET_DIR)/usr/share/man/man8/syslogd.8
 	$(INSTALL) -D -m 0644 $(@D)/syslog.conf.5 $(TARGET_DIR)/usr/share/man/man5/syslog.conf.5
@@ -39,8 +39,8 @@ define SYSKLOGD_INSTALL_TARGET_CMDS
 endef
 
 define SYSKLOGD_UNINSTALL_TARGET_CMDS
-	rm -f $(TARGET_DIR)/usr/sbin/syslogd
-	rm -f $(TARGET_DIR)/usr/sbin/klogd
+	rm -f $(TARGET_DIR)/sbin/syslogd
+	rm -f $(TARGET_DIR)/sbin/klogd
 	rm -f $(TARGET_DIR)/usr/share/man/man8/sysklogd.8
 	rm -f $(TARGET_DIR)/usr/share/man/man8/syslogd.8
 	rm -f $(TARGET_DIR)/usr/share/man/man5/syslog.conf.5
