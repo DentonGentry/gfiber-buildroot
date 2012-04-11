52c25fd4b388f5a423a7aed3b182475e1c815fa9
Ke Dong <kedong@google.com>
2012-04-11 11:30:37 -0700

    Add binverify to image.
    Change-Id: I3a8a0ce98abc66e71ebb0229488cc6aab789f03b

--- package/bcm_nexus/bcm_nexus.mk
+++ package/bcm_nexus/bcm_nexus.mk
@@ -14,7 +14,7 @@ define BCM_NEXUS_BUILD_CMDS
 	$(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D)/build all
 	$(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D)/examples pkg-config
 	$(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D)/utils all
-	$(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D)/examples apps
+	$(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D)/examples apps install
 	$(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D)/lib/os
 	cd $(@D)/../BSEAV/lib/playbackdevice && $(BCM_MAKE_ENV) NEXUS=$(BCM_NEXUS_DIR) NEXUS_MGR_DIR=$(@D)/../BSEAV/lib/playbackdevice/nexusMgr/ $(MAKE) $(BCM_MAKEFLAGS) all
 endef
@@ -34,6 +34,9 @@ endef
 define BCM_NEXUS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 644 -D $(@D)/bin/bcmdriver.ko $(TARGET_DIR)/usr/lib/modules/bcmdriver.ko
 	$(INSTALL) -D $(@D)/bin/libnexus.so $(TARGET_DIR)/usr/lib/libnexus.so
+	$(INSTALL) -m 755 -D $(@D)/bin/binverify $(TARGET_DIR)/home/test/binverify
+	$(INSTALL) -m 755 -D $(@D)/bin/otpmsptest $(TARGET_DIR)/home/test/otpmsptest
+	$(INSTALL) -m 644 -D $(@D)/examples/UserCmdTest/rsa_init_SHA1_1024k2u31_1024k1u31_1024k0u31_1024k3u0.dat $(TARGET_DIR)/home/test/rsa_init_SHA1_1024k2u31_1024k1u31_1024k0u31_1024k3u0.dat
 	$(INSTALL) -D $(BCM_NEXUS_SECURITY_LIB).so $(TARGET_DIR)/usr/lib/libnexus_security.so
 	$(INSTALL) -D $(@D)/../BSEAV/lib/playbackdevice/bin/libnexusMgr.so $(TARGET_DIR)/usr/lib/libnexusMgr.so
 	$(INSTALL) -D $(@D)/../BSEAV/lib/playbackdevice/bin/libPlaybackDevice.so $(TARGET_DIR)/usr/lib/libPlaybackDevice.so
