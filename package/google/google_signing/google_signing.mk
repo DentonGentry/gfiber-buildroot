c0a5255d0019f1bb71a38d41c8871ccd1e26904f
Ke Dong <kedong@google.com>
2012-04-29 23:58:38 -0700

    Add signing package.
    Change-Id: I5da4272e7287cc4757a603d18a05a4c590c8256f

---
+++ package/google/google_signing/google_signing.mk
@@ -0,0 +1,17 @@
+#############################################################
+#
+# google_signing (signing related code)
+#
+#############################################################
+GOOGLE_SIGNING_SITE=repo://vendor/google/platform
+GOOGLE_SIGNING_INSTALL_TARGET=YES
+
+define GOOGLE_SIGNING_BUILD_CMDS
+	CROSS_COMPILE=$(TARGET_CROSS) $(MAKE) -C $(@D)/bruno/signing
+endef
+
+define GOOGLE_SIGNING_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/bruno/signing/readverity $(TARGET_DIR)/usr/sbin/
+endef
+
+$(eval $(call GENTARGETS_NEW))
