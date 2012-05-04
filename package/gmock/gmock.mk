e3e496c1197786df398fb82133888e598abbf852
Ke Dong <kedong@google.com>
2012-05-04 09:56:06 -0700

    Add google mock and test packages.
    Change-Id: Ia377a6189bf62d331bb71c69e3993766a8cb3893

---
+++ package/gmock/gmock.mk
@@ -0,0 +1,28 @@
+############################################################
+#
+# gmock
+#
+############################################################
+
+GMOCK_SITE = http://googlemock.googlecode.com/svn/trunk
+GMOCK_SITE_METHOD = svn
+# Release 1.6.0
+GMOCK_VERSION = 386
+
+define HOST_GMOCK_BUILD_CMDS
+	CC="$(HOSTCC)" CXX="$(HOSTCXX)" CFLAGS="$(HOST_CFLAGS)" \
+	   CXXFLAGS="$(HOST_CXXFLAGS)" LDFLAGS="$(HOST_LDFLAGS)" \
+	   $(MAKE) -C $(@D)/make
+endef
+
+define HOST_GMOCK_INSTALL_CMDS
+	ln -sf $(@D)/include/gmock $(HOST_DIR)/usr/include/
+	$(INSTALL) -D $(@D)/make/gmock_main.a $(HOST_DIR)/usr/lib/libgmock.a
+endef
+
+define HOST_GMOCK_UNINSTALL_CMDS
+	rm -f $(HOST_DIR)/usr/include/gmock
+	rm -f $(HOST_DIR)/usr/lib/libgmock.a
+endef
+
+$(eval $(call GENTARGETS_NEW,host))
---
+++ package/gtest/gtest.mk
@@ -0,0 +1,28 @@
+############################################################
+#
+# gtest
+#
+############################################################
+
+GTEST_SITE = http://googletest.googlecode.com/svn/trunk
+GTEST_SITE_METHOD = svn
+# Release 1.6.0
+GTEST_VERSION = 573
+
+define HOST_GTEST_BUILD_CMDS
+	CC="$(HOSTCC)" CXX="$(HOSTCXX)" CFLAGS="$(HOST_CFLAGS)" \
+	   CXXFLAGS="$(HOST_CXXFLAGS)" LDFLAGS="$(HOST_LDFLAGS)" \
+	   $(MAKE) -C $(@D)/make
+endef
+
+define HOST_GTEST_INSTALL_CMDS
+	ln -sf $(@D)/include/gtest $(HOST_DIR)/usr/include/
+	$(INSTALL) -D $(@D)/make/gtest_main.a $(HOST_DIR)/usr/lib/libgtest.a
+endef
+
+define HOST_GTEST_UNINSTALL_CMDS
+	rm -f $(HOST_DIR)/usr/include/gtest
+	rm -f $(HOST_DIR)/usr/lib/libgtest.a
+endef
+
+$(eval $(call GENTARGETS_NEW,host))
