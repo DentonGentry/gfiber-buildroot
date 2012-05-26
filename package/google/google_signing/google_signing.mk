e22e999254cf9f898f5ac91be4d068b3d813538d
Ke Dong <kedong@google.com>
2012-05-26 03:12:24 -0700

    Add test for readverity.
    Change-Id: Ic4837decfb829506ccdfc575bae1c4c4305fd9c7

--- package/google/google_signing/google_signing.mk
+++ package/google/google_signing/google_signing.mk
@@ -4,15 +4,23 @@
 #
 #############################################################
 GOOGLE_SIGNING_SITE=repo://vendor/google/platform
+GOOGLE_SIGNING_DEPENDENCIES=host-gtest
 GOOGLE_SIGNING_INSTALL_TARGET=YES
+GOOGLE_SIGNING_TEST=YES
+HOST_GOOGLE_SIGNING_TEST=YES
 
 define GOOGLE_SIGNING_BUILD_CMDS
-	CROSS_COMPILE=$(TARGET_CROSS) $(MAKE) -C $(@D)/signing
+	HOST_DIR=$(HOST_DIR) CROSS_COMPILE=$(TARGET_CROSS) $(MAKE) -C \
+		    $(@D)/signing
 endef
 
 define GOOGLE_SIGNING_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/sbin/
 	$(INSTALL) -D -m 0755 $(@D)/signing/readverity $(TARGET_DIR)/usr/sbin/
+endef
+
+define GOOGLE_SIGNING_TEST_CMDS
+	(cd $(@D)/signing; ./readverity_test)
 endef
 
 define HOST_GOOGLE_SIGNING_INSTALL_CMDS
@@ -23,7 +31,7 @@ endef
 # TODO(kedong) add openssl for host-python and replace ubuntu python with
 # host-python
 define HOST_GOOGLE_SIGNING_TEST_CMDS
-	python $(@D)/signing/repacktest.py
+	(cd $(@D)/signing; python repacktest.py)
 endef
 
 $(eval $(call GENTARGETS))
