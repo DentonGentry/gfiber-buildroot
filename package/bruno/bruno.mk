7c5fbb48a84840d958f1c9b2fb8491b0a01df11f
Avery Pennarun <apenwarr@gmail.com>
2012-05-29 02:49:12 -0400

    bruno.mk: simplify the makefile rules.
    We no longer have different builds with and without the _TEST define, so
    take out conditionals based on it.  We no longer support b0 builds, so take
    out conditionals based on that too.
    
    Once that's done, there's no really good reason to split all the different
    parts of the build/install into their own conditionally-included parts, so
    just move them into one build and one install_target rule, which makes it
    more obvious what's going on.
    
    I also added a Makefile in vendor/google/platform/cmds, so use 'make
    install' there instead of installing grep directly.  That way we can add
    more commands without patching both buildroot and vendor/google/platform.
    
    Change-Id: I23075c2abc5c5b4a8aa8aaf88902a4cf30a95341

--- package/bruno/bruno.mk
+++ package/bruno/bruno.mk
@@ -1,6 +1,7 @@
-# Until we add a proper package
+# Until we add a proper java package
 # :TODO: (by kedong, update BR2_GCC_SHARED_LIBGCC when new toolchain is setup)
 GOOGLE_JAVA_HOME=/usr/local/buildtools/java/jdk
+
 BRUNO_SITE=repo://vendor/google/platform
 BRUNO_INSTALL_STAGING=YES
 BRUNO_INSTALL_TARGET=YES
@@ -11,79 +12,35 @@ ifneq ($(BR2_PACKAGE_BRUNO_DEBUG)$(BR2_PACKAGE_BRUNO_TEST),)
 BRUNO_DEPENDENCIES += bcm_drivers
 endif
 
-BRUNO_DEFINES=
 BRUNO_STAGING_PATH=usr/lib/bruno
+BRUNO_DEFINES += -DBRUNO_PLATFORM=1 -DBRUNO_PLATFORM_GFHD100=1
 
-ifeq ($(BR2_PACKAGE_BRUNO),y)
-BRUNO_DEFINES += -DBRUNO_PLATFORM=1
-
-define BRUNO_BUILD_CMDS_DIAG
+define BRUNO_BUILD_CMDS
+	# diag
 	CROSS_COMPILE=$(TARGET_CROSS) $(MAKE) -C $(@D)/diag
-endef
-
-define BRUNO_BUILD_CMDS_MISC
+	
+	# cmds
 	CC="$(TARGET_CC) $(TARGET_CFLAGS)" $(MAKE) -C $(@D)/cmds
 endef
 
-define BRUNO_INSTALL_STAGING_CMDS_PC
-	mkdir -p $(STAGING_DIR)/usr/lib/pkgconfig && \
-	cp $(@D)/pkg-config/bruno.pc $(STAGING_DIR)/usr/lib/pkgconfig/bruno.pc && \
-	sed -i"" -e "s@CFLAGS@$(BRUNO_DEFINES)@g" $(STAGING_DIR)/usr/lib/pkgconfig/bruno.pc
-endef
-
-ifeq ($(BR2_PACKAGE_BRUNO_GFHD100),y)
-BRUNO_DEFINES += -DBRUNO_PLATFORM_GFHD100=1
-
-define BRUNO_INSTALL_TARGET_CMDS_REGISTER_CHECK
-	mkdir -p $(TARGET_DIR)/home/test/
-	cp -rf $(@D)/registercheck $(TARGET_DIR)/home/test/
-endef
-
-define BRUNO_INSTALL_TARGET_CMDS_DIAG
-	$(INSTALL) -D -m 0755 $(@D)/diag/diagd $(TARGET_DIR)/usr/bin/diagd
-	mkdir -p $(TARGET_DIR)/user/diag
-	$(INSTALL) -D -m 0644 $(@D)/diag/diag_kern_err_msgs.txt $(TARGET_DIR)/user/diag
-	$(INSTALL) -D -m 0644 $(@D)/diag/diag_kern_warn_msgs.txt $(TARGET_DIR)/user/diag
-	$(INSTALL) -D -m 0644 $(@D)/diag/diag_ref_data.txt $(TARGET_DIR)/user/diag
-endef
-
-define BRUNO_INSTALL_TARGET_CMDS_MISC
-	$(INSTALL) -D -m 0755 $(@D)/cmds/grep $(TARGET_DIR)/bin/grep
-endef
-endif
-
-endif
-
-ifeq ($(BR2_PACKAGE_BRUNO_TEST),y)
-define BRUNO_BUILD_CMDS
-	$(BRUNO_BUILD_CMDS_DIAG)
-	$(BRUNO_BUILD_CMDS_MISC)
-endef
-endif
-
 define BRUNO_INSTALL_STAGING_CMDS
 	mkdir -p $(STAGING_DIR)/$(BRUNO_STAGING_PATH)
-	$(BRUNO_INSTALL_STAGING_CMDS_PC)
+
+	mkdir -p $(STAGING_DIR)/usr/lib/pkgconfig && \
+	cp $(@D)/pkg-config/bruno.pc \
+		$(STAGING_DIR)/usr/lib/pkgconfig/bruno.pc && \
+	sed -i"" -e "s@CFLAGS@$(BRUNO_DEFINES)@g" \
+		$(STAGING_DIR)/usr/lib/pkgconfig/bruno.pc
 endef
 
-ifeq ($(BR2_PACKAGE_BRUNO_TEST),y)
-define BRUNO_INSTALL_TARGET_CMDS_TEST
-	$(BRUNO_INSTALL_TARGET_CMDS_SKEL)
-	$(BRUNO_INSTALL_TARGET_CMDS_DIAG)
-	$(BRUNO_INSTALL_TARGET_CMDS_MISC)
-	$(BRUNO_INSTALL_TARGET_CMDS_REGISTER_CHECK)
-endef
-endif
-
-ifeq ($(BR2_BRUNO_BCHP_VER),"B2")
 ifeq ($(BR2_PACKAGE_BRUNO_PROD),y)
 BRUNO_LOADER = cfe_signed_release.bin
 else
 BRUNO_LOADER = cfe_signed_unlocked.bin
 endif
-endif
 
-define BRUNO_INSTALL_MANIFEST
+define BRUNO_INSTALL_TARGET_CMDS
+	# Generate /etc/manifest and /etc/version
 	repo --no-pager manifest -r -o $(TARGET_DIR)/etc/manifest
 	#TODO(apenwarr): 'git describe' should use all projects.
 	#  Right now it only uses buildroot.  I have a plan for this
@@ -95,12 +52,21 @@ define BRUNO_INSTALL_MANIFEST
 		(echo -n '-'; \
 		 whoami | cut -c1-2) >>$(TARGET_DIR)/etc/version; \
 	fi
-endef
-
-define BRUNO_INSTALL_TARGET_CMDS
-	$(BRUNO_INSTALL_MANIFEST)
 	cp $(TARGET_DIR)/etc/version $(BINARIES_DIR)/version
-	$(BRUNO_INSTALL_TARGET_CMDS_TEST)
+
+	#TODO(apenwarr): /user/diag is nonstandard.  Consider /usr/lib/diag.
+	mkdir -p $(TARGET_DIR)/user/diag
+	$(INSTALL) -D -m 0755 $(@D)/diag/diagd $(TARGET_DIR)/usr/bin/diagd
+	$(INSTALL) -D -m 0644 $(@D)/diag/diag_kern_err_msgs.txt $(TARGET_DIR)/user/diag
+	$(INSTALL) -D -m 0644 $(@D)/diag/diag_kern_warn_msgs.txt $(TARGET_DIR)/user/diag
+	$(INSTALL) -D -m 0644 $(@D)/diag/diag_ref_data.txt $(TARGET_DIR)/user/diag
+
+	$(MAKE) DESTDIR=$(TARGET_DIR) -C $(@D)/cmds install
+
+	# registercheck
+	#TODO(apenwarr): do we actually need this for anything?
+	mkdir -p $(TARGET_DIR)/home/test/
+	cp -rf $(@D)/registercheck $(TARGET_DIR)/home/test/
 endef
 
 define BRUNO_INSTALL_IMAGES_CMDS
