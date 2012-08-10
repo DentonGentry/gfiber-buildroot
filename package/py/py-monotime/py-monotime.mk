4b8dbdec81de0bca0e23eacb58713ec52c087143
Avery Pennarun <apenwarr@gmail.com>
2012-08-10 15:18:47 -0400

    Add py-monotime package.
    Also fixes py.mk so it can correctly cross compile python modules that
    include C extensions.
    
    Change-Id: I31e274a286aba58ee864c7c74a94a02452e11f77

---
+++ package/py/py-monotime/py-monotime.mk
@@ -0,0 +1,6 @@
+PY_MONOTIME_VERSION = 1.0
+PY_MONOTIME_SOURCE  = Monotime-$(PY_MONOTIME_VERSION).tar.gz
+PY_MONOTIME_SITE    = http://pypi.python.org/packages/source/M/Monotime/
+PY_MONOTIME_DEPENDENCIES = python py-setuptools
+
+$(eval $(call PYTARGETS))
