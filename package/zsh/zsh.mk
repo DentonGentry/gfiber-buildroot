e66d82a78431390d36b6f58a23ad364bbfe72d93
Ke Dong <kedong@google.com>
2012-09-23 16:06:36 -0700

    Update zsh.mk to install zsh binary only.
    zsh binary is about 1M, but the stuff in /usr/share/zsh is about 5MB.
    They are not used anyway. zsh provides option to install binary only.
    This CL overrides INSTALL_TARGET_OPT in buildroot and changes the
    target to install.bin.
    
    Change-Id: I8e64c273ffe7ae31d2b177053a824c21955f4ce2

--- package/zsh/zsh.mk
+++ package/zsh/zsh.mk
@@ -5,9 +5,12 @@
 ZSH_VERSION=4.3.17
 ZSH_SOURCE=zsh-$(ZSH_VERSION).tar.bz2
 ZSH_SITE=http://sourceforge.net/projects/zsh/files/zsh-dev/$(ZSH_VERSION)
+# Just intall the binary, regardless of other stuff generated for /usr/share and /usr/man.
+ifeq ($(BR2_PACKAGE_ZSH_BINARIES_ONLY),y)
+ZSH_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install.bin
+endif
 ifeq ($(BR2_PACKAGE_NCURSES),y)
 ZSH_DEPENDENCIES += ncurses
 endif
-
 
 $(eval $(call AUTOTARGETS))
