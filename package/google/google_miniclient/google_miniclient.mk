4bddaf39ec1fcf9b3a8c4d6792cbe20a001c781e
Avery Pennarun <apenwarr@gmail.com>
2012-10-15 23:30:39 -0400

    Let miniclient package do its own 'make install' of its binaries.
    Now it includes gpio-mailbox and startupvideo, but we don't have to specify
    that here.
    
    Change-Id: Iddf296d8b712924e6aede9e75ac709422f9395ee

--- package/google/google_miniclient/google_miniclient.mk
+++ package/google/google_miniclient/google_miniclient.mk
@@ -8,12 +8,12 @@ GOOGLE_MINICLIENT_DEPENDENCIES=\
 define GOOGLE_MINICLIENT_BUILD_CMDS
         PULLREADER_PATH=$(STAGING_DIR)/usr/local/ \
         SWSCALE_PATH=$(STAGING_DIR)/usr/local/ \
-        $(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D) -f Makefile.7425
+        $(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D) -f Makefile.7425 all
 endef
 
 define GOOGLE_MINICLIENT_INSTALL_TARGET_CMDS
-        $(INSTALL) -D -m 0755 $(@D)/miniclient $(TARGET_DIR)/app/client/miniclient
-        $(INSTALL) -D -m 0755 $(@D)/waitpower $(TARGET_DIR)/app/client/waitpower
+	$(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D) -f Makefile.7425 \
+		DESTDIR=$(TARGET_DIR) install
         $(INSTALL) -m 0755 -D package/google/google_miniclient/S99miniclient $(TARGET_DIR)/etc/init.d/S99miniclient; \
         $(INSTALL) -D -m 0755 package/google/google_miniclient/run-app $(TARGET_DIR)/app/client/run-app
         $(INSTALL) -D -m 0755 package/google/google_miniclient/runminiclient $(TARGET_DIR)/app/client/runminiclient
