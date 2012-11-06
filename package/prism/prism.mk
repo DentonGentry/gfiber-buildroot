48f9f13888192d6b1811adb85c00dbf77f5f8076
Ke Dong <kedong@google.com>
2012-11-06 14:49:42 -0800

    Add optical SF module package.
    Change-Id: Ia412887f29b33a724bb9800cc5c4a4b033660403

--- package/prism/prism.mk
+++ package/prism/prism.mk
@@ -4,14 +4,28 @@ PRISM_INSTALL_STAGING=YES
 PRISM_INSTALL_TARGET=YES
 PRISM_INSTALL_IMAGES=YES
 
+PRISM_DEPENDENCIES=python py-setuptools host-py-yaml host-py-mox
+
 define PRISM_BUILD_CMDS
 	CC="$(TARGET_CC) $(TARGET_CFLAGS)" \
-	$(MAKE) -C $(@D)/cmds
+	$(MAKE) -C $(@D)/cmds && \
+	TARGETPYTHONPATH=$(TARGET_PYTHONPATH) \
+	HOSTDIR=$(HOST_DIR) \
+	$(MAKE) -C $(@D)/sfmodule
+endef
+
+define PRISM_TEST_CMDS
+	HOSTPYTHONPATH=$(HOST_PYTHONPATH) \
+	HOSTDIR=$(HOST_DIR) \
+	$(MAKE) -C $(@D)/sfmodule test
 endef
 
 define PRISM_INSTALL_TARGET_CMDS
 	$(call GENIMAGEVERSION,prism)
-	DESTDIR=$(TARGET_DIR) $(MAKE) -C $(@D)/cmds install
+	DESTDIR=$(TARGET_DIR) $(MAKE) -C $(@D)/cmds install && \
+	TARGETPYTHONPATH=$(TARGET_PYTHONPATH) \
+	HOSTDIR=$(HOST_DIR) \
+	DESTDIR=$(TARGET_DIR) $(MAKE) -C $(@D)/sfmodule install
 endef
 
 $(eval $(call GENTARGETS))
