d026944df46f774a38134267f2bc58129159d394
Alice Wang <alicejwang@google.com>
2013-01-18 16:28:57 -0800

    Prism Install image bundle
    - In fs/ginstall, create a subdirectory per platform.
      * Move the orignal ubinize.cfg and ginstall.mk into gftv100 directory.
    - gflt200_defconfig - add BR2_TARGET_ROOTFS_GINSTALL
    - uboot.mk - if BR2_PACKAGE_PRISM defined, copy all u-boot binary files
      into images directory
    
    b/8020037
    
    Change-Id: Ia1ab4c46835e43153cf7451695dac7c52ac1a1fb

--- boot/uboot/uboot.mk
+++ boot/uboot/uboot.mk
@@ -28,6 +28,8 @@ else ifeq ($(BR2_TARGET_UBOOT_FORMAT_LDR),y)
 UBOOT_BIN          = u-boot.ldr
 else ifeq ($(BR2_TARGET_UBOOT_FORMAT_NAND_BIN),y)
 UBOOT_BIN          = u-boot-nand.bin
+else ifeq ($(BR2_PACKAGE_PRISM),y)
+UBOOT_BIN          = u-boot*.bin
 else
 UBOOT_BIN          = u-boot.bin
 endif
