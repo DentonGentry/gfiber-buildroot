452ed45db6f9e1c1aa352a2e19a34743a0640088
Denton Gentry <dgentry@google.com>
2013-02-13 06:31:08 -0800

    Add a package for speedtest.
    Change-Id: I71a02f264a39d189fb73a8d7f744709e272c0bdf

---
+++ package/google/google_ookla/google_ookla.mk
@@ -0,0 +1,16 @@
+GOOGLE_OOKLA_SITE = repo://vendor/ookla
+GOOGLE_OOKLA_DEPENDENCIES = poco
+
+ifeq      ($(BR2_arm),y)
+OOKLA_ARCH   := arm
+else ifeq ($(BR2_mips),y)
+OOKLA_ARCH   := mips
+else ifeq ($(BR2_mipsel),y)
+OOKLA_ARCH   := mips
+endif
+
+define GOOGLE_OOKLA_INSTALL_TARGET_CMDS
+	cp -af $(@D)/$(OOKLA_ARCH)/OoklaClient $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(call GENTARGETS))
