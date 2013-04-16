f3a200ba23025390baaddbdb952a0a31f3587b1c
Ke Dong <kedong@google.com>
2013-04-15 17:58:06 -0700

    Update uboot binary name based on uboot change.
    Change-Id: If41b490c40a5a4c88c6f4051cea6e823ef4e08a5

--- fs/ginstall/gflt200/ginstall_gflt200.mk
+++ fs/ginstall/gflt200/ginstall_gflt200.mk
@@ -8,7 +8,7 @@ ROOTFS_GINSTALL_DEPENDENCIES = linux rootfs-initramfs
 
 ROOTFS_GINSTALL_VERSION = "$$\(cat $(BINARIES_DIR)/version\)"
 
-PRISM_LOADERS := u-boot-gflt200_400rd_A-MC_ddr3_spi.bin
+PRISM_LOADERS := u-boot-gflt200_256mb_400mhz_ddr3_spi.bin
 
 define ROOTFS_GINSTALL_CMD
 	set -e; \
