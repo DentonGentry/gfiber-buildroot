fa6498638cbc3254204b3bfa044a99378853af77
Ke Dong <kedong@google.com>
2013-04-15 19:18:15 -0700

    Consolidate the build target name for uboot.
    Only pickup u-boot-spi.bin from uboot build. This way buildroot does not
    have to change based on the uboot target naming convention.
    
    Change-Id: I9fb497b360a87a29f78d0cee9bb170775d278dd9

--- boot/uboot/uboot.mk
+++ boot/uboot/uboot.mk
@@ -29,7 +29,7 @@ UBOOT_BIN          = u-boot.ldr
 else ifeq ($(BR2_TARGET_UBOOT_FORMAT_NAND_BIN),y)
 UBOOT_BIN          = u-boot-nand.bin
 else ifeq ($(BR2_PACKAGE_PRISM),y)
-UBOOT_BIN          = u-boot*.bin
+UBOOT_BIN          = u-boot-spi.bin
 else
 UBOOT_BIN          = u-boot.bin
 endif
