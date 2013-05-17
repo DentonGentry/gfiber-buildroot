c2e74d3264d4eaf4b396b91934c00b4c6b129cbc
Avery Pennarun <apenwarr@gmail.com>
2013-05-16 22:40:48 -0400

    Fix PYTHONPATH stuff after merge from master.
    Change-Id: I0abac39af5036fb109413ba19d6efb18ef63c69a

--- package/prism/prism.mk
+++ package/prism/prism.mk
@@ -7,28 +7,17 @@ PRISM_INSTALL_IMAGES=YES
 PRISM_DEPENDENCIES=python py-setuptools host-py-yaml host-py-mox host-i2c-tools
 
 define PRISM_BUILD_CMDS
-	CROSS_COMPILE=$(TARGET_CROSS) \
-	$(MAKE) -C $(@D)/cmds && \
-	PYTHONPATH=$(TARGET_PYTHONPATH) \
-	PYTHON=$(HOST_DIR)/usr/bin/python \
-	CROSS_COMPILE=$(TARGET_CROSS) \
-	$(MAKE) -C $(@D)/prism
+	$(BRUNO_MAKE) -C $(@D)/cmds && \
+	$(BRUNO_MAKE) -C $(@D)/prism
 endef
 
 define PRISM_TEST_CMDS
-	PYTHONPATH=$(HOST_PYTHONPATH) \
-	PYTHON=$(HOST_DIR)/usr/bin/python \
-	$(MAKE) -C $(@D)/prism test
+	$(BRUNO_MAKE) -C $(@D)/prism test
 endef
 
 define PRISM_INSTALL_TARGET_CMDS
 	$(call GENIMAGEVERSION,prism)
-	DESTDIR=$(TARGET_DIR) $(MAKE) -C $(@D)/cmds install && \
-	HOSTPYTHONPATH=$(HOST_PYTHONPATH) \
-	PYTHONPATH=$(TARGET_PYTHONPATH) \
-	PYTHON=$(HOST_DIR)/usr/bin/python \
-	HOSTDIR=$(HOST_DIR) \
-	DESTDIR=$(TARGET_DIR) $(MAKE) -C $(@D)/prism install
+	$(BRUNO_MAKE) -C $(@D)/prism install
 endef
 
 $(eval $(call GENTARGETS))
