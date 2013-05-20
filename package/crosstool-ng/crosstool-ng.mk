d7d07f2b3d4c4cc2473218485007d648eba2f959
Avery Pennarun <apenwarr@gmail.com>
2013-05-20 16:53:56 -0400

    kvm_defconfig: switch to using x86_64 instead of x86 32-bit build.
    We also switch from uClibc to glibc for this build.  The advantage of these
    changes is that the produced binaries can all run on a plain ubuntu
    workstation.
    
    Unfortunately, buildroot's normal toolchain compiler can't handle glibc, so
    we had to use crosstool, which is not so bad, but adds a few layers of
    complexity.  And glibc introduced a couple of compiler problems which we
    workaround with a couple of minor package patches.
    
    Change-Id: Ic262e473b0733062624ff12040aa8fb492fe706f

--- package/crosstool-ng/crosstool-ng.mk
+++ package/crosstool-ng/crosstool-ng.mk
@@ -3,7 +3,7 @@ CROSSTOOL_NG_SOURCE            = crosstool-ng-$(CROSSTOOL_NG_VERSION).tar.bz2
 CROSSTOOL_NG_SITE              = http://crosstool-ng.org/download/crosstool-ng/
 CROSSTOOL_NG_INSTALL_STAGING   = NO
 CROSSTOOL_NG_INSTALL_TARGET    = NO
-CROSSTOOL_NG_MAKE              = $(MAKE1)
+CROSSTOOL_NG_MAKE              = $(MAKE)
 HOST_CROSSTOOL_NG_DEPENDENCIES = $(if $(BR2_CCACHE),host-ccache) host-gawk host-automake
 
 $(eval $(call AUTOTARGETS,host))
