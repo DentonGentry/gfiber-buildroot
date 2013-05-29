101f12158c0274b67d79748ac22536b42c384b72
Avery Pennarun <apenwarr@gmail.com>
2013-05-28 23:47:00 -0400

    Split platform/prism into its own repo and package.
    Change-Id: Ife83cc4971a0283ab70b9c1ba923e86a6665fe0f

--- package/google/google_prism/google_prism.mk
+++ package/google/google_prism/google_prism.mk
@@ -1,21 +1,25 @@
 GOOGLE_PRISM_SITE=repo://vendor/google/prism
-
 GOOGLE_PRISM_INSTALL_STAGING=YES
 GOOGLE_PRISM_INSTALL_TARGET=YES
-GOOGLE_PRISM_INSTALL_IMAGES=YES
-
-GOOGLE_PRISM_DEPENDENCIES=python py-setuptools host-py-yaml host-py-mox host-i2c-tools
+GOOGLE_PRISM_DEPENDENCIES=\
+	host-py-mox \
+	python \
+	py-setuptools \
 
 define GOOGLE_PRISM_BUILD_CMDS
 	$(GPLAT_MAKE) -C $(@D)
 endef
 
 define GOOGLE_PRISM_TEST_CMDS
-	$(GPLAT_MAKE) -C $(@D) test
+	PYTHONPATH=$(HOST_PYTHONPATH) \
+	PYTHON=$(HOST_DIR)/usr/bin/python $(MAKE) -C $(@D) test
+endef
+
+define GOOGLE_PRISM_INSTALL_STAGING_CMDS
+	$(MAKE) DESTDIR=$(STAGING_DIR) -C $(@D) install-libs
 endef
 
 define GOOGLE_PRISM_INSTALL_TARGET_CMDS
-	$(call GENIMAGEVERSION,prism)
 	$(GPLAT_MAKE) -C $(@D) install
 endef
 
