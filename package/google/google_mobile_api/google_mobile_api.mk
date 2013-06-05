56338d9e3fca07bcdc2828af672cdd414604f2c2
Peter Baldwin <pbaldwin@google.com>
2013-06-05 10:53:29 -0400

    Made mobile API plug-in files readable by Sage
    Change-Id: If314be9b210369286aad80df353fa310acdf157e

--- package/google/google_mobile_api/google_mobile_api.mk
+++ package/google/google_mobile_api/google_mobile_api.mk
@@ -13,8 +13,9 @@ define GOOGLE_MOBILE_API_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/app/sage/gftv_mobile_api_deploy
 	unzip -qq $(@D)/gftv_mobile_api_deploy.jar -d $(TARGET_DIR)/app/sage/gftv_mobile_api_deploy
 	jar cf0 $(TARGET_DIR)/app/sage/gftv_mobile_api_deploy.jar -C $(TARGET_DIR)/app/sage/gftv_mobile_api_deploy .
+	chmod 0644 $(TARGET_DIR)/app/sage/gftv_mobile_api_deploy.jar
 	rm -rf $(TARGET_DIR)/app/sage/gftv_mobile_api_deploy
-	cp -af package/google/google_mobile_api/plugin.properties \
+	$(INSTALL) -m 0644 -D package/google/google_mobile_api/plugin.properties \
 		$(TARGET_DIR)/app/sage/Sage.properties.defaults.mobileapi
 endef
 
