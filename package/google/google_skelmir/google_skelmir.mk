8f05e06f284cf9e8f11cefd9510d123895168ec0
Avery Pennarun <apenwarr@gmail.com>
2013-06-08 03:07:16 -0400

    Changes to make SageTV work on mindspeed eval board.
    - Include some missing packages.
    - Pipe a never-ending program to siege's stdin (necessary for the
      experimental skelmir jvm).
    - Mindspeed eval board has only 512M of RAM, so use a smaller java heap.
    - Teach google_skelmir to install different binaries depending on
      architecture.
    
    Change-Id: Ib1418dd735d7dc3928b44665003b7102cf97a743

--- package/google/google_skelmir/google_skelmir.mk
+++ package/google/google_skelmir/google_skelmir.mk
@@ -1,11 +1,21 @@
 GOOGLE_SKELMIR_SITE = repo://vendor/skelmir/vm
 
+ifeq ($(BR2_mipsel),y)
+SKELMIR_ARCH=mipsel-5k-linux-uclibc
+else ifeq ($(BR2_arm),y)
+SKELMIR_ARCH=arm-v7l-linux-gnueabi
+else
+SKELMIR_ARCH=unknown-skelmir-arch
+endif
+
 define GOOGLE_SKELMIR_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/app/sage/skelmir
-	cp -af  $(@D)/Binaries/siege \
-		$(@D)/Binaries/Libraries \
-		$(@D)/Binaries/mipsel-5k-linux-uclibc \
+	cp -af  $(@D)/Binaries/Libraries \
+		$(@D)/Binaries/$(SKELMIR_ARCH) \
 		$(TARGET_DIR)/app/sage/skelmir/
+	cd $(TARGET_DIR)/app/sage/skelmir && \
+	ln -sf  $(SKELMIR_ARCH)/siege* \
+		./siege
 endef
 
 $(eval $(call GENTARGETS))
