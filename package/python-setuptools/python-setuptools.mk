edc2e8469607caf1d3fd1d8681f16bbd6af626c4
Avery Pennarun <apenwarr@gmail.com>
2013-06-12 16:10:46 -0400

    Upgrade python-setuptools to 0.7.2.
    This seems to fix installer race conditions.  Test commands:
    
    rm -rf target/usr/lib/python2.7/site-packages/; \
    mkdir target/usr/lib/python2.7/site-packages/; \
    rm -f build/{google_platform,python-setuptools,py-oauth2,py-httplib2,py-google-api,py-gflags}-*/.stamp_target_installed; \
    make google_platform python-setuptools py-oauth2 \
    	py-httplib2 py-google-api py-gflags LOGLINEAR= -j
    cat target/usr/lib/python2.7/site-packages/easy-install.pth
    
    ...and easy-install.pth should include options.py and python_gflags every
    time you run it.  With 0.6c11, some files were sometimes missing; if you
    left out the -j, it always worked.  With 0.7.2, it seems to work regardless
    of -j.
    
    Change-Id: Ib828b5ddb85930441153d9f08d44034c22b12bce

--- package/python-setuptools/python-setuptools.mk
+++ package/python-setuptools/python-setuptools.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-PYTHON_SETUPTOOLS_VERSION = 0.6c11
+PYTHON_SETUPTOOLS_VERSION = 0.7.2
 PYTHON_SETUPTOOLS_SOURCE  = setuptools-$(PYTHON_SETUPTOOLS_VERSION).tar.gz
 PYTHON_SETUPTOOLS_SITE    = http://pypi.python.org/packages/source/s/setuptools
 PYTHON_SETUPTOOLS_DEPENDENCIES = python
