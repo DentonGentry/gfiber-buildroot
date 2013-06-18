dcc3f76291a6a9dabdb853e34584cf56aa86413a
Jean-Francois Thibert <jfthibert@google.com>
2013-06-18 11:17:42 -0400

    Add removal of activation data based on flag files in /rw/sage
    Change-Id: I07b49d438cb7ccbab9a6d723f36741de1f288e9a

--- package/google/google_miniclient/google_miniclient.mk
+++ package/google/google_miniclient/google_miniclient.mk
@@ -22,6 +22,7 @@ define GOOGLE_MINICLIENT_INSTALL_TARGET_CMDS
 	$(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D) -f Makefile.7425 \
 		DESTDIR=$(TARGET_DIR) install
         $(INSTALL) -m 0755 -D package/google/google_miniclient/S99miniclient $(TARGET_DIR)/etc/init.d/S99miniclient; \
+        $(INSTALL) -D -m 0755 package/google/google_miniclient/clear-app $(TARGET_DIR)/app/client/clear-app
         $(INSTALL) -D -m 0755 package/google/google_miniclient/run-app $(TARGET_DIR)/app/client/run-app
         $(INSTALL) -D -m 0755 package/google/google_miniclient/runminiclient $(TARGET_DIR)/app/client/runminiclient
 endef
