beab8b7fba75feb4ee33586b27f140ed0a0f327d
Ed James <edjames@google.com>
2013-08-29 13:41:46 -0700

    Wifi and wired test scripts
    	* review comments addressed
    		* some params can be overridden on boot or env
    		* tabs fixed
    		* iperf server used instead of ping google.com
    		* comments and variables changed for clarity
    
    	dhclient_start weth0 failed for me because I can't connect to GoogleGuest on N,
    	but the code is sound.
    
    	lan0 gets 41MBytes/sec, wan0 gets less because I have a 100Mbit switch in the loop.
    
    	I turned the memtester down to 10M for now, takes too long to run.
    	IP addrs in Config.sh need to be standardized; we need a "test bench"
    	identical to what manufacturing might use.
    
    	---- D01dram			    PASS
    	---- D02wifi			    FAIL   failed: dhclient_start weth0
    	---- D03switch			    PASS   lan0=41 wan0=11
    	---- D04led			    FAIL   not implemented
    	---- D05fan			    FAIL   not implemented
    	---- D06usb			    FAIL   not implemented
    	---- D07disk			    FAIL   not implemented
    	---- D08button			    FAIL   not implemented
    
    	FAILURE! 6 of 8 FAILED
    	logs are in /tmp/diag.tgz
    	./rcD: done.
    
    Change-Id: I8eb50dc3f08df641567d310b0dfca57e5bc31749

--- fs/skeleton/etc/diag.d/Config.sh
+++ fs/skeleton/etc/diag.d/Config.sh
@@ -2,12 +2,36 @@
 # configuration for diagnostic scripts
 #
 
+# defaults, if not overridden
+iperf_server=192.168.1.2
+
+# these can be set on the boot line or the environment
+diag_essid="${diag_essid:-$(kernopt diag_essid)}"
+diag_iperf_server="${diag_iperf_server:-$(kernopt diag_iperf_server)}"
+
 #
 # general configs here
 #
 
-dram_memory=500M		# how much memory to test
+dram_memory=10M		# how much memory to test
 
+wifi_essid="${diag_essid:-factory}"
+
+wired_lan=lan0
+wired_wan=wan0
+
+# iperf_server_* can be overriden by environment
+iperf_server_lan0="${iperf_server_lan0:-${diag_iperf_server:-$iperf_server}}"
+iperf_min_lan0=10		# MBytes/sec
+
+iperf_server_wan0="${iperf_server_wan0:-${diag_iperf_server:-$iperf_server}}"
+iperf_min_wan0=10		# MBytes/sec
+
+iperf_server_weth0="${iperf_server_weth0:-${diag_iperf_server:-$iperf_server}}"
+iperf_min_weth0=10		# MBytes/sec
+
+iperf_server_weth1="${iperf_server_weth1:-${diag_iperf_server:-$iperf_server}}"
+iperf_min_weth1=10		# MBytes/sec
 
 #
 # platform specific configs follow
