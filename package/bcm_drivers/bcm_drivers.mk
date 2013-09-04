9e807c97fe7396d112fdc54d8062f2dd27337f4d
Avery Pennarun <apenwarr@gmail.com>
2013-09-04 14:06:36 -0400

    Add a separate config option for MoCA 2.0.
    This allows it to build and install the bmoca kernel module, not just the
    mocad.  On bruno devices, we can switch back and forth between moca 1 and 2.
    On GFRG200, we can only use moca2, but it works too.  Eventually we'll
    remove the old moca 1.x support.
    
    Change-Id: I7ee236ad7dbab8a41be92aed42468a26994516dd

--- package/bcm_drivers/bcm_drivers.mk
+++ package/bcm_drivers/bcm_drivers.mk
@@ -3,6 +3,7 @@ BCM_DRIVERS_INSTALL_STAGING=YES
 BCM_DRIVERS_INSTALL_TARGET=YES
 BCM_DRIVERS_DEPENDENCIES=linux google_platform
 
+# TODO(apenwarr): Remove the old moca1 stuff after we fully move to moca2.
 ifeq ($(BR2_PACKAGE_BCM_DRIVER_MOCA),y)
 define BCM_DRIVERS_BUILD_MOCA
 	$(TARGET_MAKE_ENV) $(MAKE1) \
@@ -10,21 +11,86 @@ define BCM_DRIVERS_BUILD_MOCA
 		CC="$(TARGET_CC)" \
 		EXTRA_CFLAGS="$(TARGET_CFLAGS)" \
 		EXTRA_LDFLAGS="$(TARGET_LDFLAGS)" \
+		LINUXDIR="$(LINUX_DIR)" \
+		ARCH="$(KERNEL_ARCH)" \
 		-C $(@D)/moca/
+	# Note: we build the moca2 kernel module.  It's compatible.
+	$(TARGET_MAKE_ENV) $(MAKE1) \
+		CROSS=$(TARGET_CROSS) \
+		CC="$(TARGET_CC)" \
+		EXTRA_CFLAGS="$(TARGET_CFLAGS)" \
+		EXTRA_LDFLAGS="$(TARGET_LDFLAGS)" \
+		LINUXDIR="$(LINUX_DIR)" \
+		ARCH="$(KERNEL_ARCH)" \
+		-C $(@D)/moca2/ bmoca/bmoca.ko
 endef
 
 define BCM_DRIVERS_INSTALL_STAGING_MOCA
-	$(INSTALL) -D -m 0644 $(@D)/moca/lib/libmoca.a $(STAGING_DIR)/usr/lib/libmoca.a
-	$(INSTALL) -D -m 0644 $(@D)/moca/util/libmocactl.a $(STAGING_DIR)/usr/lib/libmocactl.a
-	mkdir -p $(STAGING_DIR)/usr/include/moca && \
-	cp -pr $(@D)/moca/include/* $(STAGING_DIR)/usr/include/moca
+	$(INSTALL) -m 0644 \
+		$(@D)/moca/lib/libmoca.a \
+		$(@D)/moca/util/libmocactl.a \
+		$(STAGING_DIR)/usr/lib/
+	$(INSTALL) -d -m 0755 \
+		$(STAGING_DIR)/usr/include/moca
+	cp -r $(@D)/moca/include/. $(STAGING_DIR)/usr/include/moca
 endef
 
 define BCM_DRIVERS_INSTALL_TARGET_MOCA
-	$(INSTALL) -m 0700 $(@D)/moca/bin/mocad $(TARGET_DIR)/bin/
-	$(INSTALL) -m 0700 $(@D)/moca/bin/mocactl $(TARGET_DIR)/bin/
-	$(INSTALL) -m 0700 $(@D)/moca/bin/soapserver $(TARGET_DIR)/bin/
-	$(INSTALL) -D -m 0600 $(@D)/moca/mocacore-gen3.bin $(TARGET_DIR)/etc/moca/mocacore-gen3.bin
+	rm -f $(TARGET_DIR)/bin/mocap
+	$(INSTALL) -d -m 0755 \
+		$(TARGET_DIR)/etc/moca \
+		$(TARGET_DIR)/usr/lib/modules
+	$(INSTALL) -m 0755 \
+		$(@D)/moca/bin/mocad \
+		$(@D)/moca/bin/mocactl \
+		$(TARGET_DIR)/bin/
+	$(INSTALL) -m 0644 \
+		$(@D)/moca/mocacore-*.bin \
+		$(TARGET_DIR)/etc/moca/
+	# Note: we install the moca2 kernel module.  It's compatible.
+	$(INSTALL) -m 0644 \
+		$(@D)/moca2/bmoca/bmoca.ko \
+		$(TARGET_DIR)/usr/lib/modules
+endef
+endif
+
+ifeq ($(BR2_PACKAGE_BCM_DRIVER_MOCA2),y)
+define BCM_DRIVERS_BUILD_MOCA
+	$(TARGET_MAKE_ENV) $(MAKE1) \
+		CROSS=$(TARGET_CROSS) \
+		CC="$(TARGET_CC)" \
+		EXTRA_CFLAGS="$(TARGET_CFLAGS)" \
+		EXTRA_LDFLAGS="$(TARGET_LDFLAGS)" \
+		LINUXDIR="$(LINUX_DIR)" \
+		ARCH="$(KERNEL_ARCH)" \
+		-C $(@D)/moca2/
+endef
+
+define BCM_DRIVERS_INSTALL_STAGING_MOCA
+	$(INSTALL) -m 0644 \
+		$(@D)/moca2/bin/libmoca.a \
+		$(@D)/moca2/bin/libmocacli.a \
+		$(STAGING_DIR)/usr/lib/
+	$(INSTALL) -d -m 0755 \
+		$(STAGING_DIR)/usr/include/moca
+	cp -r $(@D)/moca2/include/. $(STAGING_DIR)/usr/include/moca
+endef
+
+define BCM_DRIVERS_INSTALL_TARGET_MOCA
+	rm -f $(TARGET_DIR)/bin/mocactl
+	$(INSTALL) -d -m 0755 \
+		$(TARGET_DIR)/etc/moca \
+		$(TARGET_DIR)/usr/lib/modules
+	$(INSTALL) -m 0755 \
+		$(@D)/moca2/bin/mocad \
+		$(@D)/moca2/bin/mocap \
+		$(TARGET_DIR)/bin/
+	$(INSTALL) -m 0644 \
+		$(@D)/moca2/moca20core-*.bin \
+		$(TARGET_DIR)/etc/moca/
+	$(INSTALL) -m 0644 \
+		$(@D)/moca2/bmoca/bmoca.ko \
+		$(TARGET_DIR)/usr/lib/modules
 endef
 endif
 
