e1b7ef2622656dcf2cce4a709a67c4c360871283
Jason Daly <dalyj@google.com>
2013-09-05 15:03:37 -0700

    Added new package for the Dart VM.  Also added a new download method for repositories managed with gclient.
    Change-Id: I711a9ea376b07586c15cd72ef496ea3c89f49c5f

---
+++ package/google/google_dart_vm/google_dart_vm.mk
@@ -0,0 +1,23 @@
+GOOGLE_DART_VM_SITE=http://dart.googlecode.com/svn/branches/bleeding_edge/deps/standalone.deps
+GOOGLE_DART_VM_SITE_METHOD=gclient
+GOOGLE_DART_VM_VERSION=27137
+
+GOOGLE_DART_VM_MAKE_VARS=\
+BUILDTYPE=ReleaseMIPS \
+NM.target="$(TARGET_NM)" \
+CC.target="$(TARGET_CC)" \
+LINK.target="$(TARGET_CXX)" \
+CXX.target="$(TARGET_CXX)" \
+AR.target="$(TARGET_AR)"
+
+define GOOGLE_DART_VM_BUILD_CMDS
+	$(MAKE) -C $(BUILD_DIR)/$($(PKG)_BASE_NAME)/dart/runtime $(GOOGLE_DART_VM_MAKE_VARS)
+	$(TARGET_STRIP) $(BUILD_DIR)/$($(PKG)_BASE_NAME)/dart/runtime/out/ReleaseMIPS/dart
+endef
+
+
+define GOOGLE_DART_VM_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(BUILD_DIR)/$($(PKG)_BASE_NAME)/dart/runtime/out/ReleaseMIPS/dart $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(call GENTARGETS))
