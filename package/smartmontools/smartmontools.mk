0930630bddb6dd66e31c7113e8663b52a91ecb2c
Chris Kuiper <ckuiper@google.com>
2013-10-28 11:23:28 -0700

    Log calls to smartctrl
    This enables logging of all calls to smartctl by using a wrapper
    script. The original smartctl binary is renamed to smartctl.real and
    the wrapper script is taking its (name)place.
    
    Change-Id: I0e4e54b505a635fa68f621d3502693934e1691f2

--- package/smartmontools/smartmontools.mk
+++ package/smartmontools/smartmontools.mk
@@ -7,4 +7,8 @@
 SMARTMONTOOLS_VERSION = 5.41
 SMARTMONTOOLS_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/smartmontools
 
+define SMARTMONTOOLS_INSTALL_TARGET_CMDS
+  $(INSTALL) -D -m 0755 $(@D)/smartctl $(TARGET_DIR)/usr/sbin/smartctl.real
+endef
+
 $(eval $(call AUTOTARGETS))
