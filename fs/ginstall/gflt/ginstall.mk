58b523a19c98a9a00a41b6dc9ba1774630779193
John Newlin <jnewlin@google.com>
2013-10-28 08:37:48 -0700

    Add a v3 manifest file for gflt series of devices.
    Change-Id: Ic80158108a9a3476cb8d470958ddb7430f4408b3

--- fs/ginstall/gflt/ginstall.mk
+++ fs/ginstall/gflt/ginstall.mk
@@ -3,19 +3,33 @@
 # Google platform image creation for gflt platforms.
 #
 #############################################################
+# WARNING WARNING WARNING
+#
+# Because of how buildroot handles fs generation macros, it EATS DOUBLE
+# QUOTES.  Use only single quotes in all shell commands in this file, or
+# you'll get very weird, hard-to-find errors.
+
 
 ROOTFS_GINSTALL_DEPENDENCIES = linux rootfs-initramfs
-
-ROOTFS_GINSTALL_VERSION = "$$\(cat $(BINARIES_DIR)/version\)"
-
+ROOTFS_GINSTALL_VERSION = $(shell cat $(BINARIES_DIR)/version)
+ROOTFS_GINSTALL_PLATFORMS = $(shell echo $(BR2_TARGET_GENERIC_PLATFORMS_SUPPORTED) | sed 's/[, ][, ]*/, /g' | tr a-z A-Z)
 GFLT_LOADER := u-boot-spi.bin
 
 define ROOTFS_GINSTALL_CMD
 	set -e; \
+	set -x; \
+	rm -f $(BINARIES_DIR)/manifest && \
+	echo 'installer_version: 3' >>$(BINARIES_DIR)/manifest && \
+	echo 'image_type: unlocked' >>$(BINARIES_DIR)/manifest && \
+	echo 'platforms: [GFLT110]' >>$(BINARIES_DIR)/manifest && \
+	echo 'version: $(value ROOTFS_GINSTALL_VERSION)' >>$(BINARIES_DIR)/manifest && \
 	cd $(BINARIES_DIR) && \
-	cp $(value GFLT_LOADER) loader.bin && \
+	cp $(value GFLT_LOADER) loader.img && \
+	cp uImage kernel.img && \
+	(echo -n 'kernel.img-sha1: ' && sha1sum kernel.img | cut -c1-40 && \
+	 echo -n 'loader.img-sha1: ' && sha1sum loader.img | cut -c1-40;) >>manifest && \
 	tar -cf $(value ROOTFS_GINSTALL_VERSION).gi \
-		version loader.bin uImage
+		manifest version loader.img kernel.img
 endef
 
 $(eval $(call ROOTFS_TARGET,ginstall))
