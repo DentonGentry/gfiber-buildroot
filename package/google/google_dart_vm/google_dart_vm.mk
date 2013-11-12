c0e6697fba788e1150cfb56d9ab18ff36c7f25f1
Jason Daly <dalyj@google.com>
2013-11-11 16:39:21 -0800

    Update Dart VM to r30104.  Note the build process for the Dart VM has changed slightly.
    Change-Id: I0be676cb49d51582244428c2faafcc0462451674

--- package/google/google_dart_vm/google_dart_vm.mk
+++ package/google/google_dart_vm/google_dart_vm.mk
@@ -1,6 +1,6 @@
 GOOGLE_DART_VM_SITE=http://dart.googlecode.com/svn/branches/bleeding_edge/deps/standalone.deps
 GOOGLE_DART_VM_SITE_METHOD=gclient
-GOOGLE_DART_VM_VERSION=28355
+GOOGLE_DART_VM_VERSION=30104
 
 GOOGLE_DART_VM_MAKE_VARS=\
 BUILDTYPE=ReleaseMIPS \
@@ -11,13 +11,13 @@ CXX.target="$(TARGET_CXX)" \
 AR.target="$(TARGET_AR)"
 
 define GOOGLE_DART_VM_BUILD_CMDS
-	$(MAKE) -C $(BUILD_DIR)/$($(PKG)_BASE_NAME)/dart/runtime $(GOOGLE_DART_VM_MAKE_VARS)
-	$(TARGET_STRIP) $(BUILD_DIR)/$($(PKG)_BASE_NAME)/dart/runtime/out/ReleaseMIPS/dart
+	$(MAKE) -C $(BUILD_DIR)/$($(PKG)_BASE_NAME)/dart $(GOOGLE_DART_VM_MAKE_VARS)
+	$(TARGET_STRIP) $(BUILD_DIR)/$($(PKG)_BASE_NAME)/dart/out/ReleaseMIPS/dart
 endef
 
 
 define GOOGLE_DART_VM_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 -D $(BUILD_DIR)/$($(PKG)_BASE_NAME)/dart/runtime/out/ReleaseMIPS/dart $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 -D $(BUILD_DIR)/$($(PKG)_BASE_NAME)/dart/out/ReleaseMIPS/dart $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(call GENTARGETS))
