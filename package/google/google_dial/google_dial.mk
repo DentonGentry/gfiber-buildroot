ff55288aecd3f00384b3c22c692264ba55161fad
Denton Gentry <dgentry@google.com>
2013-12-04 23:41:14 -0800

    Restart dialserver when nickname changes.
    Make fix for b/10151485 more better.
    
    Change-Id: I8fa46028cb49e8f1afcff51829e1d8ec3cf53ab9

--- package/google/google_dial/google_dial.mk
+++ package/google/google_dial/google_dial.mk
@@ -6,6 +6,8 @@ endef
 
 define GOOGLE_DIAL_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D package/google/google_dial/S98dialserver $(TARGET_DIR)/etc/init.d/S98dialserver
+	$(INSTALL) -m 0755 -D package/google/google_dial/S99dialmonitor $(TARGET_DIR)/etc/init.d/S99dialmonitor
+	$(INSTALL) -m 0755 -D package/google/google_dial/dialmonitor $(TARGET_DIR)/bin/dialmonitor
 	$(INSTALL) -m 0755 -D $(@D)/src/server/dialserver $(TARGET_DIR)/app/client/dialserver
 endef
 
