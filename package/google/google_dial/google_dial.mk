e16f13ea5bab96b1f97560dd93ecd84b9f0f6374
Jean-Francois Thibert <jfthibert@google.com>
2013-12-09 14:53:04 -0500

    Prevent starting more than one dialserver (b/12017240)
    The stop action couldn't kill the start background process
    when it was waiting on the startupvideo.done file. This
    could cause more than one dialserver to be started once
    the startupvideo.done file was created
    
    Change-Id: I59cd906e3969380972ebd5d0c75846d9f2025d32

--- package/google/google_dial/google_dial.mk
+++ package/google/google_dial/google_dial.mk
@@ -9,6 +9,7 @@ define GOOGLE_DIAL_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D package/google/google_dial/S99dialmonitor $(TARGET_DIR)/etc/init.d/S99dialmonitor
 	$(INSTALL) -m 0755 -D package/google/google_dial/dialmonitor $(TARGET_DIR)/bin/dialmonitor
 	$(INSTALL) -m 0755 -D $(@D)/src/server/dialserver $(TARGET_DIR)/app/client/dialserver
+	$(INSTALL) -m 0755 -D package/google/google_dial/rundialserver $(TARGET_DIR)/app/client/rundialserver
 endef
 
 $(eval $(call GENTARGETS))
