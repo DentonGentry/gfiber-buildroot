b010b0f5d4616fc06bd52064b8c8ddfe24babae2
Avery Pennarun <apenwarr@gmail.com>
2013-12-21 03:40:23 -0500

    package/firmware: add a package for the linux-firmware repo.
    A config item allows you to list which exact firmware blobs you wish to
    include in the image.
    
    Change-Id: I2ba0fa7a961c548a5f71f1399edbc2eec3d6e4ba

---
+++ package/firmware/firmware.mk
@@ -0,0 +1,17 @@
+FIRMWARE_SITE=repo://vendor/firmware
+
+define FIRMWARE_INSTALL_TARGET_CMDS
+	# BR2_* strings contain wrapper quotes; remove them using the $tmp
+	# assignment, so that wildcards can work.
+	set -e; \
+	tmp=$(BR2_PACKAGE_FIRMWARE_FILENAMES); \
+	for d in $$tmp; do \
+	  for dd in $(@D)/$$d; do \
+	    dir=$$(dirname "$(TARGET_DIR)/lib/firmware/$$d"); \
+	    mkdir -p "$$dir"; \
+	    cp "$$dd" "$$dir/"; \
+	  done; \
+	done
+endef
+
+$(eval $(call GENTARGETS))
