b1748147b47c0878b4fdd388a70fcb33bfbf60ea
Avery Pennarun <apenwarr@gmail.com>
2014-01-28 02:00:23 -0500

    Upgrade iw to latest version.
    Adds a few new options available in newer kernels.  There's now an 'iw scan
    dump' command that redisplays prior 'iw scan' results.
    
    Change-Id: I4614adccfd805ea9cf5849938aae560011e2c750

--- package/iw/iw.mk
+++ package/iw/iw.mk
@@ -4,23 +4,20 @@
 #
 #############################################################
 
-IW_VERSION = 3.3
-IW_SOURCE = iw-$(IW_VERSION).tar.bz2
-IW_SITE = http://wireless.kernel.org/download/iw
+IW_VERSION = 3.14
+IW_SOURCE = iw-$(IW_VERSION).tar.gz
+IW_SITE = https://kernel.org/pub/software/network/iw
 IW_DEPENDENCIES = host-pkg-config libnl
-IW_CONFIG = $(IW_DIR)/.config
 IW_MAKE_ENV = PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig" \
 	PKG_CONFIG="$(HOST_DIR)/usr/bin/pkg-config" \
-	GIT_DIR=$(IW_DIR)
-
-define IW_CONFIGURE_CMDS
-	echo "CC = $(TARGET_CC)" >$(IW_CONFIG)
-	echo "CFLAGS = $(TARGET_CFLAGS)" >>$(IW_CONFIG)
-	echo "LDFLAGS = $(TARGET_LDFLAGS)" >>$(IW_CONFIG)
-endef
+	GIT_DIR="$(IW_DIR)" \
+	CC="$(TARGET_CC)" \
+	LD="$(TARGET_LD)" \
+	CFLAGS="$(TARGET_CFLAGS)" \
+	LDFLAGS="$(TARGET_LDFLAGS)"
 
 define IW_BUILD_CMDS
-	$(IW_MAKE_ENV) $(MAKE) -C $(@D)
+	$(IW_MAKE_ENV) $(MAKE) -C $(@D) V=1
 endef
 
 define IW_INSTALL_TARGET_CMDS
