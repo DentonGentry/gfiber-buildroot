b52637d86f0d2ebc3cb3c024988953f7bad8da53
Avery Pennarun <apenwarr@gmail.com>
2014-01-27 23:31:37 -0500

    libpcap and tcpdump: update to latest versions.
    The older version of tcpdump severely misparsed radiocap headers, leading to
    strange and misleading results when looking at wifi packets.  This one seems
    much better.
    
    Change-Id: Ibb149e10d82d1f362a170ac861f3bba55ce4bcf3

--- package/libpcap/libpcap.mk
+++ package/libpcap/libpcap.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBPCAP_VERSION = 1.2.1
+LIBPCAP_VERSION = 1.5.3
 LIBPCAP_SITE = http://www.tcpdump.org/release
 LIBPCAP_INSTALL_STAGING = YES
 # doesn't have an install-strip
@@ -13,8 +13,16 @@ LIBPCAP_INSTALL_TARGET_OPT= DESTDIR="$(TARGET_DIR)" \
 LIBPCAP_INSTALL_STAGING_OPT= DESTDIR="$(STAGING_DIR)" install \
 	$(if $(BR2_PREFER_STATIC_LIB),,install-shared)
 LIBPCAP_DEPENDENCIES = zlib
-LIBPCAP_CONF_ENV = ac_cv_linux_vers=2 \
+LIBPCAP_CONF_ENV = \
+		CFLAGS=$(LIBPCAP_CFLAGS) \
+		ac_cv_linux_vers=2 \
 		ac_cv_header_linux_wireless_h=yes # configure misdetects this
 LIBPCAP_CONF_OPT = --disable-yydebug --with-pcap=linux
+
+
+ifeq ($(BR2_PACKAGE_LIBNL),y)
+LIBPCAP_DEPENDENCIES += libnl
+LIBPCAP_CFLAGS += -I$(STAGING_DIR)/usr/include/libnl3
+endif
 
 $(eval $(call AUTOTARGETS))
