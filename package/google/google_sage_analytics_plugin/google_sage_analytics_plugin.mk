9c733d2c83891bf84450e2df7083b6f66de86944
Danny Tom <dtom@google.com>
2014-02-21 14:07:24 -0800

    Update sage analytics plugin.  Enable analytics logging at storage box. 	modified:   package/google/google_sage_analytics_plugin/google_sage_analytics_plugin.mk 	modified:   package/google/google_sageserver/runsage
    Change-Id: Ia677f2c01287b19edb80a8e7ca514ebeb65f5f84

--- package/google/google_sage_analytics_plugin/google_sage_analytics_plugin.mk
+++ package/google/google_sage_analytics_plugin/google_sage_analytics_plugin.mk
@@ -3,7 +3,7 @@
 # sage_analytics_plugin (GFiber Analytics plugin for Sage)
 #
 #############################################################
-GOOGLE_SAGE_ANALYTICS_PLUGIN_VERSION = 2013-12-13-00@58182386
+GOOGLE_SAGE_ANALYTICS_PLUGIN_VERSION = 2014-02-19-01@61839646
 GOOGLE_SAGE_ANALYTICS_PLUGIN_SITE = mpm://fiber/analytics/sage_plugin
 GOOGLE_SAGE_ANALYTICS_PLUGIN_BASE_NAME = google_sage_analytics_plugin
 GOOGLE_SAGE_ANALYTICS_PLUGIN_SOURCE = analytics-$(GOOGLE_SAGE_ANALYTICS_PLUGIN_VERSION).tar
