119c9de7a5924d145120ed41a78923679d9178de
Petri Gynther <pgynther@google.com>
2014-03-10 16:50:53 -0700

    Move BCM Bluetooth firmware
    1. Move BCM Bluetooth firmware from buildroot/fs/skeleton/etc/bsa
       to vendor/broadcom/drivers/fw
    2. Remove unused bsa_config.cgr
    3. Add build config options for BSA server and firmware
    4. Install firmware to /lib/firmware/brcm
    
    Change-Id: Ib56af88e271fef4d61a1d02e1a4a67faecb77667

--- package/bcm_bluetooth/bcm_bluetooth.mk
+++ package/bcm_bluetooth/bcm_bluetooth.mk
@@ -2,7 +2,8 @@ BCM_BLUETOOTH_SITE=repo://vendor/broadcom/drivers
 BCM_BLUETOOTH_INSTALL_TARGET=YES
 BCM_BLUETOOTH_DEPENDENCIES = alsa-lib alsa-utils bcm_nexus
 
-define BCM_BLUETOOTH_CLEAN_CMDS
+ifeq ($(BR2_PACKAGE_BCM_BLUETOOTH_BSA),y)
+define BCM_BLUETOOTH_BSA_CLEAN
 	$(TARGET_MAKE_ENV) $(MAKE1) \
 		CPU=mips \
 		MIPSGCC=$(TARGET_CC_NOCCACHE) \
@@ -12,7 +13,7 @@ define BCM_BLUETOOTH_CLEAN_CMDS
 		-C $(@D)/bt/3rdparty/embedded/google/bruno/server/build/
 endef
 
-define BCM_BLUETOOTH_BUILD_CMDS
+define BCM_BLUETOOTH_BSA_BUILD
 	$(BCM_MAKE_ENV) $(TARGET_MAKE_ENV) $(MAKE1) \
 		CPU=mips \
 		MIPSGCC=$(TARGET_CC_NOCCACHE) \
@@ -30,12 +31,43 @@ define BCM_BLUETOOTH_BUILD_CMDS
 		-C $(@D)/bt/3rdparty/embedded/brcm/linux/btusb/
 endef
 
-define BCM_BLUETOOTH_INSTALL_TARGET_CMDS
+define BCM_BLUETOOTH_BSA_INSTALL_TARGET
 	$(INSTALL) -m 0700 $(@D)/bt/3rdparty/embedded/google/bruno/server/build/mips/bsa_server $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0700 $(@D)/bt/3rdparty/embedded/google/bruno/app_gtv/build/mips/app_gtv $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -D -m 0600 $(@D)/bt/3rdparty/embedded/brcm/linux/btusb/btusb.ko $(TARGET_DIR)/usr/lib/modules/btusb.ko
 	$(INSTALL) -D -m 0600 $(@D)/bt/3rdparty/embedded/brcm/linux/bthid/bthid.ko $(TARGET_DIR)/usr/lib/modules/bthid.ko
 	$(INSTALL) -m 0600 $(@D)/bt/3rdparty/embedded/google/bruno/libbsa/build/mips/sharedlib/libbsa.so $(TARGET_DIR)/usr/lib/
+endef
+else
+define BCM_BLUETOOTH_BSA_CLEAN
+endef
+define BCM_BLUETOOTH_BSA_BUILD
+endef
+define BCM_BLUETOOTH_BSA_INSTALL_TARGET
+endef
+endif
+
+ifeq ($(BR2_PACKAGE_BCM_BLUETOOTH_FW),y)
+define BCM_BLUETOOTH_FW_INSTALL_TARGET
+	$(INSTALL) -D -m 0400 $(@D)/fw/BCM20702.hcd $(TARGET_DIR)/lib/firmware/brcm/BCM20702.hcd
+	$(INSTALL) -D -m 0400 $(@D)/fw/BCM20705.hcd $(TARGET_DIR)/lib/firmware/brcm/BCM20705.hcd
+endef
+else
+define BCM_BLUETOOTH_FW_INSTALL_TARGET
+endef
+endif
+
+define BCM_BLUETOOTH_CLEAN_CMDS
+	$(BCM_BLUETOOTH_BSA_CLEAN)
+endef
+
+define BCM_BLUETOOTH_BUILD_CMDS
+	$(BCM_BLUETOOTH_BSA_BUILD)
+endef
+
+define BCM_BLUETOOTH_INSTALL_TARGET_CMDS
+	$(BCM_BLUETOOTH_BSA_INSTALL_TARGET)
+	$(BCM_BLUETOOTH_FW_INSTALL_TARGET)
 endef
 
 $(eval $(call GENTARGETS))
