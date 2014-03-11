2fdf1c98e529ac737384c1668c6f265e964eb8e6
Avery Pennarun <apenwarr@gmail.com>
2014-03-11 02:15:27 -0400

    backports: split into package/backports and package/backports-custom.
    This change puts package/backports to backports-20140114, as it was
    before the ath10k fixes in the previous commit.  It removes the ath10k
    patches from there, and moves the current backports-custom (with ath10k
    support) to package/backports-custom.
    
    At the moment, stable ath10k is too different from the most recent versions
    of linux-backports.  We need the recent versions in order to support mwifiex
    and btmrvl for the sdio8897 chip.  So let's split the two builds for now.
    In a couple of months, the necessary patches for ath10k should have migrated
    to the head of the tree and we should be able to get rid of the separate
    package again.
    
    Change-Id: I6ccdda5c753933f69a5226c056e2c0cb76fb2105

---
+++ package/backports-custom/backports-custom.mk
@@ -0,0 +1,43 @@
+BACKPORTS_CUSTOM_VERSION = custom
+BACKPORTS_CUSTOM_SOURCE  = backports-$(BACKPORTS_CUSTOM_VERSION).tar.gz
+BACKPORTS_CUSTOM_SITE    = https://www.kernel.org/pub/linux/kernel/projects/backports/${BACKPORTS_CUSTOM_YYYY}/${BACKPORTS_CUSTOM_MM}/${BACKPORTS_CUSTOM_DD}
+BACKPORTS_CUSTOM_DEPENDENCIES = linux
+
+BACKPORTS_CUSTOM_MAKE_ENV = \
+	$(TARGET_MAKE_ENV) \
+	$(LINUX_MAKE_FLAGS) \
+	KLIB_BUILD=$(LINUX_DIR) \
+	KLIB=$(TARGET_DIR)
+
+define BACKPORTS_CUSTOM_CONFIGURE_CMDS
+	cp package/backports-custom/$(BR2_PACKAGE_BACKPORTS_CUSTOM_DEFCONFIG) $(@D)/.config
+	$(BACKPORTS_CUSTOM_MAKE_ENV) $(MAKE) -C $(@D) olddefconfig
+endef
+
+define BACKPORTS_CUSTOM_BUILD_CMDS
+	$(BACKPORTS_CUSTOM_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define BACKPORTS_CUSTOM_INSTALL_TARGET_CMDS
+	$(BACKPORTS_CUSTOM_MAKE_ENV) $(MAKE) -C $(LINUX_DIR) M=$(@D) \
+		INSTALL_MOD_DIR=backports \
+		modules_install
+endef
+
+# Convenience wrappers:
+# Do 'make backports-oldconfig' or 'make backports-menuconfig' from your out/
+# directory to run 'make oldconfig' or 'make menuconfig' in the backports
+# dir, as configured for your cross compiler and kernel.
+backports-custom-oldconfig backports-custom-menuconfig: backports-custom-configure
+	# intentionally don't use $(MAKE) here, to avoid $(LOGLINEAR)
+	$(BACKPORTS_CUSTOM_MAKE_ENV) make -C $(BACKPORTS_CUSTOM_DIR) \
+		$(patsubst backports-custom-%,%,$@) </dev/tty >/dev/tty 2>&1
+
+# Same as above, but for 'make savedefconfig'.  Also copies the new defconfig
+# into the defconfig file for this platform.
+backports-custom-savedefconfig:
+	$(BACKPORTS_CUSTOM_MAKE_ENV) $(MAKE) -C $(BACKPORTS_CUSTOM_DIR) savedefconfig
+	cp $(BACKPORTS_CUSTOM_DIR)/defconfig \
+		package/backports-custom/$(BR2_PACKAGE_BACKPORTS_CUSTOM_DEFCONFIG)
+
+$(eval $(call GENTARGETS))
