24af6d36aae159144ca82c7e9021ba1ff3b68e34
Jeffrey Kardatzke <jkardatzke@google.com>
2014-04-17 10:55:44 -0700

    Added dummy decoders for AC3, TrueHD, DTS, DTS-HD and DTS-MA
    This avoids having to include actual decode logic for
    all of these different audio formats in FFMPEG while
    still being able to identify the streams for proper passthrough.
    We also only include the codecs we are explicitly using now.
    
    Change-Id: Ia4290a3d4b7e4f2e4cfefed22139fd22bc55035a

--- package/google/google_ffmpeg/google_ffmpeg.mk
+++ package/google/google_ffmpeg/google_ffmpeg.mk
@@ -15,7 +15,6 @@ define GOOGLE_FFMPEG_INSTALL_TARGET_CMDS
         $(INSTALL) -D -m 0755 $(@D)/ffmpeg $(TARGET_DIR)/app/sage/
 endef
 
-# Dummy AC3 decoder is not integrated so it won't be detected/played back
 define GOOGLE_FFMPEG_CONFIGURE_CMDS
         (cd $(GOOGLE_FFMPEG_SRCDIR) && rm -rf config.cache && \
         $(TARGET_CONFIGURE_OPTS) \
@@ -38,9 +37,81 @@ define GOOGLE_FFMPEG_CONFIGURE_CMDS
                 --disable-devices \
                 --disable-demuxer=rtsp \
                 --disable-protocol=rtp \
+                --disable-decoders \
+                --enable-decoder=aac \
+                --enable-decoder=ac3dummy \
+                --enable-decoder=adpcm_4xm \
+                --enable-decoder=adpcm_ima_amv \
+                --enable-decoder=adpcm_ima_dk3 \
+                --enable-decoder=adpcm_ima_dk4 \
+                --enable-decoder=adpcm_ima_iss \
+                --enable-decoder=adpcm_ima_qt \
+                --enable-decoder=adpcm_ima_smjpeg \
+                --enable-decoder=adpcm_ima_wav \
+                --enable-decoder=adpcm_ima_ws \
+                --enable-decoder=adpcm_ms \
+                --enable-decoder=adpcm_sbpro_2 \
+                --enable-decoder=adpcm_sbpro_3 \
+                --enable-decoder=adpcm_sbpro_4 \
+                --enable-decoder=adpcm_swf \
+                --enable-decoder=adpcm_xa \
+                --enable-decoder=alac \
+                --enable-decoder=dcadummy \
+                --enable-decoder=dts_hddummy \
+                --enable-decoder=dts_madummy \
+                --enable-decoder=dvbsub \
+                --enable-decoder=dvdsub \
+                --enable-decoder=eac3dummy \
+                --enable-decoder=flac \
+                --enable-decoder=flv \
+                --enable-decoder=h264 \
+                --enable-decoder=mjpeg \
+                --enable-decoder=mlpdummy \
+                --enable-decoder=mp1 \
+                --enable-decoder=mp2 \
+                --enable-decoder=mp3 \
+                --enable-decoder=mp3adu \
+                --enable-decoder=mp3on4 \
+                --enable-decoder=mpeg1video \
+                --enable-decoder=mpeg2video \
+                --enable-decoder=mpeg4 \
+                --enable-decoder=mpegvideo \
+                --enable-decoder=msmpeg4 \
+                --enable-decoder=msmpeg4v3 \
+                --enable-decoder=pcm_alaw \
+                --enable-decoder=pcm_bd \
+                --enable-decoder=pcm_dvd \
+                --enable-decoder=pcm_f32be \
+                --enable-decoder=pcm_f32le \
+                --enable-decoder=pcm_f64be \
+                --enable-decoder=pcm_f64le \
+                --enable-decoder=pcm_mulaw \
+                --enable-decoder=pcm_s16be \
+                --enable-decoder=pcm_s16le \
+                --enable-decoder=pcm_s16l2_planar \
+                --enable-decoder=pcm_s24be \
+                --enable-decoder=pcm_s24daud \
+                --enable-decoder=pcm_s24le \
+                --enable-decoder=pcm_s32be \
+                --enable-decoder=pcm_s32le \
+                --enable-decoder=pcm_s8 \
+                --enable-decoder=pcm_u16be \
+                --enable-decoder=pcm_u16le \
+                --enable-decoder=pcm_u24be \
+                --enable-decoder=pcm_u24le \
+                --enable-decoder=pcm_u32be \
+                --enable-decoder=pcm_u32le \
+                --enable-decoder=pcm_u8 \
+                --enable-decoder=pcm_zork \
+                --enable-decoder=pgssub \
+                --enable-decoder=truehddummy \
+                --enable-decoder=vc1 \
+                --enable-decoder=vorbis \
+                --enable-decoder=wavpack \
+                --enable-decoder=wmv3 \
+                --enable-decoder=xsub \
                 $(GOOGLE_FFMPEG_CONF_OPT) \
         )
-        echo "#define CONFIG_AC3DUMMY_DECODER 0" >>  $(GOOGLE_FFMPEG_SRCDIR)/config.h
 endef
 
 # This is not in the official installed headers but we need it for audio playback
