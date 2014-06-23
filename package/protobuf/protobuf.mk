b2af1d619d86719ff2fd94d7373764a1558a58e8
Matthias Kaehlcke <mka@google.com>
2014-06-23 12:11:20 -0700

    protobuf: Build shared libraries
    Change-Id: I1a126408ce11bea3e587bcae55965ae7330de341

--- package/protobuf/protobuf.mk
+++ package/protobuf/protobuf.mk
@@ -11,7 +11,7 @@ PROTOBUF_LICENSE_FILES = COPYING.txt
 
 # N.B. Need to use host protoc during cross compilation.
 PROTOBUF_DEPENDENCIES = host-protobuf
-PROTOBUF_CONF_OPT = --with-protoc=$(HOST_DIR)/usr/bin/protoc --enable-static --disable-shared
+PROTOBUF_CONF_OPT = --with-protoc=$(HOST_DIR)/usr/bin/protoc --enable-static
 
 PROTOBUF_INSTALL_STAGING = YES
 
