68e8a7d2ab7cec3dd00e4ad115be65b59b0e4de2
Chema Gonzalez <chema@google.com>
2014-08-18 16:11:27 -0700

    buildroot: enable sshfs package
    We need to enable openssh in order to get the sftp server, but we do not
    want to enable the openssh ssh server itself.
    
    Tested:
    
    $ sshfs fb1:/var/media/ /media/fb1
    $ md5sum /media/fb1/ads/Monuments_Men-1080i.ind
    3e577d078e3f475ebf7318820175192f  /media/fb1/ads/Monuments_Men-1080i.ind
    
    Change-Id: Ibc5c9ef5409b551dc7fc1d932887d0d752875b03

--- package/openssh/openssh.mk
+++ package/openssh/openssh.mk
@@ -12,9 +12,22 @@ OPENSSH_CONF_OPT = --libexecdir=/usr/lib --disable-lastlog --disable-utmp \
 
 OPENSSH_DEPENDENCIES = zlib openssl
 
-define OPENSSH_INSTALL_INITSCRIPT
-	$(INSTALL) -D -m 755 package/openssh/S50sshd $(TARGET_DIR)/etc/init.d/S50sshd
+define OPENSSH_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 $(@D)/sftp-server $(TARGET_DIR)/usr/libexec/sftp-server
 endef
+
+# @note We are replacing the default install steps
+# with an explicit "install only sftp-server" command.
+# The rationale is that we need sftp-server in order
+# to make sshfs work, but we do not want to move from
+# dropbear to openssh (yet). Once we move, we should
+# remove the explicit install command, so that the
+# whole package gets installed.
+
+define OPENSSH_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/libexec/sftp-server
+endef
+
 
 OPENSSH_POST_INSTALL_TARGET_HOOKS += OPENSSH_INSTALL_INITSCRIPT
 
