9b4dd13400baffd28126a9e93166c2abff44ee52
Erdi Chen <erdi@google.com>
2015-01-07 10:51:15 -0800

    Build script for Spacecast package.
    Depend on host-golang until the toolchain binaries are checked in.
    Get and build third party Go dependencies in build step until packages
    are created for them.
    
    Change-Id: Ia699c4912ba4b150ce54c0045c4ac2b0af790e54

---
+++ package/google/google_spacecast/google_spacecast.mk
@@ -0,0 +1,36 @@
+#############################################################
+#
+# google_spacecast
+#
+#############################################################
+GOOGLE_SPACECAST_SITE = repo://vendor/google/spacecast
+GOOGLE_SPACECAST_DEPENDENCIES = host-golang
+
+define GOOGLE_SPACECAST_BUILD_CMDS
+	export $(GOLANG_ENV) ; \
+	export GOPATH=$(@D)/golib:$(@D)/go:$$GOPATH ; \
+	export CGO_ENABLED=1 ; \
+	go get -u code.google.com/p/go-uuid/uuid ; \
+	go get -u code.google.com/p/go.net/{netutil,ipv4,context} ; \
+	go get -u code.google.com/p/google-api-go-client/googleapi ; \
+	go get -u code.google.com/p/goprotobuf/proto ; \
+	go get -u github.com/golang/glog ; \
+	go get -u github.com/miekg/dns ; \
+	go get -u github.com/gonzojive/mdns ; \
+	cd $(@D) && go build spacecast/appliance
+endef
+
+define GOOGLE_SPACECAST_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/appliance \
+		$(TARGET_DIR)/app/spacecast/appliance
+	$(STRIPCMD) $(TARGET_DIR)/app/spacecast/appliance
+endef
+
+define GOOGLE_SPACECAST_CLEAN_CMDS
+	export $(GOLANG_ENV) ; \
+	export GOPATH=$(@D)/golib:$(@D)/go:$$GOPATH ; \
+	export CGO_ENABLED=1 ; \
+	cd $(@D) && $(HOST_DIR)/usr/bin/go clean spacecast/appliance
+endef
+
+$(eval $(call GENTARGETS))
