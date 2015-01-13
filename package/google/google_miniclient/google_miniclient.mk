3253cf27ed7f8bddde94d6e5e48f1c511ba8920e
Nick Begley <nbegley@google.com>
2015-01-13 18:01:21 -0500

    Added run-vudu script to miniclient to help open the Cast version of vudu.
    Change-Id: I46c341345c7ee41f9ac302bb5d2db0578312c8c3

--- package/google/google_miniclient/google_miniclient.mk
+++ package/google/google_miniclient/google_miniclient.mk
@@ -26,6 +26,7 @@ define GOOGLE_MINICLIENT_INSTALL_TARGET_CMDS
         $(INSTALL) -D -m 0755 package/google/google_miniclient/clear-app $(TARGET_DIR)/app/client/clear-app
         $(INSTALL) -D -m 0755 package/google/google_miniclient/run-app $(TARGET_DIR)/app/client/run-app
         $(INSTALL) -D -m 0755 package/google/google_miniclient/run-netflix $(TARGET_DIR)/app/client/run-netflix
+        $(INSTALL) -D -m 0755 package/google/google_miniclient/run-vudu $(TARGET_DIR)/app/client/run-vudu
         $(INSTALL) -D -m 0755 package/google/google_miniclient/runminiclient $(TARGET_DIR)/app/client/runminiclient
 endef
 
