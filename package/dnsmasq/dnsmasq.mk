787922f31ef476b590f1d9930b07f7f0c382b8df
Denton Gentry <dgentry@google.com>
2015-01-24 22:25:03 -0800

    dnsmasq: move to vendor/opensource/dnsmasq
    Change-Id: I3f3eed047bceeb92bd91250a43c1150731e76c74

--- package/dnsmasq/dnsmasq.mk
+++ package/dnsmasq/dnsmasq.mk
@@ -4,9 +4,7 @@
 #
 ################################################################################
 
-DNSMASQ_VERSION = 2.72
-DNSMASQ_SOURCE = dnsmasq-$(DNSMASQ_VERSION).tar.xz
-DNSMASQ_SITE = http://thekelleys.org.uk/dnsmasq
+DNSMASQ_SITE = repo://vendor/opensource/dnsmasq
 DNSMASQ_MAKE_ENV = $(TARGET_MAKE_ENV) CC="$(TARGET_CC)"
 DNSMASQ_MAKE_OPT = COPTS="$(DNSMASQ_COPTS)" PREFIX=/usr CFLAGS="$(TARGET_CFLAGS)"
 DNSMASQ_MAKE_OPT += DESTDIR=$(TARGET_DIR) LDFLAGS="$(TARGET_LDFLAGS)"
