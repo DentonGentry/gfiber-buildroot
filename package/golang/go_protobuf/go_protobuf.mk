44aa57d5c83f503849bc40932713a25c00ad6ba6
Erdi Chen <erdi@google.com>
2015-02-23 18:15:21 -0800

    Install Go protobuf compiler on host
    Change-Id: Iace9f2e1df19d4dbff320e2ab9688f7806b4ebd4

--- package/golang/go_protobuf/go_protobuf.mk
+++ package/golang/go_protobuf/go_protobuf.mk
@@ -16,3 +16,14 @@ endef
 GO_PROTOBUF_POST_PATCH_HOOKS += GO_PROTOBUF_FIX_PATH
 
 $(eval $(call GENTARGETS))
+
+# Install Go protobuf compiler on host
+define HOST_GO_PROTOBUF_INSTALL_CMDS
+	mkdir -p "$(@D)/go/src/github.com/golang"
+	ln -sT "$(@D)" "$(@D)/go/src/github.com/golang/protobuf"
+        export $(GOLANG_ENV) ; \
+	GOARCH= GOBIN="$(HOST_DIR)/usr/bin" GOPATH="$(@D)/go" \
+	go install github.com/golang/protobuf/protoc-gen-go
+endef
+
+$(eval $(call GENTARGETS,host))
