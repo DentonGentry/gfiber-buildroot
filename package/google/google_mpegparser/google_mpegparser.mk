41b92270e760f688beb1866480673f077e7f188f
Chris Kuiper <ckuiper@google.com>
2015-03-17 14:58:27 -0700

    buildroot: google_mpegparser: use STAGING dir instead of MCASTCAPTURE_PATH
    Change-Id: Iac107a8440558126b97afee6f53e7cab3797390c

--- package/google/google_mpegparser/google_mpegparser.mk
+++ package/google/google_mpegparser/google_mpegparser.mk
@@ -2,8 +2,8 @@ GOOGLE_MPEGPARSER_SITE=repo://vendor/google/mpegparser
 GOOGLE_MPEGPARSER_DEPENDENCIES=google_tvstreamparser google_mcastcapture
 
 define GOOGLE_MPEGPARSER_BUILD_CMDS
+        STAGING_DIR=$(STAGING_DIR) \
         TVSTREAMPARSER_PATH=$(GOOGLE_TVSTREAMPARSER_DIR)/tvstreamparser/ \
-        MCASTCAPTURE_PATH=$(GOOGLE_MCASTCAPTURE_DIR)/ \
         JAVA_HOME=$(GOOGLE_JAVA_HOME) \
         TARGET=$(TARGET_CROSS) $(MAKE) -C $(@D)
 endef
