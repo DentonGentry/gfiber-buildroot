6c9d6c4b23ccf00c0b35a7afb80aa4f9c791447e
Erdi Chen <erdi@google.com>
2015-03-25 18:34:39 -0700

    Update stunnel to 5.14 and new download URL.
    Change-Id: I321a1cbbf617faef5a0187f36b2d704dd88224ca

--- package/stunnel/stunnel.mk
+++ package/stunnel/stunnel.mk
@@ -4,8 +4,8 @@
 #
 #############################################################
 
-STUNNEL_VERSION = 4.44
-STUNNEL_SITE = http://ftp.nluug.nl/pub/networking/stunnel
+STUNNEL_VERSION = 5.14
+STUNNEL_SITE = https://www.stunnel.org/downloads
 STUNNEL_DEPENDENCIES = openssl
 STUNNEL_CONF_OPT += \
 	--with-ssl=$(STAGING_DIR)/usr \
