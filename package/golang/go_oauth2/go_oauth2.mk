e6932ca797658e6c512287cb25deeed141f11244
Zhiqun Chen <zchen@google.com>
2015-04-02 19:03:38 -0700

    download oauth package from golang
    Change-Id: I4e2615d8ea338fd6c77b4424fcee088e41d057f8

--- package/golang/go_oauth2/go_oauth2.mk
+++ package/golang/go_oauth2/go_oauth2.mk
@@ -3,14 +3,14 @@
 # go_oauth
 #
 #############################################################
-GO_OAUTH2_VERSION = 11c60b6f71a6ad48ed6f93c65fa4c6f9b1b5b46a
-GO_OAUTH2_SITE = https://github.com/golang/oauth2
+GO_OAUTH2_VERSION = c58fcf0ffc1c772aa2e1ee4894bc19f2649263b2
+GO_OAUTH2_SITE = https://go.googlesource.com/oauth2
 GO_OAUTH2_SITE_METHOD = git
 GO_OAUTH2_DEPENDENCIES = host-golang
 
 define GO_OAUTH2_FIX_PATH
-	mkdir -p "$(BUILD_DIR)/go_pkgs/src/github.com/golang"
-	ln -sT "$(@D)" "$(BUILD_DIR)/go_pkgs/src/github.com/golang/oauth2"
+	mkdir -p "$(BUILD_DIR)/go_pkgs/src/golang.org/x"
+	ln -sT "$(@D)" "$(BUILD_DIR)/go_pkgs/src/golang.org/x/oauth2"
 endef
 
 GO_OAUTH2_POST_PATCH_HOOKS += GO_OAUTH2_FIX_PATH
