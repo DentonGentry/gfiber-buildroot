35b3894a1a13716d51be34ab9d8ffa1dc95b2fda
Richard Frankel <rofrankel@google.com>
2015-04-15 19:46:45 -0400

    Add defconfig for Skids.
    Uses GFEX250, with "EX" for extender and 250 indicating that this is
    part of the CPE2.5 generation of devices.
    
    Change-Id: I4e0df5e7c34a8ee0a5bf07dee63e864dcf401c87

--- support/scripts/copy-skeleton.sh
+++ support/scripts/copy-skeleton.sh
@@ -19,7 +19,7 @@ case "$PLATFORM_PREFIX" in
   gfmn*)
     PLATFORM_SUFFIX=gfiberwc
     ;;
-  gftv*|gfrg*|gfibertv|kvm|gjcb*)
+  gftv*|gfrg*|gfibertv|kvm|gjcb*|gfex*)
     PLATFORM_SUFFIX=gfibertv
     ;;
   *)
