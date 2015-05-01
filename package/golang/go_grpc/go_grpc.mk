4edabe94d8e752bbb8b192261a39fb3daef47378
Alex Sukhanov <asukhanov@google.com>
2015-05-01 15:23:38 -0700

    Bring HTTP2 library
    Change-Id: Iaa1ea7d0e7e5b17ba7848b6b7968cb638c980a14

--- package/golang/go_grpc/go_grpc.mk
+++ package/golang/go_grpc/go_grpc.mk
@@ -6,7 +6,7 @@
 GO_GRPC_VERSION = 49257b21ef8b191d79fc732601e04eeb491b5bdc
 GO_GRPC_SITE = https://github.com/grpc/grpc-go
 GO_GRPC_SITE_METHOD = git
-GO_GRPC_DEPENDENCIES = host-golang
+GO_GRPC_DEPENDENCIES = host-golang go_http2 
 
 define GO_GRPC_FIX_PATH
 	mkdir -p "$(BUILD_DIR)/go_pkgs/src/google.golang.org/"
