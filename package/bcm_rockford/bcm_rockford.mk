ccf9b0854d593fbb3d3bae49245014dad03c78e3
John Newlin <jnewlin@google.com>
2015-05-01 07:17:12 -0700

    Fix for the 7250 platform.
    Change-Id: Icf097decaba5e969af1e3885b997e4fd63b46649

--- package/bcm_rockford/bcm_rockford.mk
+++ package/bcm_rockford/bcm_rockford.mk
@@ -5,6 +5,35 @@ BCM_ROCKFORD_INSTALL_STAGING=YES
 BCM_ROCKFORD_DEBUG=B_REFSW_DEBUG=n
 
 
+ifeq ($(BR2_PACKAGE_BCM_COMMON_PLATFORM),"97250")
+
+# 97250 and ?? chips
+define BCM_ROCKFORD_BUILD_CMDS
+	$(BCM_MAKE_ENV) $(BCM_ROCKFORD_DEBUG) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D)/middleware/vc5/driver -f V3DDriver.mk
+	$(BCM_MAKE_ENV) $(BCM_ROCKFORD_DEBUG) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D)/middleware/vc5/platform/nexus -f platform_nexus.mk
+	$(BCM_MAKE_ENV) $(BCM_ROCKFORD_DEBUG) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D)/applications/khronos/v3d/nexus/cube
+	$(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) -C $(@D)/lib/psip
+	$(BCM_ROCKFORD_BUILD_PLATFORM_SPECIFIC)
+endef
+
+define BCM_ROCKFORD_INSTALL_STAGING_CMDS
+	$(INSTALL) -m 755 -d $(STAGING_DIR)/usr/include/GLES
+	$(INSTALL) -m 755 -d $(STAGING_DIR)/usr/include/GLES2
+	$(INSTALL) -m 755 -d $(STAGING_DIR)/usr/include/EGL
+	$(INSTALL) -m 644 $(@D)/middleware/vc5/driver/interface/khronos/include/GLES/*.h $(STAGING_DIR)/usr/include/GLES/
+	$(INSTALL) -m 644 $(@D)/middleware/vc5/driver/interface/khronos/include/GLES2/*.h $(STAGING_DIR)/usr/include/GLES2/
+	$(INSTALL) -m 644 $(@D)/middleware/vc5/driver/interface/khronos/include/EGL/*.h $(STAGING_DIR)/usr/include/EGL/
+	$(INSTALL) -m 644 -D $(@D)/middleware/vc5/driver/interface/khronos/include/KHR/khrplatform.h $(STAGING_DIR)/usr/include/KHR/khrplatform.h
+endef
+
+define BCM_ROCKFORD_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(BCM_NEXUS_DIR)/bin/libv3ddriver.so $(TARGET_DIR)/usr/lib/libv3ddriver.so
+	$(INSTALL) -D $(BCM_NEXUS_DIR)/bin/libnxpl.so $(TARGET_DIR)/usr/lib/libnxpl.so
+endef
+
+# 97425 and 97428 chips
+else
+
 ifeq ($(BR2_PACKAGE_BCM_COMMON_PLATFORM),"97425")
 define BCM_ROCKFORD_BUILD_PLATFORM_SPECIFIC
 	PKG_CONFIG="$(PKG_CONFIG_HOST_BINARY)" \
@@ -34,5 +63,6 @@ define BCM_ROCKFORD_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(BCM_NEXUS_DIR)/bin/libv3ddriver.so $(TARGET_DIR)/usr/lib/libv3ddriver.so
 	$(INSTALL) -D $(BCM_NEXUS_DIR)/bin/libnxpl.so $(TARGET_DIR)/usr/lib/libnxpl.so
 endef
+endif
 
 $(eval $(call GENTARGETS))
