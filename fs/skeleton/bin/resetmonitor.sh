dda753fba7c22b9d2ef27ba7ab156c213aa2b1b2
Ed James <edjames@google.com>
2015-06-10 15:28:56 -0700

    gflt110: support for improved admin mode
    	* unique ssh key for actiontec
    	* used by factory for burning/RMA processing
    	* ssh enabled for 5 min after factory reset
    	* press and hold 10 secs to reset
    	* led flickers each second to help count
    	* led flashes quickly 10 times to confirm reset (actiontec request)
    	* https://docs.google.com/document/d/1nDY0C2xA0xva9bMNpe7bQgkVtJcVvB381F2aXPvocfk/edit#
    	* fix 'stop dropbear' to kill logos dropbear
    
    	burnin/RMA process:
    		- hook up ethernet
    		- press and hold button 10+ sec
    		- look for 10 flashes
    		- walk away
    		- automation will:
    			* ssh to 192.168.10.11
    			* log in with actiontec ssh key
    			* run 'enable_telnetd'
    			* connect via telnetd
    			* run testing scripts
    
    Change-Id: I292d91b946eb3b299e28281938288a1ce912b636

--- fs/skeleton/bin/resetmonitor.sh
+++ fs/skeleton/bin/resetmonitor.sh
@@ -1,50 +1,79 @@
 #!/bin/sh
 
-# Processes the output of the reset click handler.
-# 0-2s click means: reboot device
-# 2-10s click means:
-#   If this device is in mfg mode, start dropbear
-#   else reboot
-# More than 10s:  Remove PRODUCTION_MODE and reboot.
+# 3 modes:
+#  1: PRODUCTION_UNIT=0: mfg mode (1st boot in factory)
+#  2: PRODUCTION_UNIT unset: "factory reset" mode
+#  3: PRODUCTION_UNIT=1: normal mode
 
-while read -r line
+# press and hold button 10+sec to go to #2
+# #2 -> #3 after 5 minutes
+
+# Processes the output of the reset click handler.
+# <1s click means: show provisioning info
+# 1-9s click means: reboot device
+# 10+s:  Remove PRODUCTION_MODE and reboot (factory reset)
+
+DISABLE_FILE="/tmp/gpio/disable"
+
+while read -r type timer
 do
-	case "$line" in
-		buttondown)
-			# Set the led to solid red.
-			echo none >/sys/class/leds/sys-red/trigger
-			echo none >/sys/class/leds/sys-blue/trigger
-			echo 0 >/sys/class/leds/sys-red/brightness
-			echo 0 >/sys/class/leds/sys-blue/brightness
-			echo 1 >/sys/class/leds/sys-red/brightness
-			;;
-		click0)
-			echo "click0 received"
-			reboot
-			;;
-		click2)
-			echo "click 2 received."
-			if [ ! `sysvar_cmd -g PRODUCTION_UNIT` ]; then
-				echo "Not a production unit."
-				if [ ! -e /tmp/DEBUG ]; then
-					echo "tmpdebug does not exist, starting dropbear"
-					echo 1 >/tmp/DEBUG
-					/etc/init.d/S50dropbear start
-					rm /tmp/DEBUG
-				fi
-			else
-				echo "rebooting"
-				reboot
-			fi
-			;;
-		click10)
-			echo "click10 received."
-			sysvar_cmd -r PRODUCTION_UNIT
-			reboot
-			;;
-		*)
-			;;
-	esac
+  line="$type$timer"
+  echo "$0: $line received"
+
+  if [ "$type" = buttondown ]; then
+    # stop gpio-mailbox from writing to leds
+    touch $DISABLE_FILE
+  fi
+
+  case "$line" in
+    buttondown0)
+      # Set the led to solid red.
+      echo none >/sys/class/leds/sys-red/trigger
+      echo none >/sys/class/leds/sys-blue/trigger
+      echo 0 >/sys/class/leds/sys-blue/brightness
+      echo 1 >/sys/class/leds/sys-red/brightness
+      ;;
+    buttondown[1-9]*)
+      echo 0 >/sys/class/leds/sys-red/brightness
+      sleep .2
+      echo 1 >/sys/class/leds/sys-red/brightness
+      ;;
+    click0)
+      # show provision type
+      # TODO(edjames)
+      echo 0 >/sys/class/leds/sys-red/brightness
+      echo 0 >/sys/class/leds/sys-blue/brightness
+      for n in 0 100 0 100 0 100 0; do
+        echo $n >/sys/class/leds/sys-red/brightness
+        sleep .5
+      done
+      ;;
+    click[1-9])
+      echo "$0: rebooting"
+      reboot
+      ;;
+    click10 | click*)
+      echo "$0: click10 received."
+      # 10 quick pulses indicates reset succeeded
+      echo 0 >/sys/class/leds/sys-red/brightness
+      echo 0 >/sys/class/leds/sys-blue/brightness
+      for n in $(seq 10); do
+        echo 0 >/sys/class/leds/sys-red/brightness
+        sleep .1
+        echo 100 >/sys/class/leds/sys-red/brightness
+        sleep .1
+      done
+      sysvar_cmd -r PRODUCTION_UNIT
+      reboot
+      ;;
+    *)
+      ;;
+  esac
+
+  if [ "$type" = click ]; then
+    # restore gpio-mailbox
+    rm -f $DISABLE_FILE
+  fi
 done
 
 echo "exiting"
