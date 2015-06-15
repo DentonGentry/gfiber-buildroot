b100a2940060fa383194b8e5521ba167475ad8f5
Andrew Davies <awdavies@google.com>
2015-06-15 16:25:43 -0700

    WindCharger: Update art package to install nart as well.
    Change-Id: I566260202b064898bd0700af41674ac5bf13111b

--- package/art2/art2.mk
+++ package/art2/art2.mk
@@ -13,13 +13,37 @@ ART2_DEPENDENCIES:=linux
 define ART2_CONFIGURE_CMDS
 endef
 
-define ART2_BUILD_CMDS
+ifeq ($(BR2_PACKAGE_ART2_NART),y)
+define ART2_NART_BUILD
+	$(TARGET_MAKE_ENV) $(MAKE) \
+		-C $(@D)/art2 ART_ROOT=$(@D)/art2 WORKAREA=$(@D)/art2 \
+				TOOL_PREFIX=$(TARGET_CROSS) \
+		-f makefile.nart all
+endef
+ART2_POST_BUILD_HOOKS += ART2_NART_BUILD
+
+define ART2_NART_INSTALL
+	for i in $(@D)/art2/nartbuild/*.out; do \
+		$(INSTALL) -D -m 0755 "$$i" $(TARGET_DIR)/usr/sbin/; \
+	done
+	for i in $(@D)/art2/nartbuild/*.so; do \
+		$(INSTALL) -D -m 0755 "$$i" $(TARGET_DIR)/usr/lib/; \
+	done
+	for i in $(@D)/art2/BoardData/*.bin; do \
+		$(INSTALL) -D -m 0755 "$$i" $(TARGET_DIR)/usr/sbin/; \
+	done
+	$(INSTALL) -D -m 0755 package/art2/S99art2 $(TARGET_DIR)/etc/init.d/
+endef
+ART2_POST_INSTALL_TARGET_HOOKS += ART2_NART_INSTALL
+endif
+
+
 # TODO(awdavies) Some of these options might need to be turned off if this is
 # going to be used on something other than GFMN100
+define ART2_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) $(LINUX_MAKE_FLAGS) \
 		SUBDIRS="$(@D)/art2/driver/linux/modules" \
 		EXTRA_CFLAGS="$(TARGET_CFLAGS) \
-			-DUSE_PLATFORM_FRAMEWORK=1 \
 			-DCFG_64BIT=0 \
 			-DAP83 \
 			-DWASP \
@@ -33,7 +57,6 @@ define ART2_INSTALL_TARGET_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) $(LINUX_MAKE_FLAGS) \
 		SUBDIRS="$(@D)/art2/driver/linux/modules" \
 		EXTRA_CFLAGS="$(TARGET_CFLAGS) \
-			-DUSE_PLATFORM_FRAMEWORK=1 \
 			-DCFG_64BIT=0 \
 			-DAP83 \
 			-DWASP \
