0c66dcbcfa679e6bff4973358a525920388877e0
Richard Frankel <rofrankel@google.com>
2015-07-24 16:55:34 -0400

    ARC support.
    Change-Id: Ibfd9233caa0bdde90d3532b54a0cd5b15cdc7937

--- package/google/google.mk
+++ package/google/google.mk
@@ -1,2 +1,7 @@
-include package/google/*/*.mk
+SUB_MAKEFILES := $(wildcard package/google/*/*.mk)
 
+ifeq ($(BR2_arc),y)
+  SUB_MAKEFILES := $(filter-out package/google/google_dart_vm/google_dart_vm.mk, $(SUB_MAKEFILES))
+endif
+
+include $(SUB_MAKEFILES)
