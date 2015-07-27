c4fca44ed477cd9473da05a09d774602f0c65098
Denton Gentry <dgentry@google.com>
2015-07-27 06:49:28 -0700

    mv_app: Move files into package.
    Also simplify mv_app.mk slightly.
    
    Change-Id: Id4915fb090071a910fefb676a84c8567e7e91f7e

--- package/mv_app/mv_app.mk
+++ package/mv_app/mv_app.mk
@@ -11,14 +11,16 @@ define MV_APP_BUILD_CMDS
 endef
 
 define MV_APP_INSTALL_TARGET_CMDS
-	cp -fr $(@D)/build/bin/* $(TARGET_DIR)/usr/bin/. && \
-	cp -fr $(@D)/build/lib/* $(TARGET_DIR)/usr/lib/. && \
-	cp -fr $(@D)/build/ipc/* $(TARGET_DIR)/usr/lib/. && \
-	mkdir -p $(TARGET_DIR)/etc/xml_commands && \
-	cp -fr $(@D)/build/xml/xml_commands/* $(TARGET_DIR)/etc/xml_commands/. && \
+	cp -fr $(@D)/build/bin/* $(TARGET_DIR)/usr/bin/
+	cp -fr $(@D)/build/lib/* $(TARGET_DIR)/usr/lib/
+	cp -fr $(@D)/build/ipc/* $(TARGET_DIR)/usr/lib/
+	mkdir -p $(TARGET_DIR)/etc/xml_commands
+	cp -fr $(@D)/build/xml/xml_commands/* $(TARGET_DIR)/etc/xml_commands/
 	if [ -e "$(@D)/tools/omci_tool" ]; then \
-		cp -f  $(@D)/tools/omci_tool $(TARGET_DIR)/usr/bin/.; \
+		cp -f  $(@D)/tools/omci_tool $(TARGET_DIR)/usr/bin/; \
 	fi
+	mkdir -p $(TARGET_DIR)/etc/xml_params
+	cp -fpP package/mv_app/xml_params/* $(TARGET_DIR)/etc/xml_params/
 endef
 
 $(eval $(call GENTARGETS))
