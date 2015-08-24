97b7089a35098f8dd9f366dfeef9b1dbde7b25c1
John Newlin <jnewlin@google.com>
2015-08-24 07:31:53 -0700

    rsync needs package attr for attr/xattr.h
    Change-Id: I405058c21d7ba4e5720634ab8188e3b5b34b2b99

--- package/rsync/rsync.mk
+++ package/rsync/rsync.mk
@@ -5,6 +5,7 @@
 #############################################################
 
 RSYNC_VERSION = 3.0.9
+RSYNC_DEPENDENCIES=attr
 RSYNC_SOURCE = rsync-$(RSYNC_VERSION).tar.gz
 RSYNC_SITE = http://rsync.samba.org/ftp/rsync/src
 RSYNC_CONF_OPT = $(if $(BR2_ENABLE_DEBUG),--enable-debug,--disable-debug)
