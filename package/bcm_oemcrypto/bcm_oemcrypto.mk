758e9a797ae202a18683d12ba2c650bb53bbd5fc
Jean-Francois Thibert <jfthibert@google.com>
2015-09-29 17:51:22 -0400

    Add oemcrypto package
    Change-Id: Ib4cb076baa50f1a0e00245ac943d3b6014da4e57

---
+++ package/bcm_oemcrypto/bcm_oemcrypto.mk
@@ -0,0 +1,10 @@
+BCM_OEMCRYPTO_SITE=repo://vendor/broadcom/OEMCrypto
+BCM_OEMCRYPTO_CONFIGURE_CMDS=ln -sf $(@D)/BSEAV $(BUILD_DIR)/OEMCrypto
+BCM_OEMCRYPTO_DEPENDENCIES=bcm_nexus bcm_bseav
+
+define BCM_OEMCRYPTO_BUILD_CMDS
+	$(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) COMPILE_WVSOURCE=y USE_CURL=y -C $(@D)/../OEMCrypto/lib/security/third_party/widevine/CENC21 report
+	$(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) COMPILE_WVSOURCE=y USE_CURL=y -C $(@D)/../OEMCrypto/lib/security/third_party/widevine/CENC21
+endef
+
+$(eval $(call GENTARGETS))
