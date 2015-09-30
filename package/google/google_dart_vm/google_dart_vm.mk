3a9ba562111f9bc7bdd878c9553468abbe5c7023
Jason Daly <dalyj@google.com>
2015-09-29 18:30:07 -0700

    Update DartVM to stable version 1.12.1
    Change-Id: I207aec50c4300f518c5a32654e633ac50b349d1d

--- package/google/google_dart_vm/google_dart_vm.mk
+++ package/google/google_dart_vm/google_dart_vm.mk
@@ -1,6 +1,6 @@
-GOOGLE_DART_VM_SITE=http://dart.googlecode.com/svn/branches/1.8/deps/standalone.deps
+GOOGLE_DART_VM_SITE=https://github.com/dart-lang/sdk.git
 GOOGLE_DART_VM_SITE_METHOD=gclient
-GOOGLE_DART_VM_VERSION=42828
+GOOGLE_DART_VM_VERSION=af705ac989a359ef369df66de455ef934bb40634
 GOOGLE_DART_VM_INSTALL_STAGING=YES
 
 ifeq ($(ARCH),arm)
@@ -28,22 +28,22 @@ CXX.target="$(TARGET_CXX)" \
 AR.target="$(TARGET_AR)"
 
 define GOOGLE_DART_VM_BUILD_CMDS
-	$(MAKE) -C $(BUILD_DIR)/$($(PKG)_BASE_NAME)/dart $(GOOGLE_DART_VM_MAKE_VARS)
-	cp $(BUILD_DIR)/$($(PKG)_BASE_NAME)/dart/out/$(GOOGLE_DART_VM_BTYPE)/dart $(BUILD_DIR)/$($(PKG)_BASE_NAME)/dart/out/$(GOOGLE_DART_VM_BTYPE)/dart_nostrip
-	$(TARGET_STRIP) $(BUILD_DIR)/$($(PKG)_BASE_NAME)/dart/out/$(GOOGLE_DART_VM_BTYPE)/dart
+	$(MAKE) -C $(BUILD_DIR)/$($(PKG)_BASE_NAME)/sdk $(GOOGLE_DART_VM_MAKE_VARS) runtime
+	cp $(BUILD_DIR)/$($(PKG)_BASE_NAME)/sdk/out/$(GOOGLE_DART_VM_BTYPE)/dart $(BUILD_DIR)/$($(PKG)_BASE_NAME)/sdk/out/$(GOOGLE_DART_VM_BTYPE)/dart_nostrip
+	$(TARGET_STRIP) $(BUILD_DIR)/$($(PKG)_BASE_NAME)/sdk/out/$(GOOGLE_DART_VM_BTYPE)/dart
 endef
 
 
 define GOOGLE_DART_VM_INSTALL_STAGING_CMDS
 	mkdir -p $(STAGING_DIR)/usr/local/include
-	$(INSTALL) -D -m 0644 $(BUILD_DIR)/$($(PKG)_BASE_NAME)/dart/runtime/include/dart_api.h $(STAGING_DIR)/usr/local/include
-	$(INSTALL) -D -m 0644 $(BUILD_DIR)/$($(PKG)_BASE_NAME)/dart/runtime/include/dart_native_api.h $(STAGING_DIR)/usr/local/include
-	$(INSTALL) -D -m 0644 $(BUILD_DIR)/$($(PKG)_BASE_NAME)/dart/runtime/include/dart_mirrors_api.h $(STAGING_DIR)/usr/local/include
+	$(INSTALL) -D -m 0644 $(BUILD_DIR)/$($(PKG)_BASE_NAME)/sdk/runtime/include/dart_api.h $(STAGING_DIR)/usr/local/include
+	$(INSTALL) -D -m 0644 $(BUILD_DIR)/$($(PKG)_BASE_NAME)/sdk/runtime/include/dart_native_api.h $(STAGING_DIR)/usr/local/include
+	$(INSTALL) -D -m 0644 $(BUILD_DIR)/$($(PKG)_BASE_NAME)/sdk/runtime/include/dart_mirrors_api.h $(STAGING_DIR)/usr/local/include
 endef
 
 
 define GOOGLE_DART_VM_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 -D $(BUILD_DIR)/$($(PKG)_BASE_NAME)/dart/out/$(GOOGLE_DART_VM_BTYPE)/dart $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 -D $(BUILD_DIR)/$($(PKG)_BASE_NAME)/sdk/out/$(GOOGLE_DART_VM_BTYPE)/dart $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(call GENTARGETS))
