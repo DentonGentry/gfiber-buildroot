77cd76d82a1528acd3343660f703ebfa767db000
Petri Gynther <pgynther@google.com>
2015-11-10 13:00:00 -0800

    Update netfilter libraries
    Update netfilter libraries for Optimus connection manager (cmm).
    
    Change-Id: I9786c79640f69c6f32028df570c808f82dbdbbf3

--- package/libnetfilter_conntrack/libnetfilter-conntrack.mk
+++ package/libnetfilter_conntrack/libnetfilter-conntrack.mk
@@ -4,13 +4,11 @@
 #
 ################################################################################
 
-# Mindspeed's SDK 2.8.5 uses version 0.9.1 of libnetfilter-conntrack. Let's use
-# the same.
-LIBNETFILTER_CONNTRACK_VERSION = 0.9.1
+LIBNETFILTER_CONNTRACK_VERSION = 1.0.5
 LIBNETFILTER_CONNTRACK_SOURCE = libnetfilter_conntrack-$(LIBNETFILTER_CONNTRACK_VERSION).tar.bz2
 LIBNETFILTER_CONNTRACK_SITE = http://www.netfilter.org/projects/libnetfilter_conntrack/files
 LIBNETFILTER_CONNTRACK_INSTALL_STAGING = YES
-LIBNETFILTER_CONNTRACK_DEPENDENCIES = host-pkg-config libnfnetlink
+LIBNETFILTER_CONNTRACK_DEPENDENCIES = host-pkg-config libnfnetlink libmnl
 LIBNETFILTER_CONNTRACK_AUTORECONF = YES
 LIBNETFILTER_CONNTRACK_LICENSE = GPLv2+
 LIBNETFILTER_CONNTRACK_LICENSE_FILES = COPYING
--- package/libnfnetlink/libnfnetlink.mk
+++ package/libnfnetlink/libnfnetlink.mk
@@ -4,8 +4,7 @@
 #
 ################################################################################
 
-# Mindspeed's SDK 2.8.5 uses version 1.0.0 of libnfnetlink. Let's use the same.
-LIBNFNETLINK_VERSION = 1.0.0
+LIBNFNETLINK_VERSION = 1.0.1
 LIBNFNETLINK_SOURCE = libnfnetlink-$(LIBNFNETLINK_VERSION).tar.bz2
 LIBNFNETLINK_SITE = http://www.netfilter.org/projects/libnfnetlink/files
 LIBNFNETLINK_AUTORECONF = YES
