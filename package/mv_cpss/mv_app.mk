00edd77cba3be59d78bfbd783ea78806e54ce803
Ed James <edjames@google.com>
2015-11-21 17:37:55 -0800

    cpss: add marvell's switch config software tool to gfch100 build
    Change-Id: I0a74c1d8a7d28af222163432e657e732549e8c90

---
+++ package/mv_cpss/mv_app.mk
@@ -0,0 +1,21 @@
+MV_CPSS_SITE=repo://vendor/marvell/cpss
+MV_CPSS_DEPENDENCIES=linux
+
+MV_CPSS_ENV = \
+	TARGET_CROSS=$(TARGET_CROSS) \
+	STAGING_DIR=$(STAGING_DIR) \
+	BR2_JLEVEL=$(BR2_JLEVEL) \
+
+define MV_CPSS_CONFIGURE_CMDS
+	$(MV_CPSS_ENV) $(MAKE) -C $(@D) configure
+endef
+
+define MV_CPSS_BUILD_CMDS
+	$(MV_CPSS_ENV) $(MAKE) -C $(@D) build
+endef
+
+define MV_CPSS_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/cpss $(TARGET_DIR)/usr/bin/cpss
+endef
+
+$(eval $(call GENTARGETS))
