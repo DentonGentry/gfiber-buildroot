542daf23752ee39a77d6b1535ddf9dcef1094b43
Richard Frankel <rofrankel@google.com>
2015-12-08 16:31:34 -0500

    Creates a package for pywpactrl.
    This package contains Python bindings for the wpa_supplicant control
    interface.
    
    Change-Id: Ia4a7da7d2e429863547da5fd9b651cedfa8bf52a

---
+++ package/py/py-wpactrl/py-wpactrl.mk
@@ -0,0 +1,12 @@
+#############################################################
+#
+# py-wpactrl
+#
+#############################################################
+
+PY_WPACTRL_VERSION =master
+PY_WPACTRL_SITE = git://github.com/neculaesei/pywpactrl
+
+PY_WPACTRL_DEPENDENCIES=python-setuptools
+
+$(eval $(call PYTARGETS))
