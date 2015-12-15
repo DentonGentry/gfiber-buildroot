a2cfc01b996b7277ebe86a724f0715dfcca4a7db
Richard Frankel <rofrankel@google.com>
2015-12-14 20:10:53 -0500

    pyinotify:  Switch from inotify to pyinotify.
    pyinotify has approximately 100x the usage, is already used by
    Catawampus, and doesn't require a patch to load libc on Bruno.
    
    Change-Id: I2b3b65ef04fe91ba4e364c4cb1356854caee19d3

--- package/py/py-inotify/py-inotify.mk
+++ package/py/py-inotify/py-inotify.mk
@@ -1,6 +1,6 @@
-PY_INOTIFY_VERSION = 0.2.4
-PY_INOTIFY_SOURCE  = inotify-$(PY_INOTIFY_VERSION).tar.gz
-PY_INOTIFY_SITE    = http://pypi.python.org/packages/source/i/inotify
+PY_INOTIFY_VERSION = 0.9.6
+PY_INOTIFY_SOURCE  = pyinotify-$(PY_INOTIFY_VERSION).tar.gz
+PY_INOTIFY_SITE    = http://pypi.python.org/packages/source/p/pyinotify
 PY_INOTIFY_DEPENDENCIES = python python-setuptools
 
 $(eval $(call PYTARGETS))
