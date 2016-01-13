4ff69d4622200c88fcb05c507a966aa9f371ff44
Will Angley <willangley@google.com>
2016-01-13 18:38:09 -0500

    Update iptables to 1.4.21.
    Modernizing iptables gets us access to NAT in ipv6, which is required
    for our captive portal work.
    
    Also pull in patches from Buildroot master that enable it to build.
    
    Change-Id: I5b5db3e1f8fb47397700007abdef84e53675338e

--- package/iptables/iptables.mk
+++ package/iptables/iptables.mk
@@ -4,13 +4,16 @@
 #
 #############################################################
 
-IPTABLES_VERSION = 1.4.12.2
+IPTABLES_VERSION = 1.4.21
 IPTABLES_SOURCE = iptables-$(IPTABLES_VERSION).tar.bz2
 IPTABLES_SITE = http://ftp.netfilter.org/pub/iptables
 IPTABLES_INSTALL_STAGING = YES
-IPTABLES_DEPENDENCIES = host-pkg-config
+IPTABLES_DEPENDENCIES = host-pkg-config \
+	$(if $(BR2_PACKAGE_LIBNETFILTER_CONNTRACK),libnetfilter_conntrack)
 
 IPTABLES_CONF_OPT = --libexecdir=/usr/lib --with-kernel=$(LINUX_HEADERS_DIR)
+# Because of iptables-01-fix-static-link.patch
+IPTABLES_AUTORECONF = YES
 
 define IPTABLES_TARGET_SYMLINK_CREATE
 	ln -sf xtables-multi $(TARGET_DIR)/usr/sbin/iptables
