169965662cb7b9b430c08c76480aabc2e3f4d50e
Erdi Chen <erdi@google.com>
2016-02-17 13:31:20 -0800

    Add gtest rules for target build
    Change-Id: Iaf3841a8918af0f0ca516b134a6acd464bd996ca

--- package/gtest/gtest.mk
+++ package/gtest/gtest.mk
@@ -8,6 +8,7 @@ GTEST_SITE = http://googletest.googlecode.com/svn/trunk
 GTEST_SITE_METHOD = svn
 # Release 1.6.0
 GTEST_VERSION = 573
+GTEST_INSTALL_STAGING = YES
 
 define HOST_GTEST_BUILD_CMDS
 	CC="$(HOSTCC)" CXX="$(HOSTCXX)" CFLAGS="$(HOST_CFLAGS) -fPIC" \
@@ -27,3 +28,22 @@ define HOST_GTEST_UNINSTALL_CMDS
 endef
 
 $(eval $(call GENTARGETS,host))
+
+define GTEST_BUILD_CMDS
+	CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" CFLAGS="$(TARGET_CFLAGS) -fPIC" \
+	   CXXFLAGS="$(TARGET_CXXFLAGS) -fPIC" LDFLAGS="$(TARGET_LDFLAGS)" \
+	   $(MAKE) -C $(@D)/make
+endef
+
+define GTEST_INSTALL_STAGING_CMDS
+	mkdir -p $(STAGING_DIR)/usr/include
+	ln -sf $(@D)/include/gtest $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D $(@D)/make/gtest_main.a $(STAGING_DIR)/usr/lib/libgtest.a
+endef
+
+define GTEST_UNINSTALL_STAGING_CMDS
+	rm -f $(STAGING_DIR)/usr/include/gtest
+	rm -f $(STAGING_DIR)/usr/lib/libgtest.a
+endef
+
+$(eval $(call GENTARGETS))
