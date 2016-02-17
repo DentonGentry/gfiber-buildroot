fccd0e2abeff1f7f246fcbb9f5649572299ab771
Erdi Chen <erdi@google.com>
2016-02-17 13:50:59 -0800

    Add libweave package
    Change-Id: I99afdc6be1ed66d4fe0dcac606ebdfeeccde551a

---
+++ package/libweave/libweave.mk
@@ -0,0 +1,33 @@
+#############################################################
+#
+# libweave
+#
+#############################################################
+
+LIBWEAVE_VERSION = weave-release-1.2
+LIBWEAVE_SITE = https://weave.googlesource.com/weave/libweave
+LIBWEAVE_SITE_METHOD = git
+LIBWEAVE_INSTALL_STAGING = YES
+LIBWEAVE_DEPENDENCIES = gtest
+LIBWEAVE_BUILD_MODE = Release
+
+define LIBWEAVE_CONFIGURE_CMDS
+	ln -fs "$(STAGING_DIR)/usr/include/gtest" "$(@D)"/include
+endef
+
+define LIBWEAVE_BUILD_CMDS
+        export PATH=$(TARGET_PATH):$$PATH; \
+	BUILD_MODE=$(LIBWEAVE_BUILD_MODE) \
+        $(MAKE) -C "$(@D)" -e CC="$(TARGET_CC)" -e CXX="$(TARGET_CXX)" out/$(LIBWEAVE_BUILD_MODE)/libweave.so
+endef
+
+define LIBWEAVE_INSTALL_STAGING_CMDS
+	ln -sf "$(@D)/include/weave" "$(STAGING_DIR)/usr/include"
+	$(INSTALL) -D "$(@D)/out/$(LIBWEAVE_BUILD_MODE)/libweave.so" "$(TARGET_DIR)/usr/lib/libweave.so"
+endef
+
+define LIBWEAVE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D "$(@D)/out/$(LIBWEAVE_BUILD_MODE)/libweave.so" "$(TARGET_DIR)/usr/lib/libweave.so"
+endef
+
+$(eval $(call GENTARGETS))
