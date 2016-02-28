c324e9329f2e8189c584ca07f5727d7241ce039b
Mike Mu <mikemu@google.com>
2016-02-28 17:50:06 -0500

    quantenna: move Makefile to package.
    Change-Id: I3b9556123fd4c3b91649ee023ca3ae5f9ccb71d4

--- package/quantenna_drivers/quantenna_drivers.mk
+++ package/quantenna_drivers/quantenna_drivers.mk
@@ -1,47 +1,28 @@
 QUANTENNA_DRIVERS_SITE=repo://vendor/quantenna/drivers
 QUANTENNA_DRIVERS_DEPENDENCIES=linux
 
+QUANTENNA_DRIVERS_MAKE_ENV = \
+	$(TARGET_MAKE_ENV) \
+	LINUX_DIR="$(LINUX_DIR)" \
+	TARGET_DIR="$(TARGET_DIR)" \
+	TARGET_CC="$(TARGET_CC)" \
+	TARGET_CFLAGS="$(TARGET_CFLAGS)" \
+	TARGET_STRIP="$(TARGET_STRIP)" \
+
 ifeq ($(BR2_PACKAGE_QUANTENNA_DRIVERS_PCIE_HOST),y)
-
-define QUANTENNA_DRIVERS_BUILD_CMDS
-	$(MAKE) -C $(@D)/quantenna-kmsgd CC="$(TARGET_CC)" quantenna-kmsgd
-	$(MAKE) -C $(@D)/libqcsapi_client CC="$(TARGET_CC)"
-endef
-
-define QUANTENNA_DRIVERS_INSTALL_TARGET_CMDS
-	cp -a $(@D)/topaz-linux.lzma.img $(TARGET_DIR)/lib/firmware
-	cp -a $(@D)/quantenna-kmsgd/quantenna-kmsgd $(TARGET_DIR)/usr/bin
-	cp -a $(@D)/libqcsapi_client/qcsapi_pcie_static $(TARGET_DIR)/usr/bin
-endef
-
+	QUANTENNA_DRIVERS_TARGET = pcie_host
 else ifeq ($(BR2_PACKAGE_QUANTENNA_DRIVERS_PCIE_MODULE),y)
-
-define QUANTENNA_DRIVERS_BUILD_CMDS
-	$(MAKE) -C $(@D)/kmsgcat CC="$(TARGET_CC)" kmsgcat
-	$(MAKE) MAKEFLAGS="-j1" -C $(@D)/qcsapi-1.0.1 EXTRA_PROGS="call_qcsapi_rpcd qcsapi_rpcd" CC="$(TARGET_CC)" STRIP="$(TARGET_STRIP)" XCFLAGS="$(TARGET_CFLAGS)" PREFIX="$(TARGET_DIR)" build_all
-endef
-
-define QUANTENNA_DRIVERS_INSTALL_TARGET_CMDS
-	cp -a $(@D)/fs/common/* $(TARGET_DIR)
-	cp -a $(@D)/fs/pcie_module/* $(TARGET_DIR)
-	cp -a $(@D)/kmsgcat/kmsgcat $(TARGET_DIR)/usr/bin
-	$(MAKE) MAKEFLAGS="-j1" -C $(@D)/qcsapi-1.0.1 EXTRA_PROGS="call_qcsapi_rpcd qcsapi_rpcd" CC="$(TARGET_CC)" STRIP="$(TARGET_STRIP)" XCFLAGS="$(TARGET_CFLAGS)" PREFIX="$(TARGET_DIR)" install
-endef
-
+	QUANTENNA_DRIVERS_TARGET = pcie_module
 else ifeq ($(BR2_PACKAGE_QUANTENNA_DRIVERS_STANDALONE),y)
+	QUANTENNA_DRIVERS_TARGET = standalone
+endif
 
 define QUANTENNA_DRIVERS_BUILD_CMDS
-	$(MAKE) -C $(@D)/kmsgcat CC="$(TARGET_CC)" kmsgcat
-	$(MAKE) MAKEFLAGS="-j1" -C $(@D)/qcsapi-1.0.1 CC="$(TARGET_CC)" STRIP="$(TARGET_STRIP)" XCFLAGS="$(TARGET_CFLAGS)" PREFIX="$(TARGET_DIR)" build_all
+	$(QUANTENNA_DRIVERS_MAKE_ENV) $(MAKE) -C $(@D) $(QUANTENNA_DRIVERS_TARGET)
 endef
 
 define QUANTENNA_DRIVERS_INSTALL_TARGET_CMDS
-	cp -a $(@D)/fs/common/* $(TARGET_DIR)
-	cp -a $(@D)/fs/standalone/* $(TARGET_DIR)
-	cp -a $(@D)/kmsgcat/kmsgcat $(TARGET_DIR)/usr/bin
-	$(MAKE) MAKEFLAGS="-j1" -C $(@D)/qcsapi-1.0.1 CC="$(TARGET_CC)" STRIP="$(TARGET_STRIP)" XCFLAGS="$(TARGET_CFLAGS)" PREFIX="$(TARGET_DIR)" install
+	$(QUANTENNA_DRIVERS_MAKE_ENV) $(MAKE) -C $(@D) $(QUANTENNA_DRIVERS_TARGET)_install
 endef
-
-endif
 
 $(eval $(call GENTARGETS))
