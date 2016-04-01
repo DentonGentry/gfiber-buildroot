9be26aa556f6828bdf55e5c2371fc3f61d57cb09
Jean-Francois Thibert <jfthibert@google.com>
2016-04-01 11:12:00 -0400

    Only build oemcrypto in oemcrypto package
    Change-Id: I90639d6d424574717cb24b66450766a0e98aa20f

--- package/bcm_oemcrypto/bcm_oemcrypto.mk
+++ package/bcm_oemcrypto/bcm_oemcrypto.mk
@@ -4,7 +4,7 @@ BCM_OEMCRYPTO_DEPENDENCIES=bcm_nexus bcm_bseav
 
 define BCM_OEMCRYPTO_BUILD_CMDS
 	$(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) COMPILE_WVSOURCE=y USE_CURL=y -C $(@D)/../OEMCrypto/lib/security/third_party/widevine/CENC21 report
-	$(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) COMPILE_WVSOURCE=y USE_CURL=y -C $(@D)/../OEMCrypto/lib/security/third_party/widevine/CENC21
+	$(BCM_MAKE_ENV) $(MAKE) $(BCM_MAKEFLAGS) COMPILE_WVSOURCE=y USE_CURL=y -C $(@D)/../OEMCrypto/lib/security/third_party/widevine/CENC21 oemcrypto oec_unittest
 endef
 
 $(eval $(call GENTARGETS))
