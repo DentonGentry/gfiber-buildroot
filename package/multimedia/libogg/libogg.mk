1295f89c16e10cc8b17b1e352e40ed9723268a57
Jean-Francois Thibert <jfthibert@google.com>
2016-07-08 13:17:26 -0400

    Update libogg library to version 1.3.1
    Change-Id: I7c67f87e051bc147903d6345b822ce3a6620f231

--- package/multimedia/libogg/libogg.mk
+++ package/multimedia/libogg/libogg.mk
@@ -3,7 +3,7 @@
 # libogg
 #
 #############################################################
-LIBOGG_VERSION = 1.3.0
+LIBOGG_VERSION = 1.3.1
 LIBOGG_SOURCE = libogg-$(LIBOGG_VERSION).tar.gz
 LIBOGG_SITE = http://downloads.xiph.org/releases/ogg
 LIBOGG_INSTALL_STAGING = YES
