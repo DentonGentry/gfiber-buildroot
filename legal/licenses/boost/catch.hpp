2dd7a26a85d6126c9b09358a2f9200034a384e4e
Ed James <edjames@google.com>
2016-07-28 08:41:09 -0700

    gfch100: add licenses for craftui and glaukus 3rd party bits
    	* b/26764634
    
    Change-Id: I25c75e6a56227d8ea874c91f85ed2699df553f36

---
+++ legal/licenses/boost/catch.hpp
@@ -0,0 +1,10 @@
+/*
+ *  Catch v1.2.1
+ *  Generated: 2015-06-30 18:23:27.961086
+ *  ----------------------------------------------------------
+ *  This file has been merged from multiple headers. Please don't edit it directly
+ *  Copyright (c) 2012 Two Blue Cubes Ltd. All rights reserved.
+ *
+ *  Distributed under the Boost Software License, Version 1.0. (See accompanying
+ *  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
---
+++ legal/licenses/requirejs/require.js
@@ -0,0 +1,5 @@
+/*
+ RequireJS 2.1.16 Copyright (c) 2010-2015, The Dojo Foundation All Rights Reserved.
+ Available via the MIT or new BSD license.
+ see: http://github.com/jrburke/requirejs for details
+*/
