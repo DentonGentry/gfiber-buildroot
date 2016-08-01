b84659baf0068c2cdbddc5e4649d6b2098aa1759
Chris Kuiper <ckuiper@google.com>
2016-08-01 16:01:50 -0700

    google_licenses: Get playready.bin from BSEAV source folder.
    Change-Id: Ie72fce0af26bc028f8b3860547739a0784158ac9

--- package/google/google_licenses/google_licenses.mk
+++ package/google/google_licenses/google_licenses.mk
@@ -1,6 +1,6 @@
 GOOGLE_LICENSES_SITE_METHOD=null
 
-GOOGLE_LICENSES_DEPENDENCIES = host-google_keystore_client
+GOOGLE_LICENSES_DEPENDENCIES = bcm_bseav
 
 define GOOGLE_LICENSES_EXTRACT_CMDS
 	mkdir -p $(TARGET_DIR)/usr/local/licenses
@@ -9,28 +9,21 @@ endef
 
 ifeq ($(BR2_PACKAGE_GOOGLE_LICENSES),y)
 
-ifeq ($(BR2_PACKAGE_GOOGLE_PROD),y)
-  ifeq ($(BR2_TARGET_GENERIC_PLATFORM_NAME),"gfibertv")
-    GOOGLE_LICENSES_LICTYPE=playready2_prod_license
-  endif
-  ifeq ($(BR2_TARGET_GENERIC_PLATFORM_NAME),"gftv200")
-    GOOGLE_LICENSES_LICTYPE=playready2_gfhd200_prod_license
-  endif
-else
-  ifeq ($(BR2_TARGET_GENERIC_PLATFORM_NAME),"gfibertv")
-    GOOGLE_LICENSES_LICTYPE=playready2_dev_license
-  endif
-  ifeq ($(BR2_TARGET_GENERIC_PLATFORM_NAME),"gftv200")
-    GOOGLE_LICENSES_LICTYPE=playready2_gfhd200_dev_license
-  endif
+ifeq ($(BR2_TARGET_GENERIC_PLATFORM_NAME),"gfibertv")
+  GOOGLE_PLAYREADY_LIC_NAME=playready_2_5_gfhd100_license
 endif
-GOOGLE_KEYSTORE_CLIENT_NEEDS_KEYS += $(GOOGLE_LICENSES_LICTYPE)
+ifeq ($(BR2_TARGET_GENERIC_PLATFORM_NAME),"gftv200")
+  GOOGLE_PLAYREADY_LIC_NAME=playready_2_5_gfhd200_license
+endif
+TARGET_FILE=$(TARGET_DIR)/usr/local/licenses/playready.bin
 
 endif
 
 
 define GOOGLE_LICENSES_INSTALL_TARGET_CMDS
-	$(call GOOGLE_KEYSTORE_CLIENT_EXECUTE,$(GOOGLE_LICENSES_LICTYPE),$(TARGET_DIR)/usr/local/licenses/playready.bin)
+	[ -f $(TARGET_FILE) ] && chmod 0600 $(TARGET_FILE); \
+	cp $(BCM_BSEAV_DIR)/gfiber/$(GOOGLE_PLAYREADY_LIC_NAME) $(TARGET_FILE) && \
+	chmod 0400 $(TARGET_FILE)
 endef
 
 $(eval $(call GENTARGETS))
