a42cf22b4ca8f4f90aa0c15f9e4bd2456b0cf49b
Denton Gentry <dgentry@google.com>
2016-08-21 00:40:43 -0700

    bluez_utils: revamp RCU Audio support
    :s/BUILD_IBEACON/BUILD_BLUETOOTH/
    There are a number of utilities for Bluetooth capable
    platforms now. Rename the variable to be more appropriate.
    
    Start ti-rcu-audio with Blue/Z
    GFRM210 and any future TI-based remote controls have their
    audio format conversion handled in a separate daemon which
    bluez sends the BLE frame contents to.
    
    Start gfrm100-rcu-audio from /sbin/hotplug
    GFRM100 puts audio data in a (giant) HID keypress event.
    Handle this in a separate daemon which monitors hidraw.
    
    Add links for GFRM210 firmware
    Only a TI firmware for now, not GP.
    
    Change-Id: I4fecbfb48854f40379f058c07059ccbd4f9f08db

--- package/google/google_gfrm200/google_gfrm200.mk
+++ package/google/google_gfrm200/google_gfrm200.mk
@@ -8,11 +8,14 @@ GOOGLE_GFRM200_SITE = repo://loader-bin
 GOOGLE_GFRM200_SDIR = $(@D)/philips/gfrm200
 GOOGLE_GFRM200_TDIR = $(TARGET_DIR)/etc/remote
 GOOGLE_GFRM200_LINKS = gfrm200.gp.bin gfrm200.ti.bin
+GOOGLE_GFRM210_SDIR = $(@D)/philips/gfrm210
+GOOGLE_GFRM210_TDIR = $(TARGET_DIR)/etc/remote
+GOOGLE_GFRM210_LINKS = gfrm210.ti.bin
 
 # sample remote GP firmware name: RC1534059_OAD_GP_V0011.bin
 # sample remote TI firmware name: RC1534059_OAD_TI_V0049.bin
 # symlinks gfrm200.ti.bin and gfrm200.gp.bin are used to pick current ones
- 
+
 define GOOGLE_GFRM200_INSTALL_TARGET_CMDS
 	rm -rf $(GOOGLE_GFRM200_TDIR)/*.bin
 	mkdir -p $(GOOGLE_GFRM200_TDIR)
@@ -23,7 +26,14 @@ define GOOGLE_GFRM200_INSTALL_TARGET_CMDS
 	  cp $(GOOGLE_GFRM200_SDIR)/$$l $(GOOGLE_GFRM200_TDIR); \
 	  ln -sf $$l $(GOOGLE_GFRM200_TDIR)/$$n; \
 	  echo "creating symlink link: $$n -> $$l"; \
-      	done
+	done
+	for n in $(GOOGLE_GFRM210_LINKS); do \
+	  md5sum $(GOOGLE_GFRM210_SDIR)/$$n; \
+	  l=$$(readlink $(GOOGLE_GFRM210_SDIR)/$$n); \
+	  cp $(GOOGLE_GFRM210_SDIR)/$$l $(GOOGLE_GFRM210_TDIR); \
+	  ln -sf $$l $(GOOGLE_GFRM210_TDIR)/$$n; \
+	  echo "creating symlink link: $$n -> $$l"; \
+	done
 endef
 
 $(eval $(call GENTARGETS))
