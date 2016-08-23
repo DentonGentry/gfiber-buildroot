72d4b54f87beef60580d06f36444734d2673dbad
Chris Gibson <cgibson@google.com>
2016-08-23 13:11:15 -0700

    buildroot: make multiloader ginstall files for gflt
    Additionally change the gflt110_defconfig to point to the correct
    loader-bin location.
    
    Change-Id: I064ba18974eb6db33de638132d16805352bbd5b4

--- fs/ginstall/gflt/ginstall.mk
+++ fs/ginstall/gflt/ginstall.mk
@@ -13,24 +13,35 @@
 ROOTFS_GINSTALL_DEPENDENCIES = linux rootfs-initramfs
 ROOTFS_GINSTALL_VERSION = $(shell cat $(BINARIES_DIR)/version)
 ROOTFS_GINSTALL_PLATFORMS = $(shell echo $(BR2_TARGET_GENERIC_PLATFORMS_SUPPORTED) | sed 's/[, ][, ]*/, /g' | tr a-z A-Z)
+ROOTFS_GINSTALL_MODELS = $(shell echo $(BR2_TARGET_GENERIC_PLATFORMS_SUPPORTED) | sed 's/[, ][, ]*/ /g' | tr A-Z a-z)
+
+ROOTFS_GINSTALL_DIR = $(call qstrip,$(BR2_TARGET_ROOTFS_GINSTALL_LOADER_DIR))
+ROOTFS_GINSTALL_ARCH = $(call qstrip,$(BR2_ARCH))
 
 ifeq ($(BR2_PACKAGE_GOOGLE_PROD),y)
-GFLT_LOADER := $(BR2_TARGET_ROOTFS_GINSTALL_LOADER_DIR)/u-boot-spi-prod.bin
+ROOTFS_GINSTALL_LOADER = u-boot-spi-prod.bin
 else
-GFLT_LOADER := $(BR2_TARGET_ROOTFS_GINSTALL_LOADER_DIR)/u-boot-spi-dev.bin
+ROOTFS_GINSTALL_LOADER = u-boot-spi-dev.bin
 endif
+ROOTFS_GINSTALL_SIG = $(patsubst %.bin,%.sig,$(ROOTFS_GINSTALL_LOADER))
 
-GFLT_SIG := $(patsubst %.bin,%.sig,$(GFLT_LOADER))
-
+ROOTFS_GINSTALL_LOADERBINS = $(foreach model,$(ROOTFS_GINSTALL_MODELS),loader.$(model).bin)
+ROOTFS_GINSTALL_LOADERSIGS = $(patsubst %.bin,%.sig,$(ROOTFS_GINSTALL_LOADERBINS))
 
 define ROOTFS_GINSTALL_CMD
-	set -e; \
-	set -x; \
-	cp -f $(value GFLT_LOADER) $(BINARIES_DIR) && \
-	cp -f $(value GFLT_SIG) $(BINARIES_DIR) && \
-	cp -f $(value GFLT_LOADER) $(BINARIES_DIR)/loader.img && \
-	cp -f $(value GFLT_SIG) $(BINARIES_DIR)/loader.sig && \
+	set -ex -o pipefail; \
+	rm -f $(BINARIES_DIR)/loaders-sha && \
+	for m in $(ROOTFS_GINSTALL_MODELS); do \
+		set -e -o pipefail && \
+		c=\"cp -f $(ROOTFS_GINSTALL_DIR)/\$$$$m/$(ROOTFS_GINSTALL_LOADER) $(BINARIES_DIR)/loader.\$$$$m.bin\" && \
+		echo \"running: \$$$$c\" && \$$$$c && \
+		c=\"cp -f $(ROOTFS_GINSTALL_DIR)/\$$$$m/$(ROOTFS_GINSTALL_SIG) $(BINARIES_DIR)/loader.\$$$$m.sig\" && \
+		echo \"running: \$$$$c\" && \$$$$c && \
+		c=\"echo loader.\$$$$m.bin-sha1: \$$$$(sha1sum $(BINARIES_DIR)/loader.\$$$$m.bin | cut -c1-40)\" && \
+		echo \"running: \$$$$c\" && \$$$$c >>$(BINARIES_DIR)/loaders-sha; \
+	done && \
 	rm -f $(BINARIES_DIR)/manifest && \
+	echo 'multiloader: 1' >>$(BINARIES_DIR)/manifest && \
 	echo 'installer_version: 3' >>$(BINARIES_DIR)/manifest && \
 	echo 'image_type: unlocked' >>$(BINARIES_DIR)/manifest && \
 	echo 'platforms: [ $(ROOTFS_GINSTALL_PLATFORMS) ]' >>$(BINARIES_DIR)/manifest && \
@@ -38,14 +49,14 @@ define ROOTFS_GINSTALL_CMD
 	cd $(BINARIES_DIR) && \
 	gzip -c <rootfs.cpio >rootfs.cpio.gz && \
 	$(HOST_DIR)/usr/bin/mkimage \
-        -A $(BR2_ARCH) -O linux -T kernel -C none \
-        -a 0x00008000 -e 0x00008000 -n $(value ROOTFS_GINSTALL_VERSION) \
-        -d zImage uImage && \
+		-A $(ROOTFS_GINSTALL_ARCH) -O linux -T kernel -C none \
+		-a 0x00008000 -e 0x00008000 -n $(value ROOTFS_GINSTALL_VERSION) \
+		-d zImage uImage && \
 	cp uImage kernel.img && \
-	(echo -n 'kernel.img-sha1: ' && sha1sum kernel.img | cut -c1-40 && \
-	 echo -n 'loader.img-sha1: ' && sha1sum loader.img | cut -c1-40;) >>manifest && \
+	(echo -n 'kernel.img-sha1: ' && sha1sum kernel.img | cut -c1-40;) >>manifest && \
+	cat loaders-sha >>manifest && \
 	tar -cf $(value ROOTFS_GINSTALL_VERSION).gi \
-		manifest version loader.img loader.sig kernel.img && \
+		manifest version $(ROOTFS_GINSTALL_LOADERBINS) $(ROOTFS_GINSTALL_LOADERSIGS) kernel.img && \
 	ln -sf '$(value ROOTFS_GINSTALL_VERSION).gi' latest.gi
 endef
 
