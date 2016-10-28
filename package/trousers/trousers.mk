122a390cb1180125dc70dbcd8cf66bc80f243bc2
Erdi Chen <erdi@google.com>
2016-10-27 18:08:57 -0700

    Use default TrouSers install targets
    Remove TrouSers custom install commands
    Add host build targets
    
    Change-Id: I723988beb0d10b72102d2d8810a921d9bd9e1e51

--- package/trousers/trousers.mk
+++ package/trousers/trousers.mk
@@ -9,51 +9,12 @@ TROUSERS_SITE_METHOD = git
 TROUSERS_AUTORECONF = YES
 TROUSERS_INSTALL_STAGING = YES
 TROUSERS_INSTALL_TARGET = YES
-TROUSERS_INSTALL_STAGING_OPT = DESTDIR=$${STAGING_DIR} all
-TROUSERS_INSTALL_TARGET_OPT = DESTDIR=$${TARGET_DIR} all
-TROUSERS_UNINSTALL_STAGING_OPT = DESTDIR=$${STAGING_DIR} clean
-TROUSERS_UNINSTALL_TARGET_OPT = DESTDIR=$${TARGET_DIR} clean
+TROUSERS_CONF_OPT = --disable-usercheck
 TROUSERS_DEPENDENCIES = openssl host-pkg-config
 
-define TROUSERS_INSTALL_STAGING_CMDS
-	$(INSTALL) -d $(STAGING_DIR)/usr/include/tss/
-	$(INSTALL) -m 0644 -t $(STAGING_DIR)/usr/include/tss/ $(@D)/src/include/tss/*
-	$(INSTALL) -d $(STAGING_DIR)/usr/include/trousers/
-	$(INSTALL) -m 0644 -t $(STAGING_DIR)/usr/include/trousers/ $(@D)/src/include/trousers/*
-	$(INSTALL) -D -m 0755 $(@D)/src/tspi/.libs/libtspi.so.1.2.0 \
-		$(STAGING_DIR)/usr/lib/
-	ln -fs libtspi.so.1.2.0 \
-		$(STAGING_DIR)/usr/lib/libtspi.so.1
-	ln -fs libtspi.so.1 \
-		$(STAGING_DIR)/usr/lib/libtspi.so
-endef
-
-define TROUSERS_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/src/tcsd/tcsd \
-		$(TARGET_DIR)/usr/bin/
-	$(INSTALL) -D -m 0755 $(@D)/dist/tcsd.conf \
-		$(TARGET_DIR)/etc/
-	$(INSTALL) -D -m 0755 $(@D)/src/tspi/.libs/libtspi.so.1.2.0 \
-		$(TARGET_DIR)/usr/lib/
-	ln -fs libtspi.so.1.2.0 \
-		$(TARGET_DIR)/usr/lib/libtspi.so.1
-	$(INSTALL) -D -m 0755 $(@D)/src/tspi/.libs/libtspi.a \
-		$(TARGET_DIR)/usr/lib/
-endef
-
-define TROUSERS_UNINSTALL_STAGING_CMDS
-	$(RM) -r $(STAGING_DIR)/usr/include/trousers/
-	$(RM) -r $(STAGING_DIR)/usr/include/tss/
-	$(RM) $(STAGING_DIR)/usr/lib/libtspi.so.1.2.0
-	$(RM) $(STAGING_DIR)/usr/lib/libtspi.so.1
-	$(RM) $(STAGING_DIR)/usr/lib/libtspi.so
-endef
-
-define TROUSERS_UNINSTALL_TARGET_CMDS
-	$(RM) $(TARGET_DIR)/usr/bin/tcsd
-	$(RM) $(TARGET_DIR)/etc/tcsd.conf
-	$(RM) $(TARGET_DIR)/usr/lib/libtspi.so.1.2.0
-	$(RM) $(TARGET_DIR)/usr/lib/libtspi.so.1
-endef
+HOST_TROUSERS_AUTORECONF = YES
+HOST_TROUSERS_CONF_OPT = --disable-usercheck
+HOST_TROUSERS_DEPENDENCIES = host-pkg-config
 
 $(eval $(call AUTOTARGETS))
+$(eval $(call AUTOTARGETS,host))
