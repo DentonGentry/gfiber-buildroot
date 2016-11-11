06cef97f13b894e6a628a75008aa02c8949fdaf9
Ed James <edjames@google.com>
2016-11-11 11:04:06 -0800

    libev vs libevent: fix build order issue
    	* libevent, libev, mcastcapture -> build error
    	* fixed mcastcapture to use non-deprecated headers in another CL
    	* add #error to top of deprecated headers to weed out other places
    
        Change-Id: I9eb37f0da9ed7e625e5f096c0aa0fc0b4d94989b
    
    Change-Id: I2201cf291706e77343f32e6ff10535b10c712c05

--- package/libev/libev.mk
+++ package/libev/libev.mk
@@ -8,4 +8,15 @@ LIBEV_SOURCE = libev-$(LIBEV_VERSION).tar.gz
 LIBEV_SITE = http://dist.schmorp.de/libev/Attic/
 LIBEV_INSTALL_STAGING = YES
 
+define LIBEV_DAMAGE_DEPRECATED
+	for n in event.h; do \
+		sed -i '1i#error "ev*.h deprecated, use event2/*.h or ev.h"' \
+	  		$(STAGING_DIR)/usr/include/$$n; \
+	done
+endef
+
+# libev and libevent both install deprecated event.h.
+# Damage deprecated files so we don't conflict.
+LIBEV_POST_INSTALL_TARGET_HOOKS += LIBEV_DAMAGE_DEPRECATED
+
 $(eval $(call AUTOTARGETS))
