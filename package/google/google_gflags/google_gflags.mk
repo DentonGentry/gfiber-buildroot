41c668d842ae8d69acb3b5d3b76cb316a7ff4885
Ryan Daum <rdaum@google.com>
2016-11-23 09:23:00 -0500

    dashsrv: Update deps. Add gflags. Rm dashplayer.
    Adds GFlags library for use by dashsrv.
    Upgrades GFlags due to build bug blocking kvm build:
      https://github.com/gflags/gflags/issues/101
    Add host build for GFlags.
    Removes dashplayer which we are not currently using.
    Clarify in comment which packages are dashsrv-only.
    
    Change-Id: Ic5a9df9f4986e08767286e5ae7680dfbe7747997

--- package/google/google_gflags/google_gflags.mk
+++ package/google/google_gflags/google_gflags.mk
@@ -4,9 +4,8 @@
 #
 #############################################################
 
-GOOGLE_GFLAGS_VERSION = 2.1.1
-GOOGLE_GFLAGS_SOURCE = v$(GOOGLE_GFLAGS_VERSION).tar.gz
-GOOGLE_GFLAGS_SITE = https://github.com/schuhschuh/gflags/archive
+GOOGLE_GFLAGS_VERSION = v2.1.2
+GOOGLE_GFLAGS_SITE = git://github.com/gflags/gflags.git
 
 GOOGLE_GFLAGS_DIR = $(BUILD_DIR)/google_gflags-$(GOOGLE_GFLAGS_VERSION)
 GOOGLE_GFLAGS_INSTALL_STAGING = YES
@@ -14,3 +13,4 @@ GOOGLE_GFLAGS_INSTALL_TARGET = YES
 GOOGLE_GFLAGS_CONF_OPT = -DGFLAGS_NAMESPACE=google -DBUILD_SHARED_LIBS=ON
 
 $(eval $(call CMAKETARGETS))
+$(eval $(call CMAKETARGETS,host))
